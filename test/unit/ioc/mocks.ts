d906fb971532ade8726a2166a668b9b5f21fa779
Fred Kleuver <fred@avurad.nl>
2018-03-23 20:39:54 +0100

    test(requirement-chain): add unit tests

---
+++ test/unit/ioc/mocks.ts
@@ -0,0 +1,37 @@
+import { IRequirement, IInjectionPoint, IFulfillment } from "../../../ioc/interfaces";
+import { DependencyType } from "../../../ioc/types";
+import { Requirements } from "../../../ioc/requirements";
+import { BasicInjectionPoint } from "../../../ioc/injection-point";
+
+export class MockRequirement implements IRequirement {
+  public readonly isRequirement: true = true;
+  public readonly requiredType: DependencyType;
+  public readonly injectionPoint: IInjectionPoint;
+  private fulfillment: IFulfillment;
+
+  constructor(requiredType?: DependencyType, fulfillment?: IFulfillment) {
+    this.injectionPoint = new BasicInjectionPoint(requiredType);
+    this.requiredType = requiredType;
+    this.fulfillment = fulfillment;
+  }
+
+  public isInstantiable(): boolean {
+    return this.fulfillment !== undefined;
+  }
+  public getFulfillment(): IFulfillment {
+    return this.fulfillment;
+  }
+  public restrict(fulfillment: IFulfillment): IRequirement {
+    return new MockRequirement(fulfillment.type, fulfillment);
+  }
+  public isEqualTo(other: MockRequirement): boolean {
+    if (!(other instanceof MockRequirement)) {
+      return false;
+    }
+    return (
+      other.requiredType === this.requiredType &&
+      other.injectionPoint.isEqualTo(this.injectionPoint) &&
+      (!other.fulfillment ? !this.fulfillment : other.fulfillment.isEqualTo(this.fulfillment))
+    );
+  }
+}

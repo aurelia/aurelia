f5d898259ff25d2497013076279281c5cfdb0385
Fred Kleuver <fred@avurad.nl>
2018-06-05 20:22:51 +0200

    test(task-queue): restore/add some tests for task-queue

--- test/runtime/task-queue.spec.ts
+++ test/runtime/task-queue.spec.ts
@@ -1,55 +1,112 @@
-// import { TaskQueue } from '../../../src/framework/task-queue';
+import { ITaskQueue } from '../../src/runtime/task-queue';
+import { DI } from '../../src/runtime/di';
+import { expect } from 'chai';
 
-// describe('task queue', () => {
-//   it('does not provide immediate execution', () => {
-//     let count = 0;
-//     let task = () => (count += 1);
+describe('TaskQueue', () => {
+  let sut: ITaskQueue;
 
-//     let queue = new TaskQueue();
-//     queue.queueTask(task);
-//     queue.queueTask(task);
+  beforeEach(() => {
+    sut = DI.createContainer().get(ITaskQueue);
+  });
 
-//     expect(count).toBe(0);
-//   });
+  it('queueTask does not provide immediate execution', () => {
+    let count = 0;
+    let task = () => (count += 1);
 
-//   it('will execute tasks in the correct order', done => {
-//     let queue = new TaskQueue();
-//     let task1HasRun = false;
-//     let task1 = () => {
-//       expect(queue.flushing).toBe(true);
-//       task1HasRun = true;
-//     };
-//     let task2 = () => {
-//       expect(queue.flushing).toBe(true);
-//       expect(task1HasRun).toEqual(true);
-//       setTimeout(() => {
-//         expect(queue.flushing).toBe(false);
-//         done();
-//       });
-//     };
+    sut.queueTask(task);
+    sut.queueTask(task);
 
-//     expect(queue.flushing).toBe(false);
-//     queue.queueTask(task1);
-//     queue.queueTask(task2);
-//   });
+    expect(count).to.equal(0);
+  });
 
-//   it('will use an onError handler on a task', done => {
-//     let queue = new TaskQueue();
-//     let count = 0;
-//     let task = () => {
-//       expect(queue.flushing).toBe(true);
-//       throw new Error('oops');
-//     };
+  it('flushTaskQueue provides immediate execution', () => {
+    let count = 0;
+    let task = () => (count += 1);
 
-//     (task as any).onError = ex => {
-//       expect(ex.message).toBe('oops');
-//       setTimeout(() => {
-//         expect(queue.flushing).toBe(false);
-//         done();
-//       });
-//     };
+    sut.queueTask(task);
+    sut.flushTaskQueue();
 
-//     expect(queue.flushing).toBe(false);
-//     queue.queueTask(task);
-//   });
-// });
+    expect(count).to.equal(1);
+  });
+
+  it('flushTaskQueue does not affect microTaskQueue', () => {
+    let count = 0;
+    let task = () => (count += 1);
+
+    sut.queueMicroTask(task);
+    sut.queueMicroTask(task);
+    sut.flushTaskQueue();
+
+    expect(count).to.equal(0);
+  });
+
+  it('queueMicroTask does not provide immediate execution', () => {
+    let count = 0;
+    let task = () => (count += 1);
+
+    sut.queueMicroTask(task);
+    sut.queueMicroTask(task);
+
+    expect(count).to.equal(0);
+  });
+
+  it('flushMicroTaskQueue provides immediate execution', () => {
+    let count = 0;
+    let task = () => (count += 1);
+
+    sut.queueMicroTask(task);
+    sut.flushMicroTaskQueue();
+
+    expect(count).to.equal(1);
+  });
+
+  it('flushMicroTaskQueue does not affect taskQueue', () => {
+    let count = 0;
+    let task = () => (count += 1);
+
+    sut.queueTask(task);
+    sut.queueTask(task);
+    sut.flushMicroTaskQueue();
+
+    expect(count).to.equal(0);
+  });
+
+  it('will execute tasks in the correct order', done => {
+    let task1HasRun = false;
+    let task1 = () => {
+      expect(sut.flushing).to.be.true;
+      task1HasRun = true;
+    };
+    let task2 = () => {
+      expect(sut.flushing).to.be.true;
+      expect(task1HasRun).to.be.true;
+      setTimeout(() => {
+        expect(sut.flushing).to.be.false;
+        done();
+      });
+    };
+
+    expect(sut.flushing).to.be.false;
+    sut.queueTask(task1);
+    sut.queueTask(task2);
+  });
+
+  it('will use an onError handler on a task', done => {
+    let count = 0;
+    let task = () => {
+      expect(sut.flushing).to.be.true;
+      throw new Error('oops');
+    };
+
+    (task as any).onError = ex => {
+      expect(ex.message).to.equal('oops');
+      setTimeout(() => {
+        expect(sut.flushing).to.be.false;
+        done();
+      });
+    };
+
+    expect(sut.flushing).to.be.false;
+    sut.queueTask(task);
+  });
+});

0d8a58cbc1ff36fcf1e42ab97cfa561a489b47b3
EisenbergEffect <rob@bluespire.com>
2018-07-08 14:35:11 -0700

    fix(resource): make name readonly

--- src/runtime/resource.ts
+++ src/runtime/resource.ts
@@ -2,7 +2,7 @@ import { Constructable, Immutable } from "../kernel/interfaces";
 import { IRegistry } from "../kernel/di";
 
 export interface IResourceKind<TSource, TType extends IResourceType<TSource> = IResourceType<TSource>> {
-  name: string;
+  readonly name: string;
   key(name: string): string;
   isType<T extends Constructable>(type: T): type is T & TType;
   define<T extends Constructable>(nameOrSource: string | TSource, ctor: T): T & TType;

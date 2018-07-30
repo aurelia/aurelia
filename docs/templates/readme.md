4da12f2d86e3bf6fa0b1894ade8180d54c56b6cc
EisenbergEffect <rob@bluespire.com>
2018-07-29 21:41:43 -0700

    chore(monorepo): begin reorg

---
+++ docs/templates/readme.md
@@ -0,0 +1,3 @@
+# Templates
+
+Scaffolding for creating new assets within this monorepo. e.g. package, article, example, etc.
--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import './test-aot/startup';
-//import './test-dbmonster/startup';
-
-// import { installRequireJSPlugins } from './plugins/requirejs/configuration';
-
-// declare function require(imports: string[]);
-
-// installRequireJSPlugins();
-// require(['./test-jit/startup']);

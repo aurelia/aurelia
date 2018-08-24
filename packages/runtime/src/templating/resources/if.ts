6b23ec3809ce7e27d7d073324982dfaf45e60863
EisenbergEffect <rob@bluespire.com>
2018-08-24 15:00:53 -0700

    fix(runtime): convert if/else to use render location

--- packages/runtime/src/templating/resources/if.ts
+++ packages/runtime/src/templating/resources/if.ts
@@ -1,71 +1,104 @@
 import { inject } from '@aurelia/kernel';
-import { bindable } from '../bindable';
-import { templateController } from '../custom-attribute';
-import { IViewFactory } from '../view';
-import { IViewSlot, SwapOrder } from '../view-slot';
-import { Else } from './else';
-import { IfCore } from './if-core';
 import { BindingFlags } from '../../binding';
+import { INode, IRenderLocation } from '../../dom';
+import { bindable } from '../bindable';
+import { ICustomAttribute, templateController } from '../custom-attribute';
+import { IView, IViewFactory } from '../view';
 
+export interface If extends ICustomAttribute {}
 @templateController('if')
-@inject(IViewFactory, IViewSlot)
-export class If extends IfCore {
-  @bindable public swapOrder: SwapOrder = SwapOrder.after;
-  @bindable public condition: boolean = false;
+@inject(IViewFactory, IRenderLocation)
+export class If {
+  @bindable public value: boolean;
 
-  private animating: boolean = false;
   private elseBehavior: Else;
+  private $child: IView;
+  private ifView: IView;
+  private elseView: IView;
+  private encapsulationSource: INode;
 
-  public conditionChanged(newValue: boolean): void { // can/should we get the flags in here from @templateController's $bind?
+  constructor(private factory: IViewFactory, private location: IRenderLocation) {}
+
+  public bound(): void {
+    this.update(this.value, BindingFlags.fromBind);
+  }
+
+  public attaching(encapsulationSource: INode): void {
+    this.encapsulationSource = encapsulationSource;
+  }
+
+  public unbound(): void {
+    if (this.$child) {
+      this.$child.$unbind(BindingFlags.fromUnbind);
+      this.$child = null;
+    }
+  }
+
+  public valueChanged(newValue: any): void {
     this.update(newValue, BindingFlags.fromBindableHandler);
   }
 
-  public link(elseBehavior: Else): this {
-    if (this.elseBehavior === elseBehavior) {
-      return this;
+  public link(elseBehavior: Else): void {
+    if (this.elseBehavior !== elseBehavior) {
+      this.elseBehavior = elseBehavior;
+      elseBehavior.link(this);
     }
-
-    this.elseBehavior = elseBehavior;
-    elseBehavior.link(this);
-
-    return this;
   }
 
-  private update(show: boolean, flags: BindingFlags): void {
-    if (this.animating) {
-      return;
-    }
-
-    let promise;
-
-    if (this.elseBehavior) {
-      promise = show ? this.swap(this.elseBehavior, this, flags) : this.swap(this, this.elseBehavior, flags);
-    } else {
-      promise = show ? this.show(flags) : this.hide(flags);
-    }
-
-    if (promise) {
-      this.animating = true;
-
-      promise.then(() => {
-        this.animating = false;
-
-        if (this.condition !== this.showing) {
-          this.update(this.condition, flags);
+  private update(renderTrueBranch: boolean, flags: BindingFlags): void {
+    if (renderTrueBranch) {
+      if (this.$child) {
+        if (this.$child === this.ifView) {
+          return;
         }
-      });
+
+        this.removeChild(flags);
+      }
+
+      this.$child = this.ifView || (this.ifView = this.factory.create());
+      this.addChild(flags);
+    } else if (this.elseBehavior) {
+      if (this.$child) {
+        if (this.$child === this.elseView) {
+          return;
+        }
+
+        this.removeChild(flags);
+      }
+
+      this.$child = this.elseView || (this.elseView = this.elseBehavior.factory.create());
+      this.addChild(flags);
+    } else if (this.$child) {
+      this.removeChild(flags);
     }
   }
 
-  private swap(remove: IfCore, add: IfCore, flags: BindingFlags) {
-    switch (this.swapOrder) {
-      case SwapOrder.before:
-        return Promise.resolve(<any>add.show(flags)).then(() => remove.hide(flags));
-      case SwapOrder.with:
-        return Promise.all([<any>remove.hide(flags), add.show(flags)]);
-      default:  // "after", default and unknown values
-        let promise = remove.hide(flags);
-        return promise ? promise.then(() => <any>add.show(flags)) : add.show(flags);
+  private addChild(flags: BindingFlags): void {
+    this.$child.$bind(flags, this.$scope);
+    this.$child.onRender = () => this.$child.$nodes.insertBefore(this.location);
+
+    if (this.$isAttached) {
+      this.$child.$attach(this.encapsulationSource);
+    }
+  }
+
+  private removeChild(flags: BindingFlags): void {
+    this.$child.$detach();
+    this.$child.$unbind(flags);
+  }
+}
+
+@templateController('else')
+@inject(IViewFactory)
+export class Else {
+  private ifBehavior: If;
+
+  constructor(public factory: IViewFactory) {}
+
+  public link(ifBehavior: If): void {
+    if (this.ifBehavior !== ifBehavior) {
+      this.ifBehavior = ifBehavior;
+      ifBehavior.link(this);
     }
   }
 }

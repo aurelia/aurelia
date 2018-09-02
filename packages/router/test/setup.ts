0c2a40051b08237695f72d25da34304ad0b07d77
Fred Kleuver <fkleuver@users.noreply.github.com>
2018-09-02 05:36:23 +0200

    Cleanup (#130)
    * chore(all): remove aurelia-pal-browser and aurelia-polyfills
    
    * chore(all): add more test-friendly chrome flags

--- packages/router/test/setup.ts
+++ packages/router/test/setup.ts
@@ -1,9 +1,6 @@
-import 'aurelia-polyfills';
 import * as chai from 'chai';
 import * as sinon from 'sinon';
 import * as sinonChai from 'sinon-chai';
-import { initialize } from 'aurelia-pal-browser';
-initialize();
 
 chai.should();
 chai.use(sinonChai);

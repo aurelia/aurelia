cb2826515f0d8e80762f680861118841646903f5
Rob Eisenberg <rob@bluespire.com>
2018-09-15 19:20:04 -0700

    test(lifecycle): stubbing out some tests

--- packages/runtime/test/unit/templating/lifecycle.spec.ts
+++ packages/runtime/test/unit/templating/lifecycle.spec.ts
@@ -1,10 +1,22 @@
 import { AttachLifecycle, DetachLifecycle } from '../../../src/index';
 import { expect } from 'chai';
 
-describe('AttachLifecycle', () => {
+describe('The Attach Lifecycle', () => {
+  it('preserves an existing lifecycle if one is in progress', () => {
 
+  });
+
+  it('create a new lifecycle if one is not in progress', () => {
+
+  });
 });
 
-describe('DetachLifecycle', () => {
+describe('The Detach Lifecycle', () => {
+  it('preserves an existing lifecycle if one is in progress', () => {
 
+  });
+
+  it('create a new lifecycle if one is not in progress', () => {
+
+  });
 });

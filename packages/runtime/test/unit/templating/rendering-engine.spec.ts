2e05f265f2edee67795db10c5d8f243b62201c94
bigopon.777@gmail.com <bigopon.777@gmail.com>
2018-09-26 19:31:58 +1000

    fix(RuntimeCompilationResources): do not auto register

--- packages/runtime/test/unit/templating/rendering-engine.spec.ts
+++ packages/runtime/test/unit/templating/rendering-engine.spec.ts
@@ -1,5 +1,20 @@
-import { RenderingEngine, CompiledTemplate, RuntimeCompilationResources, View, ViewFactory } from '../../../src/index';
+import {
+  RenderingEngine,
+  CompiledTemplate,
+  RuntimeCompilationResources,
+  View,
+  ViewFactory,
+  IRenderContext,
+  ExposedContext,
+  CustomElementResource,
+  CustomAttributeResource,
+  BindingBehaviorResource,
+  ValueConverterResource,
+  RenderStrategyResource
+} from '../../../src/index';
 import { expect } from 'chai';
+import { Container } from '../../../../kernel/src';
+import { BindingCommandResource } from '../../../../jit/src';
 
 
 describe('RenderingEngine', () => {
@@ -12,6 +27,22 @@ describe('CompiledTemplate', () => {
 
 describe('RuntimeCompilationResources', () => {
 
+  it('does not register while finding resource', () => {
+    const container = new Container();
+    const resources = new RuntimeCompilationResources((container as any) as ExposedContext);
+
+    [
+      CustomElementResource,
+      CustomAttributeResource,
+      BindingBehaviorResource,
+      ValueConverterResource,
+      RenderStrategyResource,
+      BindingCommandResource
+    ].forEach(r => {
+      resources.find(r, 'a');
+      expect(container.getResolver(r.keyFrom('a'), false)).to.be.null;
+    });
+  });
 });
 
 describe('View', () => {

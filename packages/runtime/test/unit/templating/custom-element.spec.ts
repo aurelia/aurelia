b8e6282e390d331ead6882b5d6be39131e859da2
Fred Kleuver <fred@avurad.nl>
2018-10-01 03:29:11 +0200

    test(custom-element): improve decorator test coverage

--- packages/runtime/test/unit/templating/custom-element.spec.ts
+++ packages/runtime/test/unit/templating/custom-element.spec.ts
@@ -1,20 +1,119 @@
-import { customElement, useShadowDOM, containerless, CustomElementResource, createCustomElementDescription, ShadowDOMProjector, ContainerlessProjector, HostProjector } from '../../../src/index';
+import { customElement, useShadowDOM, containerless, CustomElementResource, createCustomElementDescription, ShadowDOMProjector, ContainerlessProjector, HostProjector, CustomAttributeResource } from '../../../src/index';
 import { expect } from 'chai';
 
 describe('@customElement', () => {
+  it(`with string`, () => {
+    @customElement('foo')
+    class Foo {}
 
+    expect(Foo['description'].name).to.equal('foo');
+  });
+
+  it(`with object`, () => {
+    @customElement({ name: 'foo' })
+    class Foo {}
+
+    expect(Foo['description'].name).to.equal('foo');
+  });
 });
 
 describe('@useShadowDOM', () => {
+  it(`non-invocation`, () => {
+    @useShadowDOM
+    class Foo {}
 
+    expect(Foo['shadowOptions'].mode).to.equal('open');
+  });
+
+  it(`invocation without options`, () => {
+    @useShadowDOM()
+    class Foo {}
+
+    expect(Foo['shadowOptions'].mode).to.equal('open');
+  });
+
+  it(`invocation with options mode=open`, () => {
+    @useShadowDOM({ mode: 'open' })
+    class Foo {}
+
+    expect(Foo['shadowOptions'].mode).to.equal('open');
+  });
+
+  it(`invocation with options mode=closed`, () => {
+    @useShadowDOM({ mode: 'closed' })
+    class Foo {}
+
+    expect(Foo['shadowOptions'].mode).to.equal('closed');
+  });
 });
 
 describe('@containerless', () => {
+  it(`non-invocation`, () => {
+    @containerless
+    class Foo {}
 
+    expect(Foo['containerless']).to.be.true;
+  });
+
+  it(`invocation`, () => {
+    @containerless()
+    class Foo {}
+
+    expect(Foo['containerless']).to.be.true;
+  });
 });
 
 describe('CustomElementResource', () => {
+  describe(`define`, () => {
+    it(`creates a new class when applied to null`, () => {
+      const type = CustomElementResource.define('foo', null);
+      expect(typeof type).to.equal('function');
+      expect(typeof type.constructor).to.equal('function');
+      expect(type.name).to.equal('HTMLOnlyElement');
+      expect(typeof type.prototype).to.equal('object');
+    });
 
+    it(`creates a new class when applied to undefined`, () => { // how though?? it explicitly checks for null??
+      const type = (<any>CustomElementResource).define('foo');
+      expect(typeof type).to.equal('function');
+      expect(typeof type.constructor).to.equal('function');
+      expect(type.name).to.equal('HTMLOnlyElement');
+      expect(typeof type.prototype).to.equal('object');
+    });
+
+    it(`names the resource 'unnamed' if no name is provided`, () => {
+      const type = CustomElementResource.define({}, class Foo {});
+      expect(type.description.name).to.equal('unnamed');
+    });
+  });
+
+  describe(`keyFrom`, () => {
+    it(`returns the correct key`, () => {
+      expect(CustomElementResource.keyFrom('foo')).to.equal('custom-element:foo');
+    });
+  });
+
+  describe(`isType`, () => {
+    it(`returns true when given a resource with the correct kind`, () => {
+      const type = CustomElementResource.define('foo', class Foo{});
+      expect(CustomElementResource.isType(type)).to.be.true;
+    });
+
+    it(`returns false when given a resource with the wrong kind`, () => {
+      const type = CustomAttributeResource.define('foo', class Foo{});
+      expect(CustomElementResource.isType(type)).to.be.false;
+    });
+  });
+
+  describe(`behaviorFor`, () => {
+    it(`returns $customElement variable if it exists`, () => {
+      expect(CustomElementResource.behaviorFor(<any>{$customElement: 'foo'})).to.equal('foo');
+    });
+
+    it(`returns null if the $customElement variable does nots`, () => {
+      expect(CustomElementResource.behaviorFor(<any>{})).to.be.null;
+    });
+  });
 });
 
 describe('createCustomElementDescription', () => {

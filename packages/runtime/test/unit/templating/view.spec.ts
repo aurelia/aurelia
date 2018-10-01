a9f81b8e38d9cd31b7b3508915018317a3ac3c81
Fred Kleuver <fred@avurad.nl>
2018-10-01 06:02:32 +0200

    fix(view): validate renderLocation and ensure $nodes.lastChild exists on mount
    test(view): initial setup for test combinations
    chore(test): add extra type params to cartesianJoinFactory

--- packages/runtime/test/unit/templating/view.spec.ts
+++ packages/runtime/test/unit/templating/view.spec.ts
@@ -1,6 +1,27 @@
-import { ViewFactory } from '../../../src';
+import {
+  noViewTemplate,
+  ITemplate,
+  IScope,
+  BindingContext,
+  ViewFactory,
+  View,
+  DetachLifecycleController,
+  IViewFactory,
+  BindingFlags,
+  IView,
+  IAttachLifecycle,
+  INode,
+  AttachLifecycleController,
+  LifecycleFlags,
+  IDetachLifecycle,
+  Lifecycle,
+  IAttachLifecycleController,
+  IDetachLifecycleController,
+  IRenderLocation
+} from '../../../src';
 import { expect } from 'chai';
-import { eachCartesianJoin } from '../../../../../scripts/test-lib';
+import { eachCartesianJoin, eachCartesianJoinFactory } from '../../../../../scripts/test-lib';
+import { MockTextNodeTemplate } from '../mock';
 
 class StubView {
   constructor(public cached = false) {}
@@ -112,4 +133,106 @@ describe(`ViewFactory`, () => {
       }
     );
   });
+});
+
+describe(`View`, () => {
+  eachCartesianJoinFactory<
+    [string, IView, ITemplate, IViewFactory, boolean],
+    [string, BindingFlags, IScope],
+    [string, (sut: IView) => void],
+    [string, IRenderLocation],
+    [string, (sut: IView) => void],
+    [string, INode, IAttachLifecycleController],
+    [string, (sut: IView) => void],
+    [string, (sut: IView) => void],
+    [string, IDetachLifecycleController],
+    [string, (sut: IView) => void],
+    [string, BindingFlags],
+    [string, (sut: IView) => void],
+    void
+  >(
+    [
+      [
+        () => {
+          const factory = new ViewFactory('foo', noViewTemplate);
+          factory.setCacheSize('*', true);
+          return [`noViewTemplate, viewFactory(cache=true )`, factory.create(), noViewTemplate, factory, true];
+        },
+        () => {
+          const factory = new ViewFactory('foo', noViewTemplate);
+          return [`noViewTemplate, viewFactory(cache=false)`, factory.create(), noViewTemplate, factory, false];
+        }
+      ],
+      [
+        () => [`fromBind, {}`, BindingFlags.fromBind, BindingContext.createScope({})]
+      ],
+      [
+        ([$11, $12, $13, sut], [$21, flags, scope]) => [`$bind`, (sut) => {
+          sut.$bind(flags, scope);
+
+          expect(sut.$scope).to.equal(scope);
+          expect(sut.$isBound).to.be.true;
+        }]
+      ],
+      [
+        () => {
+          const location = document.createElement('div');
+          return [`div`, location]
+        }
+      ],
+      [
+        ([$11, $12, $13, sut], $2, $3, [$41, location]) => [` noop`, (sut) => {
+        }],
+        ([$11, $12, $13, sut], $2, $3, [$41, location]) => [`mount`, (sut) => {
+          if (!location.parentNode) {
+            expect(() => sut.mount(location)).to.throw(/60/);
+          } else {
+            sut.mount(location);
+          }
+        }]
+      ],
+      [
+        () => {
+          const encapsulationSource = document.createElement('div');
+          return [`AttachLifecycle(div, none)`, encapsulationSource, Lifecycle.beginAttach(encapsulationSource, LifecycleFlags.none)]
+        }
+      ],
+      [
+        ([$11, $12, $13, sut], $2, $3, $4, $5, [$61, source, lifecycle]) => [`$attach`, (sut) => {
+          lifecycle.attach(sut).end();
+        }]
+      ],
+      [
+        ([$11, $12, $13, sut], $2, $3, $4, $5, $6, $7) => [`release`, (sut) => {
+          sut.release();
+        }]
+      ],
+      [
+        () => [`DetachLifecycle(none)`, new DetachLifecycleController(LifecycleFlags.none)]
+      ],
+      [
+        ([$11, $12, $13, sut], $2, $3, $4, $5, $6, $7, $8, [$91, lifecycle]) => [`$detach`, (sut) => {
+          lifecycle.detach(sut).end();
+        }]
+      ],
+      [
+        () => [`fromUnbind`, BindingFlags.fromBind]
+      ],
+      [
+        ([$11, $12, $13, sut], $2, $3, $4, $5, $6, $7, $8, $9, $10, [$111, flags]) => [`$unbind`, (sut) => {
+          sut.$unbind(flags);
+        }]
+      ]
+    ],
+    ([t1, sut], [t2], [t3, bind], [t4], [t5, mount], [t6], [t7, attach], [t8, release], [t9], [t10, detach], [t11], [t12, unbind]) => {
+      it(`create(${t1}) -> ${t3}(${t2}) -> ${t5}(${t4}) -> ${t7}(${t6}) -> ${t8}() -> ${t10}(${t9}) -> ${t12}(${t11})`, () => {
+        bind(sut);
+        mount(sut);
+        attach(sut);
+        release(sut);
+        detach(sut);
+        unbind(sut);
+      });
+    }
+  );
 });
--- scripts/test-lib.ts
+++ scripts/test-lib.ts
@@ -369,6 +369,18 @@ export function eachCartesianJoinFactory<T1, T2, T3, T4, T5, T6, T7, T8, T9, U>(
   arrays: [(()=>T1)[], ((arg1: T1)=>T2)[], ((arg1: T1, arg2: T2)=>T3)[], ((arg1: T1, arg2: T2, arg3: T3)=>T4)[], ((arg1: T1, arg2: T2, arg3: T3, arg4: T4)=>T5)[], ((arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5)=>T6)[], ((arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6)=>T7)[], ((arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7)=>T8)[], ((arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7, arg8: T8)=>T9)[]],
   callback: (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7, arg8: T8, arg9: T9) => U): void;
 
+export function eachCartesianJoinFactory<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, U>(
+  arrays: [(()=>T1)[], ((arg1: T1)=>T2)[], ((arg1: T1, arg2: T2)=>T3)[], ((arg1: T1, arg2: T2, arg3: T3)=>T4)[], ((arg1: T1, arg2: T2, arg3: T3, arg4: T4)=>T5)[], ((arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5)=>T6)[], ((arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6)=>T7)[], ((arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7)=>T8)[], ((arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7, arg8: T8)=>T9)[], ((arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7, arg8: T8, arg9: T9)=>T10)[]],
+  callback: (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7, arg8: T8, arg9: T9, arg10: T10) => U): void;
+
+export function eachCartesianJoinFactory<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, U>(
+  arrays: [(()=>T1)[], ((arg1: T1)=>T2)[], ((arg1: T1, arg2: T2)=>T3)[], ((arg1: T1, arg2: T2, arg3: T3)=>T4)[], ((arg1: T1, arg2: T2, arg3: T3, arg4: T4)=>T5)[], ((arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5)=>T6)[], ((arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6)=>T7)[], ((arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7)=>T8)[], ((arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7, arg8: T8)=>T9)[], ((arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7, arg8: T8, arg9: T9)=>T10)[], ((arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7, arg8: T8, arg9: T9, arg10: T10)=>T11)[]],
+  callback: (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7, arg8: T8, arg9: T9, arg10: T10, arg11: T11) => U): void;
+
+export function eachCartesianJoinFactory<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, U>(
+  arrays: [(()=>T1)[], ((arg1: T1)=>T2)[], ((arg1: T1, arg2: T2)=>T3)[], ((arg1: T1, arg2: T2, arg3: T3)=>T4)[], ((arg1: T1, arg2: T2, arg3: T3, arg4: T4)=>T5)[], ((arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5)=>T6)[], ((arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6)=>T7)[], ((arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7)=>T8)[], ((arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7, arg8: T8)=>T9)[], ((arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7, arg8: T8, arg9: T9)=>T10)[], ((arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7, arg8: T8, arg9: T9, arg10: T10)=>T11)[], ((arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7, arg8: T8, arg9: T9, arg10: T10, arg11: T11)=>T12)[]],
+  callback: (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7, arg8: T8, arg9: T9, arg10: T10, arg11: T11, arg12: T12) => U): void;
+
 export function eachCartesianJoinFactory<T extends any, U>(
   arrays: ((...args: T[]) => T)[][],
   callback: (...args: any[]) => U): void {

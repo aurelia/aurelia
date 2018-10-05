ea3d15511746bc885630ce0b4331f62dd5fcbc3c
Fred Kleuver <fred@avurad.nl>
2018-10-05 16:36:40 +0200

    chore(jit): export common

--- packages/jit/src/index.ts
+++ packages/jit/src/index.ts
@@ -1,5 +1,6 @@
 export * from './attribute-parser';
 export * from './binding-command';
+export * from './common';
 export * from './configuration';
 export * from './element-parser';
 export * from './expression-parser';

3f4962c68ef9443d4524830e4637e004f879a531
bigopon.777@gmail.com <bigopon.777@gmail.com>
2018-10-08 01:38:13 +1100

    chore(SelectObsrever): add more integration tests

--- packages/jit/test/integration/template-compiler.select-integration.spec.ts
+++ packages/jit/test/integration/template-compiler.select-integration.spec.ts
@@ -1,4 +1,4 @@
-import { IContainer, DI, PLATFORM, Constructable } from '../../../kernel/src';
+import { IContainer, DI, PLATFORM, Constructable, Primitive } from '../../../kernel/src';
 import { BasicConfiguration } from '../../src';
 import {
   Aurelia, IChangeSet, CustomElementResource, valueConverter,
@@ -101,22 +101,22 @@ describe('TemplateCompiler - <select/> Integration', () => {
 
   describe('<select/> - single', () => {
 
-    it(`multiple toViewBinding <select/>`, () => {
+    it(`works with multiple toView bindings`, () => {
       const component = createCustomElement(
-        template(null,
-          select(
-            { id: 'select1', 'value.to-view': 'selectedValue' },
-            ...[1, 2].map(v => option({ value: v }))
-          ),
-          select(
-            { id: 'select2', 'value.to-view': 'selectedValue' },
-            ...[1, 2].map(v => option({ value: v }))
-          ),
-          select(
-            { id: 'select3', 'value.to-view': 'selectedValue' },
-            ...[3, 4].map(v => option({ value: v }))
-          )
-        ),
+        `<template>
+          <select id="select1" value.to-view="selectedValue">
+            <option>1</option>
+            <option>2</option>
+          </select>
+          <select id="select2" value.to-view="selectedValue">
+            <option>1</option>
+            <option>2</option>
+          </select>
+          <select id="select3" value.to-view="selectedValue">
+            <option>3</option>
+            <option>4</option>
+          </select>
+        </template>`,
         class App {
           selectedValue: string = '2';
         }
@@ -142,20 +142,20 @@ describe('TemplateCompiler - <select/> Integration', () => {
 
     it(`works with mixed of multiple binding: twoWay + toView`, () => {
       const component = createCustomElement(
-        template(null,
-          select(
-            { id: 'select1', 'value.to-view': 'selectedValue' },
-            ...[1, 2].map(v => option({ value: v }))
-          ),
-          select(
-            { id: 'select2', 'value.two-way': 'selectedValue' },
-            ...[1, 2].map(v => option({ value: v }))
-          ),
-          select(
-            { id: 'select3', 'value.to-view': 'selectedValue' },
-            ...[3, 4].map(v => option({ value: v }))
-          )
-        ),
+        `<template>
+          <select id="select1" value.to-view="selectedValue">
+            <option>1</option>
+            <option>2</option>
+          </select>
+          <select id="select2" value.two-way="selectedValue">
+            <option>1</option>
+            <option>2</option>
+          </select>
+          <select id="select3" value.to-view="selectedValue">
+            <option>3</option>
+            <option>4</option>
+          </select>
+        </template>`,
         class App {
           selectedValue: string = '2';
         }
@@ -185,6 +185,116 @@ describe('TemplateCompiler - <select/> Integration', () => {
       expect(observer1.currentValue).to.equal('1');
       // verify observer 3 will take the view model value from changes, regardless valid value from view model
       expect(observer3.currentValue).to.equal('1');
+    });
+  });
+
+  describe('<select/> - multiple', () => {
+
+    it(`works with multiple toView bindings without pre-selection`, () => {
+      const component = createCustomElement(
+        `<template>
+          <select id="select1" multiple value.to-view="selectedValues">
+            <option id="o11">1</option>
+            <option id="o12">2</option>
+            <option id="o13">5</option>
+            <option id="o14">7</option>
+          </select>
+          <select id="select2" multiple value.to-view="selectedValues">
+            <option id="o21">2</option>
+            <option id="o22">4</option>
+            <option id="o23">5</option>
+            <option id="o24">6</option>
+          </select>
+          <select id="select3" multiple value.to-view="selectedValues">
+            <option id="o31">8</option>
+            <option id="o32">9</option>
+            <option id="o33">10</option>
+            <option id="o34">11</option>
+            <option id="o35">12</option>
+          </select>
+        </template>`,
+        class App {
+          selectedValues: Primitive[] = ['1', 2, '2', 3, '3'];
+        }
+      );
+      au.app({ host, component }).start();
+      const select1 = document.querySelector('#select1') as HTMLSelectElement;
+      const select2 = document.querySelector('#select2') as HTMLSelectElement;
+      const select3 = document.querySelector('#select3') as HTMLSelectElement;
+      const observer1 = observerLocator.getObserver(select1, 'value') as SelectValueObserver;
+      const observer2 = observerLocator.getObserver(select2, 'value') as SelectValueObserver;
+      const observer3 = observerLocator.getObserver(select3, 'value') as SelectValueObserver;
+      expect(observer1.currentValue).to.equal(component.selectedValues);
+      expect(observer2.currentValue).to.equal(component.selectedValues);
+      expect(observer3.currentValue).to.equal(component.selectedValues);
+      cs.flushChanges();
+      const options = document.querySelectorAll('option');
+      options.forEach(option => {
+        expect(option.selected).to.be[component.selectedValues.includes(option.value) ? 'true' : 'false'];
+      });
+      component.selectedValues = [];
+      cs.flushChanges();
+      options.forEach(option => {
+        expect(option.selected).to.be.false;
+      });
+    });
+
+    it(`works with mixed of two-way + to-view bindings with pre-selection`, () => {
+      const component = createCustomElement(
+        `<template>
+          <select id="select1" multiple value.to-view="selectedValues">
+            <option id="o11">1</option>
+            <option id="o12">2</option>
+            <option id="o13" selected>5</option>
+            <option id="o14" selected>7</option>
+          </select>
+          <select id="select2" multiple value.two-way="selectedValues">
+            <option id="o21">2</option>
+            <option id="o22">4</option>
+            <option id="o23" selected>5</option>
+            <option id="o24" selected>6</option>
+          </select>
+          <select id="select3" multiple value.two-way="selectedValues">
+            <option id="o31">8</option>
+            <option id="o32">9</option>
+            <option id="o33" selected>10</option>
+            <option id="o34">11</option>
+            <option id="o35" selected>12</option>
+          </select>
+        </template>`,
+        class App {
+          selectedValues: Primitive[] = [];
+        }
+      );
+      au.app({ host, component }).start();
+      const select1 = document.querySelector('#select1') as HTMLSelectElement;
+      const select2 = document.querySelector('#select2') as HTMLSelectElement;
+      const select3 = document.querySelector('#select3') as HTMLSelectElement;
+      const observer1 = observerLocator.getObserver(select1, 'value') as SelectValueObserver;
+      const observer2 = observerLocator.getObserver(select2, 'value') as SelectValueObserver;
+      const observer3 = observerLocator.getObserver(select3, 'value') as SelectValueObserver;
+      expect(observer1.currentValue).to.equal(component.selectedValues);
+      expect(observer2.currentValue).to.equal(component.selectedValues);
+      expect(observer3.currentValue).to.equal(component.selectedValues);
+      cs.flushChanges();
+      const options = document.querySelectorAll('option');
+      options.forEach(option => {
+        expect(option.selected).to.be[component.selectedValues.includes(option.value) ? 'true' : 'false'];
+      });
+      component.selectedValues = [];
+      cs.flushChanges();
+      options.forEach(option => {
+        expect(option.selected).to.be.false;
+      });
+
+      [].forEach.call(select3.options, (option: HTMLOptionElement) => {
+        option.selected = true;
+      });
+      select3.dispatchEvent(new CustomEvent('change', { bubbles: true }));
+      expect(component.selectedValues.toString()).to.equal(['8', '9', '10', '11', '12'].toString());
+      [].forEach.call(select2.options, (option: HTMLOptionElement) => {
+        option.selected = true;
+      });
     });
   });
 

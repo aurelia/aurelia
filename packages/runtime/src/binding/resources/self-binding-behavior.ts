738e0f7991edbdac621bdf434fbf68f030642232
Fred Kleuver <fred@avurad.nl>
2018-10-09 20:01:17 +0200

    fix(self-binding-behavior): fix event.path and type defs

--- packages/runtime/src/binding/resources/self-binding-behavior.ts
+++ packages/runtime/src/binding/resources/self-binding-behavior.ts
@@ -4,29 +4,42 @@ import { IScope } from '../binding-context';
 import { BindingFlags } from '../binding-flags';
 import { Listener } from '../listener';
 
+type CompatibleEvent = {
+  target?: EventTarget;
+
+  // legacy
+  path?: EventTarget[];
+
+  // old composedPath
+  deepPath?(): EventTarget[];
+
+  // current spec
+  composedPath?(): EventTarget[];
+};
+
 /*@internal*/
-export function findOriginalEventTarget(event) {
-  return (event.path && event.path[0]) || (event.deepPath && event.deepPath[0]) || event.target;
+export function findOriginalEventTarget(event: Event & CompatibleEvent): EventTarget {
+  return (event.composedPath && event.composedPath()[0]) || (event.deepPath && event.deepPath()[0]) || (event.path && event.path[0]) || event.target;
 }
 
 /*@internal*/
-export function handleSelfEvent(event: Event) {
-  let target = findOriginalEventTarget(event);
+export function handleSelfEvent(event: Event): ReturnType<Listener['callSource']> {
+  const target = findOriginalEventTarget(event);
 
   if (this.target !== target) {
     return;
   }
 
-  this.selfEventCallSource(event);
+  return this.selfEventCallSource(event);
 }
 
 export type SelfableBinding = Listener & {
-  selfEventCallSource: (event: Event) => any;
+  selfEventCallSource: Listener['callSource'];
 };
 
 @bindingBehavior('self')
 export class SelfBindingBehavior {
-  public bind(flags: BindingFlags, scope: IScope, binding: SelfableBinding) {
+  public bind(flags: BindingFlags, scope: IScope, binding: SelfableBinding): void {
     if (!binding.callSource || !binding.targetEvent) {
       throw Reporter.error(8);
     }
@@ -35,7 +48,7 @@ export class SelfBindingBehavior {
     binding.callSource = handleSelfEvent;
   }
 
-  public unbind(flags: BindingFlags, scope: IScope, binding: SelfableBinding) {
+  public unbind(flags: BindingFlags, scope: IScope, binding: SelfableBinding): void {
     binding.callSource = binding.selfEventCallSource;
     binding.selfEventCallSource = null;
   }

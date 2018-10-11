c71f07718ab90be95853f34e34606102db3bda50
Fred Kleuver <fred@avurad.nl>
2018-10-11 06:53:11 +0200

    chore(plugin-parcel/plugin-webpack): add bundler plugin boilerplate for parcel and webpack

---
+++ packages/plugin-parcel/test/unit/index.spec.ts
@@ -0,0 +1,9 @@
+import { placeholder } from '../../src/index';
+import { expect } from 'chai';
+
+describe('index', () => {
+  it('placeholder should be null', () => {
+    expect(placeholder).to.be.null;
+  });
+});
+

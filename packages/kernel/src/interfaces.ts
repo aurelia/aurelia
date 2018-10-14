bb2a0f2dec6c7b75bc4268b0cceeebc56825b7ee
Fred Kleuver <fred@avurad.nl>
2018-10-14 21:01:31 +0200

    fix(kernel): fix circular type reference issue introduced by ts 3.1

--- packages/kernel/src/interfaces.ts
+++ packages/kernel/src/interfaces.ts
@@ -27,23 +27,26 @@ export type Injectable<T = {}> = Constructable<T> & { inject?: any[] };
 export type IIndexable<T extends object = object> = T & { [key: string]: any };
 
 export type ImmutableObject<T> =
-  T extends [infer A]                                                       ? Immutable<[A]> :
-  T extends [infer A, infer B]                                              ? Immutable<[A, B]> :
-  T extends [infer A, infer B, infer C]                                     ? Immutable<[A, B, C]> :
-  T extends [infer A, infer B, infer C, infer D]                            ? Immutable<[A, B, C, D]> :
-  T extends [infer A, infer B, infer C, infer D, infer E]                   ? Immutable<[A, B, C, D, E]> :
-  T extends [infer A, infer B, infer C, infer D, infer E, infer F]          ? Immutable<[A, B, C, D, E, F]> :
-  T extends [infer A, infer B, infer C, infer D, infer E, infer F, infer G] ? Immutable<[A, B, C, D, E, F, G]> :
-  T extends (infer A)[]           ? ImmutableArray<A> :
-  T extends Function              ? T : // can change to never to forbid functions
-  T extends Map<infer U, infer V> ? ReadonlyMap<Immutable<U>, Immutable<V>> :
-  T extends Set<infer U>          ? ReadonlySet<Immutable<U>> :
-  T extends Record<string, infer V> ? Record<string, Immutable<V>> :
-  T extends object                ? Immutable<T> :
-  T;
+    T extends [infer A1, infer B1, infer C1, infer D1, infer E1, infer F1, infer G] ? ImmutableArray<[A1, B1, C1, D1, E1, F1, G]> :
+    T extends [infer A2, infer B2, infer C2, infer D2, infer E2, infer F2]          ? ImmutableArray<[A2, B2, C2, D2, E2, F2]> :
+    T extends [infer A3, infer B3, infer C3, infer D3, infer E3]                    ? ImmutableArray<[A3, B3, C3, D3, E3]> :
+    T extends [infer A4, infer B4, infer C4, infer D4]                              ? ImmutableArray<[A4, B4, C4, D4]> :
+    T extends [infer A5, infer B5, infer C5]                                        ? ImmutableArray<[A5, B5, C5]> :
+    T extends [infer A6, infer B6]                                                  ? ImmutableArray<[A6, B6]> :
+    T extends [infer A7]                                                            ? ImmutableArray<[A7]> :
+    T extends (infer A)[]              ? ImmutableArray<A> :
+    T extends any[]                    ? ImmutableArray<T[number]> :
+    T extends Map<infer U1, infer V1>  ? ReadonlyMap<Immutable<U1>, Immutable<V1>> :
+    T extends Set<infer U2>            ? ReadonlySet<Immutable<U2>> :
+    T extends Record<string, infer V2> ? Record<string, Immutable<V2>> :
+    T extends object                   ? Immutable<T> :
+    T;
 
-export interface ImmutableArray<T> extends ReadonlyArray<ImmutableObject<T>> {}
-export type Immutable<T> = { readonly [P in keyof T]: ImmutableObject<T[P]> };
+export interface ImmutableArray<T> extends ReadonlyArray<ImmutableObject<T>> {};
+
+export type Immutable<T> = {
+    readonly [K in keyof T]: ImmutableObject<T[K]>
+};
 
 export type Writable<T> = {
   -readonly [K in keyof T]: T[K]

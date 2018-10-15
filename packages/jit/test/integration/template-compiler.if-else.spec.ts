0e66eb3e6576527256890b535cea662044d74d72
Fred Kleuver <fred@avurad.nl>
2018-10-15 03:34:29 +0200

    perf(node-sequence): implement special textNodeSequence and reorganize a few things
    fixes #219

--- packages/jit/test/integration/template-compiler.if-else.spec.ts
+++ packages/jit/test/integration/template-compiler.if-else.spec.ts
@@ -1,8 +1,13 @@
 import { expect } from 'chai';
-import { tearDown, setupAndStart, cleanup } from './prepare';
+import { tearDown, setupAndStart, cleanup, defineCustomElement } from './prepare';
+import { baseSuite } from './template-compiler.base';
+import { IContainer, Constructable } from '@aurelia/kernel';
+import { Aurelia, IChangeSet, ICustomElementType } from '@aurelia/runtime';
+
+const spec = 'template-compiler.if-else';
 
 // TemplateCompiler - if/else integration
-describe('template-compiler.if-else', () => {
+describe(spec, () => {
   beforeEach(cleanup);
   afterEach(cleanup);
 
@@ -32,4 +37,61 @@ describe('template-compiler.if-else', () => {
     expect(host.innerText).to.equal('bar');
     tearDown(au, cs, host);
   });
+
 });
+
+type TApp = Constructable<{ ifText: string; elseText: string; show: boolean; }> & ICustomElementType;
+const suite = baseSuite.clone<IContainer, Aurelia, IChangeSet, HTMLElement, TApp, InstanceType<TApp>>(spec);
+
+suite.addDataSlot('e').addData('app').setFactory(ctx => {
+  const { a: container } = ctx;
+  const template = document.createElement('template');
+
+  const ifTemplate = document.createElement('template');
+  const elseTemplate = document.createElement('template');
+  template.content.appendChild(ifTemplate);
+  template.content.appendChild(elseTemplate);
+  const ifText = document.createTextNode('${ifText}');
+  const elseText = document.createTextNode('${elseText}');
+  ifTemplate.content.appendChild(ifText);
+  elseTemplate.content.appendChild(elseText);
+  ifTemplate.setAttribute('if.bind', 'show');
+  elseTemplate.setAttribute('else', '');
+
+  const $App = defineCustomElement('app', template, class App {
+    public ifText: string;
+    public elseText: string;
+    public show: boolean;
+  });
+  container.register($App);
+  return $App;
+});
+
+suite.addActionSlot('setup').addAction(null, ctx => {
+  const { a: container, b: au, c: cs, d: host, e: app } = ctx;
+
+  const component = ctx.f = new app();
+  component.ifText = '1';
+  component.elseText = '2';
+  component.show = true;
+
+  au.app({ component, host }).start();
+});
+
+suite.addActionSlot('act')
+.addAction('1', ctx => {
+  const { c: cs, d: host, f: component } = ctx;
+
+  expect(host.textContent).to.equal('1');
+
+  component.show = false;
+
+  expect(host.textContent).to.equal('1');
+
+  cs.flushChanges();
+
+  expect(host.textContent).to.equal('2');
+});
+
+suite.load();
+suite.run();

a122b28c478fe56b952a36830b7591103b9bedd6
Fred Kleuver <fred@avurad.nl>
2018-10-21 20:30:17 +0200

    chore(wdio): add browser logs to test output

--- test/wdio/specs/examples.spec.js
+++ test/wdio/specs/examples.spec.js
@@ -3,6 +3,7 @@ const { equal } = require('assert');
 describe('aurelia example', function() {
   it('should display "Hello World!"', function() {
     browser.url('/');
+    console.log(`[browser logs]: ${JSON.stringify(browser.log('browser'), null, 2)}`);
     equal(browser.getText('app>div'), 'Hello World!');
   });
 });

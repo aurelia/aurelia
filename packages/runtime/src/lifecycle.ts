c5c8865339dea8db73a55df57852f17510eed0f8
Fred Kleuver <fred@avurad.nl>
2018-10-27 03:50:31 +0200

    perf(dom): only remove root nodes

--- packages/runtime/src/lifecycle.ts
+++ packages/runtime/src/lifecycle.ts
@@ -571,10 +571,7 @@ export class DetachLifecycleController implements IDetachLifecycle, IDetachLifec
     if (this.allowUnmount) {
       this.unmountTail.$nextUnmount = requestor;
       this.unmountTail = requestor;
-      // Note: this comment is just a temporary measure while we get some complex integration tests to work first.
-      // Just to reduce the amount of potential things to track down and check if something fails.
-      // When everything is working and tested, we can add this optimization (and others) back in.
-      //this.allowUnmount = false; // only remove roots
+      this.allowUnmount = false; // only remove roots
     }
   }
 

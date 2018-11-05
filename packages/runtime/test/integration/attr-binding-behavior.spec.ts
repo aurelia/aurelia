57c6a6d4baa902b74f3e3cf4c9de8347253c2b48
Fred Kleuver <fred@avurad.nl>
2018-11-06 00:00:25 +0100

    chore(runtime): fix some more integration tests

--- packages/runtime/test/integration/attr-binding-behavior.spec.ts
+++ packages/runtime/test/integration/attr-binding-behavior.spec.ts
@@ -1,35 +1,35 @@
-// import { DataAttributeAccessor, AttrBindingBehavior ,IExpression , IObserverLocator ,Binding ,LifecycleFlags,IScope ,BindingMode,DOM, IsBindingBehavior } from '../../src/index';
-// import { expect } from 'chai';
-// import { IContainer, DI } from '../../../kernel/src/index';
+import { DataAttributeAccessor, AttrBindingBehavior, IExpression, IObserverLocator, Binding, LifecycleFlags, IScope, BindingMode, DOM, IsBindingBehavior } from '../../src/index';
+import { expect } from 'chai';
+import { IContainer, DI } from '../../../kernel/src/index';
 
-// describe('AttrBindingBehavior', () => {
-//   let sourceExpression: IsBindingBehavior;
-//   let target: any;
-//   let targetProperty: string;
-//   let mode: BindingMode;
-//   let observerLocator: IObserverLocator;
-//   let container: IContainer;
-//   let sut: AttrBindingBehavior;
-//   let binding: Binding;
-//   let flags: LifecycleFlags;
-//   let scope: IScope;
+describe('AttrBindingBehavior', () => {
+  let sourceExpression: IsBindingBehavior;
+  let target: any;
+  let targetProperty: string;
+  let mode: BindingMode;
+  let observerLocator: IObserverLocator;
+  let container: IContainer;
+  let sut: AttrBindingBehavior;
+  let binding: Binding;
+  let flags: LifecycleFlags;
+  let scope: IScope;
 
-//   beforeEach(() => {
-//     target = DOM.createElement('div');
-//     targetProperty = 'foo';
-//     sut = new AttrBindingBehavior();
-//     container = DI.createContainer();
-//     binding = new Binding(sourceExpression, target, targetProperty, mode, observerLocator, container);
-//     sut.bind(flags, scope, binding);
-//   });
+  beforeEach(() => {
+    target = DOM.createElement('div');
+    targetProperty = 'foo';
+    sut = new AttrBindingBehavior();
+    container = DI.createContainer();
+    binding = new Binding(sourceExpression, target, targetProperty, mode, observerLocator, <any>container);
+    sut.bind(flags, scope, binding);
+  });
 
-//   it('bind()   should put a DataAttributeObserver on the binding', () => {
-//     expect(binding.targetObserver instanceof DataAttributeAccessor).to.be.true;
-//     expect(binding.targetObserver['obj'] === target).to.be.true;
-//     expect(binding.targetObserver['propertyKey'] === targetProperty).to.be.true;
-//   });
+  it('bind()   should put a DataAttributeObserver on the binding', () => {
+    expect(binding.targetObserver instanceof DataAttributeAccessor).to.be.true;
+    expect(binding.targetObserver['obj'] === target).to.be.true;
+    expect(binding.targetObserver['propertyKey'] === targetProperty).to.be.true;
+  });
 
-//   // it('unbind() should clear the DataAttributeObserver from the binding', () => {
-//   //   // TODO: it doesn't actually do, and it should
-//   // });
-// });
+  // it('unbind() should clear the DataAttributeObserver from the binding', () => {
+  //   // TODO: it doesn't actually do, and it should
+  // });
+});

e10f4fdaea25dc63ae026eec5e7474b59fa6c0a4
Fred Kleuver <fred@avurad.nl>
2018-11-07 04:09:50 +0100

    chore(runtime): re-enable bind resources tests

--- packages/runtime/test/integration/throttle-binding-behavior.spec.ts
+++ packages/runtime/test/integration/throttle-binding-behavior.spec.ts
@@ -1,42 +1,42 @@
-// import { IExpression, IObserverLocator, Binding, LifecycleFlags, IScope, BindingMode, ThrottleBindingBehavior, IsBindingBehavior } from '../../src/index';
-// import { expect } from 'chai';
-// import { IContainer } from '../../../kernel/src/index';
+import { IExpression, IObserverLocator, Binding, LifecycleFlags, IScope, BindingMode, ThrottleBindingBehavior, IsBindingBehavior } from '../../src/index';
+import { expect } from 'chai';
+import { IContainer, DI } from '../../../kernel/src/index';
 
-// describe('ThrottleBindingBehavior', () => {
-//   let sourceExpression: IsBindingBehavior;
-//   let target: any;
-//   let targetProperty: string;
-//   let mode: BindingMode;
-//   let observerLocator: IObserverLocator;
-//   let container: IContainer;
-//   let sut: ThrottleBindingBehavior;
-//   let binding: Binding;
-//   let flags: LifecycleFlags;
-//   let scope: IScope;
-//   let originalFn: Function;
+describe('ThrottleBindingBehavior', () => {
+  let sourceExpression: IsBindingBehavior;
+  let target: any;
+  let targetProperty: string;
+  let mode: BindingMode;
+  let observerLocator: IObserverLocator;
+  let container: IContainer = DI.createContainer();
+  let sut: ThrottleBindingBehavior;
+  let binding: Binding;
+  let flags: LifecycleFlags;
+  let scope: IScope;
+  let originalFn: Function;
 
-//   beforeEach(() => {
-//     sut = new ThrottleBindingBehavior();
-//     binding = new Binding(sourceExpression, target, targetProperty, mode, observerLocator, container);
-//     originalFn = binding.updateTarget;
-//     sut.bind(flags, scope, <any>binding);
-//   });
+  beforeEach(() => {
+    sut = new ThrottleBindingBehavior();
+    binding = new Binding(sourceExpression, target, targetProperty, mode, observerLocator, <any>container);
+    originalFn = binding.updateTarget;
+    sut.bind(flags, scope, <any>binding);
+  });
 
-//   // TODO: test properly (whether throttling works etc)
-//   it('bind()   should apply the correct behavior', () => {
-//     expect(binding['throttledMethod'] === originalFn).to.be.true;
-//     expect(binding['throttledMethod'].originalName).to.equal('updateTarget');
-//     expect(binding.updateTarget === originalFn).to.be.false;
-//     expect(typeof binding.updateTarget).to.equal('function');
-//     expect(binding['throttleState']).not.to.be.null
-//     expect(typeof binding['throttleState']).to.equal('object');
-//   });
+  // TODO: test properly (whether throttling works etc)
+  it('bind()   should apply the correct behavior', () => {
+    expect(binding['throttledMethod'] === originalFn).to.be.true;
+    expect(binding['throttledMethod'].originalName).to.equal('updateTarget');
+    expect(binding.updateTarget === originalFn).to.be.false;
+    expect(typeof binding.updateTarget).to.equal('function');
+    expect(binding['throttleState']).not.to.be.null
+    expect(typeof binding['throttleState']).to.equal('object');
+  });
 
-//   it('unbind() should revert the original behavior', () => {
-//     sut.unbind(flags, scope, <any>binding);
-//     expect(binding['throttledMethod']).to.be.null;
-//     expect(binding.updateTarget === originalFn).to.be.true;
-//     expect(typeof binding.updateTarget).to.equal('function');
-//     expect(binding['throttleState']).to.be.null
-//   });
-// });
+  it('unbind() should revert the original behavior', () => {
+    sut.unbind(flags, scope, <any>binding);
+    expect(binding['throttledMethod']).to.be.null;
+    expect(binding.updateTarget === originalFn).to.be.true;
+    expect(typeof binding.updateTarget).to.equal('function');
+    expect(binding['throttleState']).to.be.null
+  });
+});

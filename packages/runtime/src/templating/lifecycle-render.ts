40ce838b15d4fa901b2256c01216521aa05ec04e
Fred Kleuver <fred@avurad.nl>
2018-11-14 21:26:18 +0100

    chore(encapsulation-source): cleanup render api from previous attempt

--- packages/runtime/src/templating/lifecycle-render.ts
+++ packages/runtime/src/templating/lifecycle-render.ts
@@ -14,7 +14,7 @@ import { Observer } from '../binding/property-observation';
 import { Ref } from '../binding/ref';
 import { subscriberCollection } from '../binding/subscriber-collection';
 import { BindableDefinitions, buildTemplateDefinition, customAttributeKey, customElementBehavior, CustomElementConstructor, customElementKey, IAttributeDefinition, ICallBindingInstruction, IHydrateAttributeInstruction, IHydrateElementInstruction, IHydrateTemplateController, IInterpolationInstruction, IIteratorBindingInstruction, ILetElementInstruction, IListenerBindingInstruction, IPropertyBindingInstruction, IRefBindingInstruction, IRenderStrategyInstruction, ISetAttributeInstruction, ISetPropertyInstruction, IStylePropertyBindingInstruction, ITargetedInstruction, ITemplateDefinition, ITextBindingInstruction, TargetedInstructionType, TemplateDefinition, TemplatePartDefinitions } from '../definitions';
-import { DOM, IEncapsulationSource, INode, INodeSequence, INodeSequenceFactory, IRenderLocation, NodeSequence, NodeSequenceFactory } from '../dom';
+import { DOM, INode, INodeSequence, INodeSequenceFactory, IRenderLocation, NodeSequence, NodeSequenceFactory } from '../dom';
 import { Hooks, IAttach, IAttachables, IBindables, IBindScope, IBindSelf, ILifecycle, ILifecycleHooks, ILifecycleUnbindAfterDetach, IMountable, IRenderable, IRenderContext, IState, IViewFactory, State } from '../lifecycle';
 import { IAccessor, IChangeTracker, IPropertySubscriber, ISubscribable, ISubscriberCollection, LifecycleFlags, MutationKind } from '../observation';
 import { IResourceDescriptions, IResourceKind, IResourceType, ResourceDescription } from '../resource';
@@ -138,7 +138,6 @@ export interface ILifecycleRender {
    * - Populate `this.$context` with the RenderContext / Container scoped to this instance
    *
    * @param host The DOM node that declares this custom element
-   * @param encapsulationSource The nearest app host or DOM boundary
    * @param parts Replaceable parts, if any
    *
    * @returns Either an implementation of `IElementTemplateProvider`, or void
@@ -147,7 +146,7 @@ export interface ILifecycleRender {
    * This is the first "hydrate" lifecycle hook. It happens only once per instance (contrary to bind/attach
    * which can happen many times per instance), though it can happen many times per type (once for each instance)
    */
-  render?(host: INode, encapsulationSource: IEncapsulationSource, parts: Immutable<Pick<IHydrateElementInstruction, 'parts'>>): IElementTemplateProvider | void;
+  render?(host: INode, parts: Immutable<Pick<IHydrateElementInstruction, 'parts'>>): IElementTemplateProvider | void;
 }
 
 export interface ICustomAttribute extends Partial<IChangeTracker>, IBindScope, ILifecycleUnbindAfterDetach, IAttach, OptionalHooks, RequiredLifecycleProperties {
@@ -172,13 +171,12 @@ export function $hydrateElement(this: Writable<ICustomElement>, renderingEngine:
 
   this.$scope = Scope.create(this, null);
   this.$host = host;
-  const projector = this.$projector = determineProjector(this, host, description);
-  const encapsulationSource = projector.provideEncapsulationSource(host);
+  this.$projector = determineProjector(this, host, description);
 
   renderingEngine.applyRuntimeBehavior(Type, this);
 
   if (this.$hooks & Hooks.hasRender) {
-    const result = this.render(host, encapsulationSource, options.parts);
+    const result = this.render(host, options.parts);
 
     if (result && 'getElementTemplate' in result) {
       const template = result.getElementTemplate(renderingEngine, Type);

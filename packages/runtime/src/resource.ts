d88baf46d5ccc37614f96894613c5810e8f36ea1
Fred Kleuver <fred@avurad.nl>
2018-11-16 19:13:19 +0100

    chore(resource): rename TSource to TDef

--- packages/runtime/src/resource.ts
+++ packages/runtime/src/resource.ts
@@ -4,32 +4,32 @@ export interface IResourceDefinition {
   name: string;
 }
 
-export interface IResourceKind<TSource, TProto, TClass extends Class<TProto, unknown> = Class<TProto>> {
+export interface IResourceKind<TDef, TProto, TClass extends Class<TProto, unknown> = Class<TProto>> {
   readonly name: string;
   keyFrom(name: string): string;
-  isType<T>(Type: T & Partial<IResourceType<TSource, TProto>>): Type is T & TClass & IResourceType<TSource, TProto>;
+  isType<T>(Type: T & Partial<IResourceType<TDef, TProto>>): Type is T & TClass & IResourceType<TDef, TProto>;
 
-  define<T>(name: string, ctor: T & Partial<IResourceType<TSource, TProto>>): T & TClass & IResourceType<TSource, TProto>;
-  define<T>(definition: TSource, ctor: T & Partial<IResourceType<TSource, TProto>>): T & TClass & IResourceType<TSource, TProto>;
-  define<T>(nameOrDefinition: string | TSource, ctor: T & Partial<IResourceType<TSource, TProto>>): T & TClass & IResourceType<TSource, TProto>;
+  define<T>(name: string, ctor: T & Partial<IResourceType<TDef, TProto>>): T & TClass & IResourceType<TDef, TProto>;
+  define<T>(definition: TDef, ctor: T & Partial<IResourceType<TDef, TProto>>): T & TClass & IResourceType<TDef, TProto>;
+  define<T>(nameOrDefinition: string | TDef, ctor: T & Partial<IResourceType<TDef, TProto>>): T & TClass & IResourceType<TDef, TProto>;
 }
 
-export type ResourceDescription<TSource> = Immutable<Required<TSource>>;
+export type ResourceDescription<TDef> = Immutable<Required<TDef>>;
 
-export interface IResourceType<TSource, TProto, TClass extends Class<TProto, unknown> = Class<TProto>> extends Class<TProto, unknown>, IRegistry {
-  readonly kind: IResourceKind<TSource, TProto, TClass>;
-  readonly description: ResourceDescription<TSource>;
+export interface IResourceType<TDef, TProto, TClass extends Class<TProto, unknown> = Class<TProto>> extends Class<TProto, unknown>, IRegistry {
+  readonly kind: IResourceKind<TDef, TProto, TClass>;
+  readonly description: ResourceDescription<TDef>;
 }
 
 export interface IResourceDescriptions {
-  find<TSource, TProto>(kind: IResourceKind<TSource, TProto>, name: string): ResourceDescription<TSource> | null;
-  create<TSource, TProto>(kind: IResourceKind<TSource, TProto>, name: string): TProto;
+  find<TDef, TProto>(kind: IResourceKind<TDef, TProto>, name: string): ResourceDescription<TDef> | null;
+  create<TDef, TProto>(kind: IResourceKind<TDef, TProto>, name: string): TProto;
 }
 
 export class RuntimeCompilationResources implements IResourceDescriptions {
   constructor(private context: IContainer) {}
 
-  public find<TSource, TProto>(kind: IResourceKind<TSource, TProto>, name: string): ResourceDescription<TSource> | null {
+  public find<TDef, TProto>(kind: IResourceKind<TDef, TProto>, name: string): ResourceDescription<TDef> | null {
     const key = kind.keyFrom(name);
     const resolver = this.context.getResolver(key, false);
 
@@ -37,7 +37,7 @@ export class RuntimeCompilationResources implements IResourceDescriptions {
       const factory = resolver.getFactory(this.context);
 
       if (factory !== null) {
-        const description = (factory.type as IResourceType<TSource, TProto>).description;
+        const description = (factory.type as IResourceType<TDef, TProto>).description;
         return description === undefined ? null : description;
       }
     }
@@ -45,7 +45,7 @@ export class RuntimeCompilationResources implements IResourceDescriptions {
     return null;
   }
 
-  public create<TSource, TProto>(kind: IResourceKind<TSource, TProto>, name: string): TProto {
+  public create<TDef, TProto>(kind: IResourceKind<TDef, TProto>, name: string): TProto {
     const key = kind.keyFrom(name);
     if (this.context.has(key, false)) {
       const instance = this.context.get<Constructable>(key);

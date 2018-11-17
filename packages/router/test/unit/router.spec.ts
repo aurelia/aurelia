2b220dd028ef347569479a7383b3880cbbed59f2
Fred Kleuver <fred@avurad.nl>
2018-11-17 01:33:56 +0100

    test(router): add unit test skeleton

---
+++ packages/router/test/unit/router.spec.ts
@@ -0,0 +1,9 @@
+import { Router } from '../../src/index';
+import { expect } from 'chai';
+
+describe('Router', () => {
+  it('can be created', () => {
+    const sut = new Router();
+  });
+});
+
---
+++ packages/router/test/unit/viewport.spec.ts
@@ -0,0 +1,9 @@
+import { Viewport } from '../../src/index';
+import { expect } from 'chai';
+
+describe('Viewport', () => {
+  it('can be created', () => {
+    const sut = new Viewport(null, null);
+  });
+});
+

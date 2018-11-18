ded1468c3e0128f87179d18a9859a4e33eb18528
Jürgen Wenzel <jurgen@jwenzel.se>
2018-11-18 22:22:21 +0100

    chore(router): temp commit

--- packages/router/src/router.ts
+++ packages/router/src/router.ts
@@ -1,125 +1,119 @@
 import { IHistoryEntry, HistoryBrowser } from './history-browser';
 import { Viewport } from './viewport';
+import { IContainer, inject } from '@aurelia/kernel';
 
 export interface IRoute {
-    name: string;
-    path: string;
-    title?: string;
-    viewports: Object;
-    meta?: Object;
+  name: string;
+  path: string;
+  title?: string;
+  viewports: Object;
+  meta?: Object;
 }
 
 export interface IRouteViewport {
-    name: string;
-    component: any;
+  name: string;
+  component: any;
 }
 
+@inject(IContainer)
 export class Router {
-    public routes: IRoute[] = [];
-    public viewports: Object = {};
+  public routes: IRoute[] = [];
+  public viewports: Object = {};
 
-    private options: any;
-    private isActive: boolean = false;
+  private options: any;
+  private isActive: boolean = false;
 
-    public historyBrowser: HistoryBrowser;
+  public historyBrowser: HistoryBrowser;
 
-    constructor() {
-        this.historyBrowser = new HistoryBrowser();
+  constructor(private container: IContainer) {
+    this.historyBrowser = new HistoryBrowser();
+  }
+
+  public activate(options?: Object): void {
+    if (this.isActive) {
+      throw new Error('Router has already been activated.');
     }
 
-    public activate(options?: Object): void {
-        if (this.isActive) {
-            throw new Error('Router has already been activated.');
+    this.isActive = true;
+    this.options = Object.assign({}, {
+      callback: (entry, flags) => {
+        this.historyCallback(entry, flags);
+      }
+    }, options);
+
+    this.historyBrowser.activate(this.options);
+  }
+
+  public historyCallback(entry: IHistoryEntry, flags: any): Promise<void> {
+    if (this.options.reportCallback) {
+      this.options.reportCallback(entry, flags);
+    }
+
+    const route: IRoute = this.findRoute(entry);
+    if (!route) {
+      return;
+    }
+    if (route.title) {
+      this.historyBrowser.setEntryTitle(route.title);
+    }
+
+    const viewports: Viewport[] = [];
+    for (const vp in route.viewports) {
+      const routeViewport: IRouteViewport = route.viewports[vp];
+      const viewport = this.findViewport(vp);
+      if (viewport.setNextContent(routeViewport.component)) {
+        viewports.push(viewport);
+      }
+    }
+
+    let cancel: boolean = false;
+    return Promise.all(viewports.map((value) => value.canLeave()))
+      .then((promises: boolean[]) => {
+        if (!flags.isCancel && promises.findIndex((value) => value === false) >= 0) {
+          cancel = true;
+          return Promise.resolve([]);
+        } else {
+          return Promise.all(viewports.map((value) => value.canEnter()));
         }
-
-        this.isActive = true;
-        this.options = Object.assign({}, {
-            callback: (entry, flags) => {
-                this.historyCallback(entry, flags);
-            }
-        }, options);
-
-        this.historyBrowser.activate(this.options);
-    }
-
-    public historyCallback(entry: IHistoryEntry, flags: any) {
-        if (this.options.reportCallback) {
-            this.options.reportCallback(entry, flags);
+      }).then((promises: boolean[]) => {
+        if (!flags.isCancel && promises.findIndex((value) => value === false) >= 0) {
+          cancel = true;
+          return Promise.resolve([]);
+        } else {
+          return Promise.all(viewports.map((value) => value.loadContent()));
         }
-
-        const route: IRoute = this.findRoute(entry);
-        if (!route) {
-            return;
+        // No need to check this for false/failures, right?
+        // }).then((promises: boolean[]) => {
+        //   if (promises.findIndex((value) => value === false) >= 0) {
+        //     cancel = true;
+        //   }
+        //   return Promise.resolve();
+      }).then(() => {
+        if (cancel) {
+          this.historyBrowser.cancel();
         }
-        if (route.title) {
-            this.historyBrowser.setEntryTitle(route.title);
-        }
+      });
+  }
 
-        const viewports: Viewport[] = [];
-        for (let vp in route.viewports) {
-            let routeViewport: IRouteViewport = route.viewports[vp];
-            let viewport = this.findViewport(vp);
-            if (viewport.setNextContent(routeViewport.component)) {
-                viewports.push(viewport);
-            }
-        }
+  public findRoute(entry: IHistoryEntry): IRoute {
+    return this.routes.find((value) => value.path === entry.path);
+  }
 
-        let cancel: boolean = false;
-        return Promise.all(viewports.map((value) => value.canEnter())).then((enters: boolean[]) => {
-            if (!flags.isCancel && enters.findIndex((value) => value === false) >= 0) {
-                cancel = true;
-                return Promise.resolve([]);
-            }
-            else {
-                return Promise.all(viewports.map((value) => value.loadContent()));
-            }
-        }).then((loads: boolean[]) => {
-            if (loads.findIndex((value) => value === false) >= 0) {
-                cancel = true;
-                return Promise.resolve([]);
-            }
-            else {
-                return Promise.all(viewports.map((value) => value.canLeave()));
-            }
-        }).then((leaves: boolean[]) => {
-            if (leaves.findIndex((value) => value === false) >= 0) {
-                cancel = true;
-                return Promise.resolve([]);
-            }
-            else {
-                return Promise.all(viewports.map((value) => value.mountContent()));
-            }
-        }).then((mounts: boolean[]) => {
-            if (!flags.isCancel && mounts.findIndex((value) => value === false) >= 0) {
-                cancel = true;
-            }
-            return Promise.resolve(cancel);
-        }).then((cancel) => {
-            if (cancel) {
-                this.historyBrowser.cancel();
-            }
-        });
+  public findViewport(name: string): Viewport {
+    return this.viewports[name];
+  }
+
+  public renderViewports(viewports: Viewport[]): void {
+    for (const viewport of viewports) {
+      viewport.loadContent();
     }
+  }
 
-    public findRoute(entry: IHistoryEntry): IRoute {
-        return this.routes.find((value) => value.path === entry.path);
-    }
+  public addViewport(name: string, element: Element, controller: any): Viewport {
+    return this.viewports[name] = new Viewport(this.container, name, element, controller);
+  }
 
-    public findViewport(name: string): Viewport {
-        return this.viewports[name];
-    }
-
-    public renderViewports(viewports: Viewport[]) {
-        for (let viewport of viewports) {
-            viewport.loadContent();
-        }
-    }
-
-    public addViewport(name: string, controller: any): void {
-        this.viewports[name] = new Viewport(name, controller);
-    }
-
-    public addRoute(route: IRoute) {
-        this.routes.push(route);
-    }
+  public addRoute(route: IRoute): void {
+    this.routes.push(route);
+  }
 }
--- packages/router/src/viewport.ts
+++ packages/router/src/viewport.ts
@@ -1,41 +1,90 @@
+import { Aurelia, ICustomElement, CustomElementResource, IRenderingEngine, LifecycleFlags, INode } from '@aurelia/runtime';
+import { DI, IContainer } from '../../kernel/dist';
+
 export class Viewport {
-    public content: string;
-    public nextContent: string;
+  public content: string;
+  public nextContent: any;
 
-    constructor(public name: string, public controller: any) {
-    }
+  public component: ICustomElement;
+  public nextComponent: ICustomElement;
 
-    public canEnter(): Promise<boolean> {
-        let result = this.controller.canEnter();
-        if (typeof result === 'boolean') {
-            return Promise.resolve(result);
-        }
-        return result;
-    }
-    public canLeave(): Promise<boolean> {
-        let result = this.controller.canLeave();
-        if (typeof result === 'boolean') {
-            return Promise.resolve(result);
-        }
-        return result;
-    }
+  constructor(private container: IContainer, public name: string, public element: Element, public controller: any) {
+  }
 
-    public setNextContent(content: string): boolean {
-        if (this.content === content) {
-            return false;
-        }
-        this.nextContent = content;
-        return true;
+  public setNextContent(content: ICustomElement | string): boolean {
+    if (this.content === content) {
+      return false;
+    }
+    this.nextContent = content;
+    return true;
+  }
+
+  public canLeave(): Promise<boolean> {
+    if (!this.component) {
+      return Promise.resolve(true);
     }
 
-    public loadContent(): Promise<boolean> {
-        console.log('Loading', this.name, this.nextContent);
-        return this.controller.load(this.nextContent);
+    const component: any = this.component;
+    console.log('viewport canLeave', component.canLeave());
+
+    const result = component.canLeave();
+    if (typeof result === 'boolean') {
+      return Promise.resolve(result);
+    }
+    return result;
+  }
+
+  public canEnter(): Promise<boolean> {
+    this.loadComponent(this.nextContent);
+    if (!this.nextComponent) {
+      return Promise.resolve(false);
     }
 
-    public mountContent(): Promise<boolean> {
-        console.log('Mounting', this.name, this.nextContent);
-        this.content = this.nextContent;
-        return this.controller.mount();
+    const component: any = this.nextComponent;
+    console.log('viewport canEnter', component.canEnter());
+    const result = component.canEnter();
+    if (typeof result === 'boolean') {
+      return Promise.resolve(result);
     }
+    return result;
+  }
+
+  public loadContent(): Promise<boolean> {
+    console.log('Viewport loadContent', this.name); //, this.nextContent);
+
+    const host: INode = this.element;
+    const renderingEngine = this.container.get(IRenderingEngine);
+
+    if (this.component) {
+      (<any>this.component).leave();
+      this.component.$detach(LifecycleFlags.fromStopTask);
+      // this.component.$unbind(LifecycleFlags.fromStopTask | LifecycleFlags.fromUnbind);
+    }
+
+    (<any>this.nextComponent).enter();
+    this.nextComponent.$hydrate(renderingEngine, host);
+    this.nextComponent.$bind(LifecycleFlags.fromStartTask | LifecycleFlags.fromBind);
+    this.nextComponent.$attach(LifecycleFlags.fromStartTask, host);
+
+    this.content = this.nextContent;
+    this.component = this.nextComponent;
+
+    return Promise.resolve(true);
+    // return this.controller.loadContent(this.nextContent);
+  }
+
+  // public mountContent(): Promise<boolean> {
+  //   console.log('Mounting', this.name, this.nextContent);
+  //   if (this.component) {
+  //     (<any>this.component).leave();
+  //   }
+  //   this.content = this.nextContent;
+  //   this.component = this.nextComponent;
+  //   return this.controller.mount();
+  // }
+
+  private loadComponent(componentOrName: ICustomElement) {
+    // const host: INode = this.element;
+    this.nextComponent = <any>this.container.get(CustomElementResource.keyFrom((<any>componentOrName).description.name));
+  }
 }

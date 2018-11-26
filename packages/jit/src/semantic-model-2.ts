7b02a6edf10b8c4b829bf67289da5f84fd3c472d
Fred Kleuver <fred@avurad.nl>
2018-11-26 14:33:58 +0100

    chore(semantic-model): remove duplicate code

--- packages/jit/src/semantic-model-2.ts
+++ packages/jit/src/semantic-model-2.ts
@@ -1,7 +1,7 @@
-import { all, DI, Immutable, inject, PLATFORM, Reporter } from '@aurelia/kernel';
+import { DI, Immutable, PLATFORM, Reporter } from '@aurelia/kernel';
 import { AttributeDefinition, BindingType, CustomAttributeResource, CustomElementResource, DOM, IAttr, IBindableDescription, IChildNode, IDocumentFragment, IElement, IExpressionParser, IHTMLElement, IHTMLSlotElement, IHTMLTemplateElement, Interpolation, IResourceDescriptions, IsExpressionOrStatement, ITemplateDefinition, IText, NodeType, TemplateDefinition } from '@aurelia/runtime';
 import { AttrSyntax } from './ast';
-import { IAttributePattern, IAttributePatternHandler, Interpretation, ISyntaxInterpreter } from './attribute-pattern';
+import { IAttributeParser } from './attribute-parser';
 import { BindingCommandResource, IBindingCommand } from './binding-command';
 
 // tslint:disable-next-line:no-any
@@ -126,46 +126,6 @@ export class TemplateFactory {
       input.parentNode.removeChild(input);
     }
     return <IHTMLTemplateElement>input;
-  }
-}
-
-export interface IAttributeParser {
-  parse(name: string, value: string): AttrSyntax;
-}
-
-export const IAttributeParser = DI.createInterface<IAttributeParser>()
-  .withDefault(x => x.singleton(AttributeParser));
-
-@inject(ISyntaxInterpreter, all(IAttributePattern))
-export class AttributeParser implements IAttributeParser {
-  private interpreter: ISyntaxInterpreter;
-  private cache: Record<string, Interpretation>;
-  private patterns: Record<string, IAttributePatternHandler>;
-
-  constructor(interpreter: ISyntaxInterpreter, attrPatterns: IAttributePattern[]) {
-    this.interpreter = interpreter;
-    this.cache = {};
-    const patterns: AttributeParser['patterns'] = this.patterns = {};
-    attrPatterns.forEach(attrPattern => {
-      const defs = attrPattern.$patternDefs;
-      interpreter.add(defs);
-      defs.forEach(def => {
-        patterns[def.pattern] = attrPattern as unknown as IAttributePatternHandler;
-      });
-    });
-  }
-
-  public parse(name: string, value: string): AttrSyntax {
-    let interpretation = this.cache[name];
-    if (interpretation === undefined) {
-      interpretation = this.cache[name] = this.interpreter.interpret(name);
-    }
-    const pattern = interpretation.pattern;
-    if (pattern === null) {
-      return new AttrSyntax(name, value, name, null);
-    } else {
-      return this.patterns[pattern][pattern](name, value, interpretation.parts);
-    }
   }
 }
 

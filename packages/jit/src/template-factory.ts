d5af9eb9247eb598c96fc7b5964cb76d0bd734a7
Fred Kleuver <fred@avurad.nl>
2018-12-09 18:26:44 +0100

    chore(template-factory): add API comments

--- packages/jit/src/template-factory.ts
+++ packages/jit/src/template-factory.ts
@@ -1,9 +1,31 @@
 import { DI } from '@aurelia/kernel';
 import { DOM, IHTMLTemplateElement, INode } from '@aurelia/runtime';
 
+/**
+ * Utility that creates a `HTMLTemplateElement` out of string markup or an existing DOM node.
+ *
+ * It is idempotent in the sense that passing in an existing template element will simply return that template element,
+ * so it is always safe to pass in a node without causing unnecessary DOM parsing or template creation.
+ */
 export interface ITemplateFactory {
+  /**
+   * Create a `HTMLTemplateElement` from a provided html string.
+   *
+   * @param markup A raw html string that may or may not be wrapped in `<template></template>`
+   */
   createTemplate(markup: string): IHTMLTemplateElement;
+  /**
+   * Create a `HTMLTemplateElement` from a provided DOM node. If the node is already a template, it
+   * will be returned as-is (and removed from the DOM).
+   *
+   * @param node A DOM node that may or may not be wrapped in `<template></template>`
+   */
   createTemplate(node: INode): IHTMLTemplateElement;
+  /**
+   * Create a `HTMLTemplateElement` from a provided DOM node or html string.
+   *
+   * @param node A DOM node or raw html string that may or may not be wrapped in `<template></template>`
+   */
   createTemplate(input: string | INode): IHTMLTemplateElement;
   createTemplate(input: string | INode): IHTMLTemplateElement;
 }
@@ -11,6 +33,11 @@ export interface ITemplateFactory {
 export const ITemplateFactory = DI.createInterface<ITemplateFactory>()
   .withDefault(x => x.singleton(TemplateFactory));
 
+/**
+ * Default implementation for `ITemplateFactory` for use in an HTML based runtime.
+ *
+ * @internal
+ */
 export class TemplateFactory {
   private template: IHTMLTemplateElement;
 

71c774ddddb701c47ac416e41c648559195fd487
Fred Kleuver <fred@avurad.nl>
2018-12-10 10:02:59 +0100

    fix(template-factory): correctly handle multiple node scenario

--- packages/jit/src/template-factory.ts
+++ packages/jit/src/template-factory.ts
@@ -52,20 +52,27 @@ export class TemplateFactory {
     if (typeof input === 'string') {
       const template = this.template;
       template.innerHTML = input;
-      const node = template.content.firstElementChild;
-      if (node.nodeName !== 'TEMPLATE') {
+      const node = template.content.firstChild;
+      // if the input is either not wrapped in a template or there is more than one node,
+      // return the whole template that wraps it/them (and create a new one for the next input)
+      if (node.nodeName !== 'TEMPLATE' || node.nextSibling !== null) {
         this.template = DOM.createTemplate();
         return template;
       }
+      // the node to return is both a template and the only node, so return just the node
+      // and clean up the template for the next input
       template.content.removeChild(node);
       return node as IHTMLTemplateElement;
     }
     if (input.nodeName !== 'TEMPLATE') {
+      // if we get one node that is not a template, wrap it in one
       const template = this.template;
       this.template = DOM.createTemplate();
       template.content.appendChild(input);
       return template;
     }
+    // we got a template element, remove it from the DOM if it's present there and don't
+    // do any other processing
     if (input.parentNode !== null) {
       input.parentNode.removeChild(input);
     }

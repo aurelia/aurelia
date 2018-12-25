848881dda2bcb1169530d1f44bf8dcc1b6517a88
Fred Kleuver <fred@avurad.nl>
2018-12-25 10:18:42 +0100

    fix(jit): add missing registrations

--- packages/jit-html/src/configuration.ts
+++ packages/jit-html/src/configuration.ts
@@ -1,12 +1,14 @@
 import { JitConfiguration } from '@aurelia/jit';
 import { DI, IContainer, IRegistry, Registration } from '@aurelia/kernel';
 import { ITemplateCompiler } from '@aurelia/runtime';
+import { HTMLRuntimeConfiguration } from '@aurelia/runtime-html';
 import {
   CaptureBindingCommand,
   DelegateBindingCommand,
   TriggerBindingCommand
 } from './binding-command';
 import { TemplateCompiler } from './template-compiler';
+import { HTMLTemplateFactory, ITemplateFactory } from './template-factory';
 
 export const HTMLBindingLanguage: IRegistry[] = [
   TriggerBindingCommand,
@@ -17,8 +19,10 @@ export const HTMLBindingLanguage: IRegistry[] = [
 export const HTMLJitConfiguration = {
   register(container: IContainer): void {
     container.register(
+      HTMLRuntimeConfiguration,
       JitConfiguration,
       Registration.singleton(ITemplateCompiler, TemplateCompiler),
+      Registration.singleton(ITemplateFactory, HTMLTemplateFactory),
       ...HTMLBindingLanguage
     );
   },
--- packages/jit-html/src/index.ts
+++ packages/jit-html/src/index.ts
@@ -1,4 +1,5 @@
 export * from './binding-command';
+export * from './configuration';
 export * from './debugging';
 export * from './template-binder';
 export * from './template-compiler';

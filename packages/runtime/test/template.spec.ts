fac19f526e073a8c8a6da0c44c17d6665a8a854f
Fred Kleuver <fred@avurad.nl>
2018-12-27 06:13:51 +0100

    test(runtime): fix CompiledTemplate tests

--- packages/runtime/test/template.spec.ts
+++ packages/runtime/test/template.spec.ts
@@ -1,33 +1,30 @@
-import { IContainer, Registration, DI } from '@aurelia/kernel';
-import { MockRenderingEngine } from "../mock";
-import {
-  Renderer,
-  CompiledTemplate,
-  ViewFactory,
-  IViewFactory,
-  IRenderable,
-  ITargetedInstruction,
-  IRenderLocation,
-  INode,
-  DOM
-
-} from '../src/index';
+import { IContainer, Registration } from '@aurelia/kernel';
 import { expect } from 'chai';
-import { createElement } from '../util';
-
-const dom = new DOM(<any>document);
+import {
+  CompiledTemplate,
+  IDOM,
+  INode,
+  IRenderable,
+  IRenderLocation,
+  ITargetedInstruction,
+  IViewFactory,
+  TemplateDefinition,
+  ViewFactory
+} from '../src/index';
+import { AuDOM, AuDOMConfiguration, AuNode, AuNodeSequenceFactory } from './au-dom';
 
 describe(`CompiledTemplate`, () => {
   describe(`constructor`, () => {
     it(`creates a new renderContext and createNodeSequence function`, () => {
-      class Foo{}
-      class Bar{static register(container: IContainer){ container.register(Registration.singleton(Bar, Bar)) }}
-      const def = { template: createElement('<div>foo</div>'), dependencies: [Foo, Bar] };
+      class Foo {}
+      class Bar {public static register(container: IContainer) { container.register(Registration.singleton(Bar, Bar)); }}
+      const container = AuDOMConfiguration.createContainer();
+      const dom = container.get<AuDOM>(IDOM);
+      const templateNode = AuNode.createTemplate().appendChild(AuNode.createText('foo'));
+      const nsFactory = new AuNodeSequenceFactory(dom, templateNode);
+      const def = { template: templateNode, dependencies: [Foo, Bar] } as unknown as TemplateDefinition;
       const viewFactory = new ViewFactory(null, null, null);
-      const renderer = new Renderer([]);
-      const renderingEngine = new MockRenderingEngine(null, viewFactory, renderer, null);
-      const container = DI.createContainer();
-      const sut = new CompiledTemplate(dom, renderingEngine, container as any, def as any);
+      const sut = new CompiledTemplate(dom, def, nsFactory, container as any);
 
       expect(sut.renderContext['parent']).to.equal(container);
 
@@ -38,9 +35,7 @@ describe(`CompiledTemplate`, () => {
       expect(sut.renderContext.has(Foo, false)).to.equal(false);
       expect(sut.renderContext.has(Bar, false)).to.equal(true);
       expect(sut.renderContext.has(INode, false)).to.equal(true);
-      expect(sut.renderContext.has(Element, false)).to.equal(true);
-      expect(sut.renderContext.has(HTMLElement, false)).to.equal(true);
-      expect(sut.renderContext.has(SVGElement, false)).to.equal(true);
+      expect(sut.renderContext.has(AuNode, false)).to.equal(true);
 
       expect(typeof sut.renderContext.render).to.equal('function');
       expect(typeof sut.renderContext.beginComponentOperation).to.equal('function');
@@ -57,19 +52,17 @@ describe(`CompiledTemplate`, () => {
       const parts = {} as any;
       const location = {} as any;
 
-      sut.renderContext.beginComponentOperation(renderable, target, instruction, viewFactory, parts, location);
+      sut.renderContext.beginComponentOperation(renderable, target, instruction, viewFactory as any, parts, location);
 
       expect(() => sut.renderContext.get(IViewFactory)).to.throw(/51/);
       expect(sut.renderContext.get(IRenderable)).to.equal(renderable);
       expect(sut.renderContext.get(INode)).to.equal(target);
-      expect(sut.renderContext.get(Element)).to.equal(target);
-      expect(sut.renderContext.get(HTMLElement)).to.equal(target);
-      expect(sut.renderContext.get(SVGElement)).to.equal(target);
+      expect(sut.renderContext.get(AuNode)).to.equal(target);
       expect(sut.renderContext.get(ITargetedInstruction)).to.equal(instruction);
       expect(sut.renderContext.get(IRenderLocation)).to.equal(location);
 
       const nodes = sut.factory.createNodeSequence();
-      expect(nodes.firstChild['textContent']).to.equal('foo');
-    })
+      expect(nodes.childNodes[0].textContent).to.equal('foo');
+    });
   });
 });

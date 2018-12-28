f6e72ab945712b7560e602002673b59a4d2de464
Fred Kleuver <fred@avurad.nl>
2018-12-28 01:19:54 +0100

    fix(jit-html): correct the iife exports

--- packages/jit-html/src/index.full.ts
+++ packages/jit-html/src/index.full.ts
@@ -1,4 +1,6 @@
+import * as jit from '@aurelia/jit';
 import * as kernel from '@aurelia/kernel';
-import * as runtime from '@aurelia/runtime-html';
-import * as jit from './index';
-export { kernel, runtime, jit };
+import * as runtime from '@aurelia/runtime';
+import * as runtimeHtml from '@aurelia/runtime-html';
+import * as jitHtml from './index';
+export { kernel, runtime, runtimeHtml, jit, jitHtml };

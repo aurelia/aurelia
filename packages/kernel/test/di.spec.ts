ae9c402ef69146c57df7fde39a70410e112da5f5
Fred Kleuver <fred@avurad.nl>
2018-12-31 18:25:48 +0100

    chore(kernel): fix di test

--- packages/kernel/test/di.spec.ts
+++ packages/kernel/test/di.spec.ts
@@ -1051,9 +1051,17 @@ describe(`The Container class`, () => {
   });
 
   describe(`createChild()`, () => {
-    it(`creates a child with same config and sut as parent`, () => {
+    it(`creates a child with same config and sut as parent, and copies over the resourceLookup`, () => {
+      const obj = {};
+      Registration.instance('foo', obj).register(sut, 'foo');
+      expect(sut['resourceLookup'].foo.state).to.equal(obj);
+      expect(sut['configuration'].resourceLookup.foo.state).to.equal(obj);
       const actual = sut.createChild();
-      expect(actual['configuration']).to.equal(sut['configuration']);
+      expect(actual['configuration']).not.to.equal(sut['configuration']);
+      expect(actual['configuration'].factories).to.equal(sut['configuration'].factories);
+      expect(actual['configuration'].resourceLookup).not.to.equal(sut['configuration'].resourceLookup);
+      expect(actual['configuration'].resourceLookup).to.deep.equal(sut['configuration'].resourceLookup);
+      expect(actual['configuration'].resourceLookup.foo.state).to.equal(obj);
       expect(actual['parent']).to.equal(sut);
       expect(sut['parent']).to.equal(null);
     });

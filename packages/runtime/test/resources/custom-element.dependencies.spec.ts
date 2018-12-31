9bb889ddb22649a91935f774dee535e7c1e886b9
Fred Kleuver <fred@avurad.nl>
2018-12-31 17:57:21 +0100

    chore(runtime): fix tests

--- packages/runtime/test/resources/custom-element.dependencies.spec.ts
+++ packages/runtime/test/resources/custom-element.dependencies.spec.ts
@@ -1,23 +1,23 @@
 import { expect } from 'chai';
 import { CustomElementResource } from '../../src/index';
-import { AuDOMConfiguration, AuNode, AuDOM, AuDOMTest } from '../au-dom';
+import { AuDOMTest } from '../au-dom';
 
 describe('CustomElementResource', () => {
   describe(`define`, () => {
-    it(`creates a new class when applied to null`, () => {
-      const { au, container, lifecycle, host } = AuDOMTest.setup();
+    it(`registers local dependencies`, () => {
+      const { au, host } = AuDOMTest.setup();
 
       const fooDef = AuDOMTest.createTextDefinition('msg', 'foo');
-      const Foo = CustomElementResource.define(fooDef, class { public msg = 'asdf'; });
+      const Foo = CustomElementResource.define(fooDef, class { public msg = 'asdf1'; });
 
       //@ts-ignore
       const barDef = AuDOMTest.createElementDefinition([[AuDOMTest.createElementInstruction('foo', [['msg', 'msg']])]], 'bar');
       barDef.dependencies = [Foo];
-      const Bar = CustomElementResource.define(barDef, class { public msg = 'ssss'; });
+      const Bar = CustomElementResource.define(barDef, class { public msg = 'asdf2'; });
 
       au.app({ host, component: new Bar() });
       au.start();
-      expect(host.textContent).to.equal('asdf');
+      expect(host.textContent).to.equal('asdf2');
     });
   });
 });

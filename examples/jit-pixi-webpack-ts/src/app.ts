fb9f709bbd0e60a4bae4fea68553c484fae212a3
Fred Kleuver <fred@avurad.nl>
2019-01-03 05:52:30 +0100

    chore(pixi): add a basic example

--- examples/jit-pixi-webpack-ts/src/app.ts
+++ examples/jit-pixi-webpack-ts/src/app.ts
@@ -1,7 +1,73 @@
-import { customElement } from '@aurelia/runtime';
+import { customElement, ICustomElement } from '@aurelia/runtime';
 import template from './app.html';
+import { LoadResourceTask } from '@aurelia/runtime-pixi';
+
+interface ISprite {
+  src: string;
+  entropy?: number;
+  x?: number;
+  y?: number;
+  anchor?: { x: number; y: number };
+  scale?: { x: number; y: number };
+  width?: number;
+}
+
+export interface App extends ICustomElement<Node> {}
 
 @customElement({ name: 'app', template })
 export class App {
-  message = 'Hello World!';
+  public sprites: ISprite[];
+  public timestamp: number;
+  constructor() {
+    this.timestamp = 0;
+    this.sprites = [];
+  }
+
+  public created(): void {
+    LoadResourceTask
+      .register(this)
+      .add('logo', require('img/aurelia-icon-256x256.png'));
+
+    for (let i = 0; i < 50; ++i) {
+      this.addSprite();
+    }
+  }
+
+  public update({delta}: {delta: number}): void {
+    this.timestamp += delta;
+    let sprite: App['sprites'] extends Array<infer S> ? S : never;
+    const sprites = this.sprites;
+    const len = sprites.length;
+    let entropy: number;
+    let i = 0;
+    for (; i < len; ++i) {
+      sprite = sprites[i];
+      entropy = sprite['entropy'];
+      sprite.x += entropy;
+      if (sprite.x > (640)) {
+        sprite.x = 0 - sprite.width;
+        if ((entropy % 1) > 0.5) {
+          sprite.y += entropy * 25;
+        } else {
+          sprite.y -= entropy * 25;
+        }
+        if (sprite.y > 480) {
+          sprite.y = 0;
+        }
+      }
+    }
+  }
+
+  public addSprite(): void {
+    const scale = 0.1 + 0.75 * Math.random();
+    const sprite = {
+      src: 'logo',
+      entropy: 1 + Math.random() * 5,
+      x: 640 * Math.random(),
+      y: 480 * Math.random(),
+      anchor: { x: 0.5, y: 0.5 },
+      scale: { x: scale, y: scale }
+    };
+    this.sprites.push(sprite);
+  }
 }
--- examples/jit-pixi-webpack-ts/webpack.config.js
+++ examples/jit-pixi-webpack-ts/webpack.config.js
@@ -1,4 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
 
 module.exports = function(env, { mode }) {
   const production = mode === 'production';
@@ -7,21 +9,28 @@ module.exports = function(env, { mode }) {
     entry: './src/startup.ts',
     devtool: production ? 'source-map' : 'eval-source-map',
     resolve: {
-      extensions: ['.ts'],
-      modules: ['src', 'node_modules']
+      extensions: ['.ts', '.js'],
+      modules: [
+        path.resolve(__dirname, 'src'),
+        path.resolve(__dirname, 'node_modules')
+      ]
     },
     devServer: {
       port: 9000,
       historyApiFallback: true,
       open: true,
-      lazy: false
+      lazy: false,
+      contentBase: '/'
     },
     module: {
       rules: [
         { test: /\.ts$/i, loader: 'ts-loader' },
-        { test: /\.html$/i, loader: 'html-loader' }
+        { test: /\.html$/i, loader: 'html-loader' },
+        { test: /\.(jpe?g|png|gif|svg)$/i, loader: 'file-loader' }
       ]
     },
-    plugins: [new HtmlWebpackPlugin({ template: 'index.ejs' })]
+    plugins: [
+      new HtmlWebpackPlugin({ template: 'index.ejs' })
+    ]
   }
 }

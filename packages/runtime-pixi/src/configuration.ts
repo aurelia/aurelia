9a53ba84f72e0d3b57fab1bc5950a8a27929601f
Fred Kleuver <fred@avurad.nl>
2019-01-03 05:48:49 +0100

    feat(runtime-pixi): simple initial implementation for renderer and sprite

--- packages/runtime-pixi/src/configuration.ts
+++ packages/runtime-pixi/src/configuration.ts
@@ -1,10 +1,18 @@
-import { DI, IContainer } from '@aurelia/kernel';
+import { DI, IContainer, IRegistry } from '@aurelia/kernel';
 import { HTMLRuntimeConfiguration } from '@aurelia/runtime-html';
+import { PixiApp } from './resources/custom-elements/pixi-app';
+import { PixiSprite } from './resources/custom-elements/pixi-sprite';
+
+export const PixiGlobalResources: IRegistry[] = [
+  PixiApp,
+  PixiSprite
+];
 
 export const PixiRuntimeConfiguration = {
   register(container: IContainer): void {
     container.register(
-      HTMLRuntimeConfiguration
+      HTMLRuntimeConfiguration,
+      ...PixiGlobalResources
     );
   },
   createContainer(): IContainer {
--- packages/runtime-pixi/src/index.ts
+++ packages/runtime-pixi/src/index.ts
@@ -1,3 +1,13 @@
 export {
+  PixiGlobalResources,
   PixiRuntimeConfiguration
 } from './configuration';
+export {
+  LoadResourceTask
+} from './load-resource-task';
+export {
+  PixiApp
+} from './resources/custom-elements/pixi-app';
+export {
+  PixiSprite
+} from './resources/custom-elements/pixi-sprite';

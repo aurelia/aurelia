28dd8bab0655753a00f28c167b48b131a9066606
Fred Kleuver <fred@avurad.nl>
2019-02-03 18:39:18 +0100

    chore(router): cleanup tests a bit

---
+++ packages/router/test/util.ts
@@ -0,0 +1,17 @@
+import {
+  IContainer,
+  Registration,
+  Tracer
+} from '@aurelia/kernel';
+import { expect } from 'chai';
+import { spy } from 'sinon';
+import {
+  eachCartesianJoin
+} from '../../../scripts/test-lib';
+import {
+  Tracer as DebugTracer
+} from '../../debug/src/tracer';
+
+export {
+  eachCartesianJoin
+};

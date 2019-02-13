38b94e14f21b9590e37e8dceac81e062177c7ebb
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-02-13 12:41:35 +1000

    fix(e2e): local test app now working

--- test/cypress/app/src/startup.ts
+++ test/cypress/app/src/startup.ts
@@ -1,4 +1,3 @@
-import { Registration } from '@aurelia/kernel';
 import { NavCustomElement, ViewportCustomElement } from '@aurelia/router';
 import { DebugConfiguration } from '@aurelia/debug';
 import { BasicConfiguration } from '@aurelia/jit-html-browser';
@@ -13,8 +12,7 @@ const container = BasicConfiguration.createContainer();
 
 container.register(
   ViewportCustomElement as any,
-  NavCustomElement as any,
-  App as any
+  NavCustomElement as any
 );
 
 registerComponent(
@@ -22,7 +20,7 @@ registerComponent(
   RouterHome as any
 );
 
-window['au'] = new Aurelia()
+window['au'] = new Aurelia(container)
   .register(DebugConfiguration)
   .app({ host: document.querySelector('app'), component: App })
   .start();

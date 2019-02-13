11eb956c99f7059becea6a61bf5cd40a1a517c2e
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-02-13 12:14:43 +1000

    fix(e2e): router import and utils from jurgen demo

--- test/cypress/app/src/startup.ts
+++ test/cypress/app/src/startup.ts
@@ -1,9 +1,28 @@
+import { Registration } from '@aurelia/kernel';
+import { NavCustomElement, ViewportCustomElement } from '@aurelia/router';
 import { DebugConfiguration } from '@aurelia/debug';
 import { BasicConfiguration } from '@aurelia/jit-html-browser';
 import { Aurelia } from '@aurelia/runtime';
+
 import { App } from './app';
 
+import { HomeRoute } from './components/router/home';
+import { registerComponent } from './utils';
+
+const container = BasicConfiguration.createContainer();
+
+container.register(
+  ViewportCustomElement as any,
+  NavCustomElement as any,
+  App as any
+);
+
+registerComponent(
+  container,
+  HomeRoute as any
+);
+
 window['au'] = new Aurelia()
-  .register(BasicConfiguration, DebugConfiguration)
-  .app({ host: document.querySelector('app'), component: new App() })
+  .register(DebugConfiguration)
+  .app({ host: document.querySelector('app'), component: App })
   .start();

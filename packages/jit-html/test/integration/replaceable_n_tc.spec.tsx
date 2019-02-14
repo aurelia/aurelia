84f76e2e3254f341b42211e58a8a06fdcf5c1903
bigopon.777@gmail.com <bigopon.777@gmail.com>
2019-02-15 08:16:53 +1100

    chore(tests): more different usage tests

--- packages/jit-html/test/integration/replaceable_n_tc.spec.tsx
+++ packages/jit-html/test/integration/replaceable_n_tc.spec.tsx
@@ -16,7 +16,7 @@ describe('replaceable', function () {
           [
             [
               '[repeat]',
-              '  [replaceable #0] << replace this'
+              '  [replaceable #0] << replace #0'
             ].join('\n'),
             <div repeat$for="item of items">
               <div replaceable part="p0">{'${item.name}'}</div>
@@ -27,10 +27,22 @@ describe('replaceable', function () {
             createItems(2),
             `replacement of 0-item-0.replacement of 1-item-1`
           ],
+          // Same with previous, though [repeat] + [replaceable] are on same element
+          [
+            [
+              '[repeat] [replaceable #0] << replace #0'
+            ].join('\n'),
+            <div repeat$for="item of items" replaceable part="p0">{'${item.name}'}</div>,
+            <foo>
+              <template replace-part="p0">replacement of {'${item.idx}-${item.name}.'}</template>
+            </foo>,
+            createItems(2),
+            `replacement of 0-item-0.replacement of 1-item-1`
+          ],
           [
             [
               '[repeat]',
-              '  [replaceable #0] << replace this',
+              '  [replaceable #0] << replace #0',
               '    [replaceable #1]'
             ].join('\n'),
             <div repeat$for="item of items">
@@ -43,13 +55,29 @@ describe('replaceable', function () {
               <template replace-part="p0">replacement of {'${item.idx}-${item.name}.'}</template>
             </foo>,
             createItems(2),
-            createExpectedReplacementText(2)
+            `replacement of 0-item-0. replacement of 1-item-1.`
+          ],
+          // Same with previous, though [repeat] + [replaceable] are on same element
+          [
+            [
+              '[repeat] [replaceable #0] << replace #0',
+              '  [replaceable #1]'
+            ].join('\n'),
+            <div repeat$for="item of items" replaceable part="p0">
+              {'${item.name}'}
+              <div replaceable part="p1">{'${item.name}'}</div>
+            </div>,
+            <foo>
+              <template replace-part="p0">replacement of {'${item.idx}-${item.name}.'}</template>
+            </foo>,
+            createItems(2),
+            `replacement of 0-item-0.replacement of 1-item-1.`
           ],
           [
             [
               '[repeat]',
               '  [replaceable #0]',
-              '    [replaceable #1] << replace this'
+              '    [replaceable #1] << replace #1'
             ].join('\n'),
             <div repeat$for="item of items">
               <div replaceable part="p0">
@@ -67,26 +95,42 @@ describe('replaceable', function () {
             [
               '[replaceable #0]',
               '  [repeat]',
-              '    [replaceable #1] << replace this'
+              '    [replaceable #1] << replace #1'
             ].join('\n'),
             <div replaceable part="p0">
-              item-0
+              item-0.
               <div repeat$for="item of items">
                 <div replaceable part="p1">{'${item.name}'}</div>
               </div>
             </div>,
             <foo>
-              <template replace-part="p1">{'${item.idx}-${item.name}.'}</template>
+              <template replace-part="p1">replacement of {'${item.idx}-${item.name}.'}</template>
             </foo>,
             createItems(2),
-            'item-0replacement of 0-item-0.replacement of 1-item-1.'
+            'item-0.replacement of 0-item-0.replacement of 1-item-1.'
+          ],
+          [
+            [
+              '[replaceable #0]',
+              '  [repeat]',
+              '    [replaceable #1] << replace #1'
+            ].join('\n'),
+            <div replaceable part="p0">
+              item-0.
+              <div repeat$for="item of items" replaceable part="p1">{'${item.name}'}</div>
+            </div>,
+            <foo>
+              <template replace-part="p1">replacement of {'${item.idx}-${item.name}.'}</template>
+            </foo>,
+            createItems(2),
+            'item-0.replacement of 0-item-0.replacement of 1-item-1.'
           ],
           [
             [
               '[repeat]',
-              '  [replaceable #0] << replace this',
+              '  [replaceable #0] << replace #0',
               '[repeat]',
-              '  [replaceable #0] << replace this'
+              '  [replaceable #0] << replace #0'
             ].join('\n'),
             <div>
               <div repeat$for="item of items">
@@ -100,15 +144,31 @@ describe('replaceable', function () {
               <template replace-part="p0">replacement of {'${item.idx}-${item.name}.'}</template>
             </foo>,
             createItems(2),
-            'replacement of 0-item0.replacement of 1-item-1.replacement of 0-item-0.replacement of 1-item-1'
+            'replacement of 0-item-0.replacement of 1-item-1.replacement of 0-item-0.replacement of 1-item-1.'
+          ],
+          // Same with previous. Though [repeat] + [replaceable] are on same element
+          [
+            [
+              '[repeat] [replaceable #0] << replace #0',
+              '[repeat] [replaceable #0] << replace #0'
+            ].join('\n'),
+            <div>
+              <div repeat$for="item of items" replaceable part="p0">{'${item.name}'}</div>
+              <div repeat$for="item of items" replaceable part="p0">{'${item.name}'}</div>
+            </div>,
+            <foo>
+              <template replace-part="p0">replacement of {'${item.idx}-${item.name}.'}</template>
+            </foo>,
+            createItems(2),
+            'replacement of 0-item-0.replacement of 1-item-1.replacement of 0-item-0.replacement of 1-item-1.'
           ],
           [
             [
               '[repeat]',
               '  [replaceable #0]',
-              '    [replaceable #1] << replace this',
+              '    [replaceable #1] << replace #1',
               '  [replaceable #0]',
-              '    [replaceable #1] << replace this'
+              '    [replaceable #1] << replace #1'
             ].join('\n'),
             <div repeat$for="item of items">
               <div replaceable part="p0">
@@ -129,7 +189,7 @@ describe('replaceable', function () {
           [
             [
               '[repeat]',
-              '  [replaceable #0] << replace this',
+              '  [replaceable #0] << replace #0',
               '🔻',
               '[foo]',
               '  [${}] <-- by interpolation binding from consumer'
@@ -150,12 +210,36 @@ describe('replaceable', function () {
                 .equal('0-item-0. Message: Hello world from Aurelia.1-item-1. Message: Hello world from Aurelia');
             }
           ],
+          // Same with previous. Though [repeat] + [replaceable] are on same element
+          [
+            [
+              '[repeat] [replaceable #0] << replace #0',
+              '🔻',
+              '[foo]',
+              '  [${}] <-- by interpolation binding from consumer'
+            ].join('\n'),
+            <div repeat$for="item of items" replaceable part="p0">{'${item.name}'}</div>,
+            <foo>
+              <template replace-part="p0">{'${item.idx}-${item.name}. Message: ${message}.'}</template>
+            </foo>,
+            createItems(2),
+            `0-item-0. Message: Aurelia.1-item-1 Message: Aurelia`,
+            async (host, app, foo) => {
+              app.message = 'Hello world from Aurelia';
+              await Promise.resolve();
+              expect(host.textContent, 'host.textContent@changed')
+                .to
+                .equal('0-item-0. Message: Hello world from Aurelia.1-item-1. Message: Hello world from Aurelia');
+            }
+          ],
           [
             [
               '[repeat]',
-              '  [replaceable #0] << replace this',
+              '  [replaceable #0] << replace #0',
               '🔻',
               '[foo]',
+              // note that we are not repeating this template, as it seems to be incorrect usage
+              // if there are two replace-part aiming for 1 replaceable, which should be the right one?
               '  [template r#0]',
               '    [repeat] <-- by a repeat'
             ].join('\n'),
@@ -168,7 +252,75 @@ describe('replaceable', function () {
               </template>
             </foo>,
             createItems(2),
-            `0-item-0.1-item-1.0-item-0.1-item-1.`
+            `0-item-0.1-item-1.0-item-0.1-item-1.`,
+            async (host, app, foo) => {
+              foo.items = createItems(3, 'ITEM');
+              await Promise.resolve();
+              expect(host.textContent, 'host.textContent@changed')
+                .to
+                .equal(`0-ITEM-0.1-ITEM-1.2-ITEM-2.`.repeat(3));
+              
+              foo.items = [];
+              await Promise.resolve();
+              expect(host.textContent, 'host.textContent@[]').to.equal('');
+
+              foo.items.push(...createItems(1));
+              await Promise.resolve();
+              expect(host.textContent, 'host.textContent@[1]').to.equal('0-item-0.');
+
+              foo.items.push(...createItems(2).slice(1));
+              await Promise.resolve();
+              expect(host.textContent, 'host.textContent@[1]').to.equal('0-item-0.1-item-1.');
+
+              foo.items.sort((i1, i2) => i1.idx > i2.idx ? -1 : 1);
+              await Promise.resolve();
+              expect(host.textContent, 'host.textContent@[0🔁1]').to.equal('1-item-1.0-item-0.');
+            }
+          ],
+          // Same with previous. Though [repeat] + [replaceable] are on same element
+          [
+            [
+              '[repeat] [replaceable #0] << replace #0',
+              '🔻',
+              '[foo]',
+              // note that we are not repeating this template, as it seems to be incorrect usage
+              // if there are two replace-part aiming for 1 replaceable, which should be the right one?
+              '  [template r#0]',
+              '    [repeat] <-- by a repeat'
+            ].join('\n'),
+            <div repeat$for="item of items">
+              <div replaceable part="p0">{'${item.name}'}</div>
+            </div>,
+            <foo>
+              <template replace-part="p0">
+                <template repeat$for="item of items">{'${item.idx}-${item.name}.'}</template>
+              </template>
+            </foo>,
+            createItems(2),
+            `0-item-0.1-item-1.0-item-0.1-item-1.`,
+            async (host, app, foo) => {
+              foo.items = createItems(3, 'ITEM');
+              await Promise.resolve();
+              expect(host.textContent, 'host.textContent@changed')
+                .to
+                .equal(`0-ITEM-0.1-ITEM-1.2-ITEM-2.`.repeat(3));
+              
+              foo.items = [];
+              await Promise.resolve();
+              expect(host.textContent, 'host.textContent@[]').to.equal('');
+
+              foo.items.push(...createItems(1));
+              await Promise.resolve();
+              expect(host.textContent, 'host.textContent@[1]').to.equal('0-item-0.');
+
+              foo.items.push(...createItems(2).slice(1));
+              await Promise.resolve();
+              expect(host.textContent, 'host.textContent@[1]').to.equal('0-item-0.1-item-1.');
+
+              foo.items.sort((i1, i2) => i1.idx > i2.idx ? -1 : 1);
+              await Promise.resolve();
+              expect(host.textContent, 'host.textContent@[0🔁1]').to.equal('1-item-1.0-item-0.');
+            }
           ]
         ];
         for (

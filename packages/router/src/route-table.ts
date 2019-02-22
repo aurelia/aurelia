b3d0ba7c77dca574199ff80fcd24e8bd6601540d
Jürgen Wenzel <jurgen@jwenzel.se>
2019-02-22 10:15:17 +0100

    refactor(router): remove example in route table

--- packages/router/src/route-table.ts
+++ packages/router/src/route-table.ts
@@ -14,9 +14,7 @@ export class RouteTable {
    * @returns The viewport instructions for a found route or the route if not found.
    */
   public transformFromUrl = (route: string, router: Router): string | ViewportInstruction[] => {
-    if (route === 'welcome' || route === '/welcome') {
-      return [new ViewportInstruction('books'), new ViewportInstruction('about')];
-    }
+    // TODO: Implement route recognizing to transform a configured route to a set of viewport instructions
     return route;
   }
 
@@ -28,26 +26,7 @@ export class RouteTable {
    * @returns The route for a found set of viewport instructions or the viewport instructions if not found.
    */
   public transformToUrl = (instructions: ViewportInstruction[], router: Router): string | ViewportInstruction[] => {
-    const components = router.instructionResolver.viewportInstructionsFromString('books+about');
-    if (this.matchViewportInstructions(instructions, components)) {
-      return 'welcome';
-    }
+    // TODO: Implement mapping from set of viewport instructions to a configured route
     return instructions;
-  }
-
-  // Check if instruction sets match. NOT COMPLETE CODE: it should also check viewport
-  // and, possibly, parameters (or their configuration)
-  private matchViewportInstructions(instructionsA: ViewportInstruction[], instructionsB: ViewportInstruction[]): boolean {
-    for (const instruction of instructionsA) {
-      if (!instructionsB.find((i) => i.sameComponent(instruction))) {
-        return false;
-      }
-    }
-    for (const instruction of instructionsB) {
-      if (!instructionsA.find((i) => i.sameComponent(instruction))) {
-        return false;
-      }
-    }
-    return true;
   }
 }

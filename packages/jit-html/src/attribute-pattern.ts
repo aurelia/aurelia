66e303524cffd5e5145714e35caf07773bc3d19f
bigopon.777@gmail.com <bigopon.777@gmail.com>
2019-03-03 15:39:19 +1100

    feat(attr-syntax): attr,style, class pattern

---
+++ packages/jit-html/src/attribute-pattern.ts
@@ -0,0 +1,51 @@
+import { attributePattern, AttrSyntax, IAttributePattern } from '@aurelia/jit';
+import { IRegistry } from '@aurelia/kernel';
+
+export interface AttrPattern extends IAttributePattern {}
+export class AttrPattern implements AttrPattern {
+  public static register: IRegistry['register'];
+
+  public ['attr.PART'](rawName: string, rawValue: string, parts: string[]): AttrSyntax {
+    return new AttrSyntax(rawName, rawValue, parts[1], 'attr');
+  }
+
+  public ['PART.attr'](rawName: string, rawValue: string, parts: string[]): AttrSyntax {
+    return new AttrSyntax(rawName, rawValue, parts[0], 'attr');
+  }
+}
+
+export interface StyleAttributePattern extends IAttributePattern {}
+export class StyleAttributePattern implements StyleAttributePattern {
+  public static register: IRegistry['register'];
+
+  public ['style.PART'](rawName: string, rawValue: string, parts: string[]): AttrSyntax {
+    return new AttrSyntax(rawName, rawValue, parts[1], 'style');
+  }
+
+  public ['PART.style'](rawName: string, rawValue: string, parts: string[]): AttrSyntax {
+    return new AttrSyntax(rawName, rawValue, parts[0], 'style');
+  }
+}
+
+attributePattern(
+  { pattern: 'style.PART', symbols: '.' },
+  { pattern: 'PART.style', symbols: '.' }
+)(StyleAttributePattern);
+
+export interface ClassAttributePattern extends IAttributePattern {}
+export class ClassAttributePattern implements ClassAttributePattern {
+  public static register: IRegistry['register'];
+
+  public ['class.PART'](rawName: string, rawValue: string, parts: string[]): AttrSyntax {
+    return new AttrSyntax(rawName, rawValue, parts[1], 'class');
+  }
+
+  public ['PART.class'](rawName: string, rawValue: string, parts: string[]): AttrSyntax {
+    return new AttrSyntax(rawName, rawValue, parts[0], 'class');
+  }
+}
+
+attributePattern(
+  { pattern: 'class.PART', symbols: '.' },
+  { pattern: 'PART.class', symbols: '.' }
+)(ClassAttributePattern);

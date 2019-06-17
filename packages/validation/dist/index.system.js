40566a28e9710d4110460ad0de0f123d632c2fd6
Fred Kleuver <fred@avurad.nl>
2019-06-17 17:13:34 +0200

    chore(all): add latest build artifacts

---
+++ packages/validation/dist/index.system.js
@@ -0,0 +1,11 @@
+System.register('validation', [], function (exports) {
+	'use strict';
+	return {
+		execute: function () {
+
+			const placeholder = exports('placeholder', null);
+
+		}
+	};
+});
+//# sourceMappingURL=index.system.js.map

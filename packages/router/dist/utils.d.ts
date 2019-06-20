d425dd4735b0a28a84826a74fe0926f3718ce61e
AureliaEffect <aurelia@bluespire.com>
2019-06-20 17:00:06 +0000

    chore(all): add latest build artifacts

--- packages/router/dist/utils.d.ts
+++ packages/router/dist/utils.d.ts
@@ -1,3 +1,4 @@
 import { CustomElementHost } from '@aurelia/runtime';
 export declare function closestCustomElement(element: CustomElementHost<Element>): CustomElementHost;
+export declare function arrayRemove<T>(arr: T[], func: (value: T, index?: number, obj?: T[]) => boolean): T[];
 //# sourceMappingURL=utils.d.ts.map

3b8fbf6a0b347692ebc8f9c51a650c7533103d93
Fred Kleuver <fred@avurad.nl>
2019-06-24 16:47:39 +0200

    ci: fix tests and clean_merge_commit_dist

---
+++ scripts/change-package-refs.ts
@@ -0,0 +1,39 @@
+import { c, createLogger } from './logger';
+import { loadPackageJson, savePackageJson } from './package.json';
+import project from './project';
+
+const log = createLogger('change-package-refs');
+
+const refs = {
+  dev: {
+    'main': 'dist/esnext/index.js',
+    'module': 'dist/esnext/index.js',
+    'jsnext:main': 'dist/esnext/index.js',
+    'browser': 'dist/esnext/index.js'
+  },
+  release: {
+    'main': 'dist/umd/index.js',
+    'module': 'dist/esnext/index.js',
+    'jsnext:main': 'dist/esnext/index.js',
+    'browser': 'dist/esnext/index.js'
+  }
+};
+
+const fields = ['main', 'module', 'jsnext:main', 'browser'];
+
+async function run(): Promise<void> {
+  const ref = process.argv.slice(2)[0];
+
+  for (const { name } of project.packages) {
+    log(`changing package.json fields to ${ref} for: ${c.magentaBright(name.npm)}`);
+    const pkg = await loadPackageJson('packages', name.kebab);
+    for (const field of fields) {
+      pkg[field] = refs[ref][field];
+    }
+    await savePackageJson(pkg, 'packages', name.kebab);
+  }
+}
+
+run().then(() => {
+  log(`Done.`);
+});

27a0571477b82dfe23a37f56d9b918853c5b1525
Sayan751 <sayan751@gmail.com>
2019-07-20 18:54:11 +0200

    chore(i18n): code cleanup

--- packages/i18n/src/configuration.ts
+++ packages/i18n/src/configuration.ts
@@ -1,27 +1,22 @@
-import { IContainer, IRegistry, Registration } from '@aurelia/kernel';
-import i18next from 'i18next';
+import { IContainer, Registration } from '@aurelia/kernel';
 import { I18nConfigurationOptions } from './i18n-configuration-options';
 import { TCustomAttribute } from './t-custom-attribute';
 
 export type I18NConfigOptionsProvider = () => I18nConfigurationOptions;
 
-// const TCustomAttributeRegistration = TCustomAttribute as IRegistry;
+function createI18nConfiguration(optionsProvider: I18NConfigOptionsProvider) {
+  return {
+    optionsProvider,
+    register(container: IContainer) {
+      return container.register(
+        TCustomAttribute,
+        Registration.callback(I18nConfiguration, this.optionsProvider),
+      );
+    },
+    customize(cb?: I18NConfigOptionsProvider) {
+      return createI18nConfiguration(cb || optionsProvider);
+    },
+  };
+}
 
-const i18nConfiguration: IRegistry & { optionsProvider: I18NConfigOptionsProvider } = {
-  optionsProvider: () => Object.create(null),
-  register(container: IContainer): IContainer {
-    container.register(TCustomAttribute as IRegistry);
-    Registration.callback(I18nConfiguration, this.optionsProvider).register(container);
-    return container;
-  }
-};
-
-export const I18nConfiguration: IRegistry & { customize(cb: I18NConfigOptionsProvider): IRegistry } = {
-  customize(cb?: () => I18nConfigurationOptions) {
-    i18nConfiguration.optionsProvider = cb ? cb : i18nConfiguration.optionsProvider;
-    return {
-      ...i18nConfiguration,
-    };
-  },
-  register: i18nConfiguration.register.bind(i18nConfiguration)
-};
+export const I18nConfiguration = createI18nConfiguration(() => Object.create(null));
--- packages/i18n/src/t-custom-attribute.ts
+++ packages/i18n/src/t-custom-attribute.ts
@@ -21,17 +21,7 @@ import { I18N } from './i18n';
  * @export
  */
 export class TCustomAttribute {
-  // public get value(): string {
-  //     return this._value;
-  // }
-  // public set value(newValue: string) {
-  //     const oldValue = this._value;
-  //     if (oldValue !== newValue) {
-  //         this._value = newValue;
-  //         this.valueChanged(newValue, oldValue, this.$controller.flags);
-  //     }
-  // }
-  public static readonly inject: readonly Key[] = [Element, /* IRenderLocation, */ I18N];
+  public static readonly inject: readonly Key[] = [Element, I18N];
   public static readonly kind: ICustomAttributeResource = CustomAttribute;
   public static readonly description: Required<IAttributeDefinition> = Object.freeze({
     name: 't',
@@ -56,25 +46,8 @@ export class TCustomAttribute {
     container.register(Registration.transient(this, this));
   }
 
-  public valueChanged(newValue: string, oldValue: string) {
-    console.log(`custom-attribute:t: value changed from ${oldValue} to ${newValue}`);
-  }
-
-  public binding(flags: LifecycleFlags) {
-    console.log(`custom-attribute:t binding ${this.value} ${flags}`);
-  }
-
   public attaching(flags: LifecycleFlags) {
-    console.log(`custom-attribute:t attaching ${this.value} ${flags}`);
-    console.log(this.element);
     this.i18n.updateValue(this.element, this.value, undefined);
   }
 
-  public detaching(flags: LifecycleFlags) {
-    console.log(`custom-attribute:t detaching ${this.value} ${flags}`);
-  }
-
-  public unbinding(flags: LifecycleFlags) {
-    console.log(`custom-attribute:t unbinding ${this.value} ${flags}`);
-  }
 }

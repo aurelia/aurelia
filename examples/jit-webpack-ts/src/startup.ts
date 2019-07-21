e8c4d05cd36d96030539678a6284e9cd9a96873b
Sayan751 <sayan751@gmail.com>
2019-07-21 19:23:13 +0200

    chore(example): clean up in favor of e2e

--- examples/jit-webpack-ts/src/startup.ts
+++ examples/jit-webpack-ts/src/startup.ts
@@ -1,27 +1,9 @@
 import { DebugConfiguration } from '@aurelia/debug';
-import { I18nConfiguration } from "@aurelia/i18n";
 import { BasicConfiguration } from '@aurelia/jit-html-browser';
-import { IRegistry } from '@aurelia/kernel';
 import { Aurelia } from '@aurelia/runtime';
 import { App } from './app';
 
-const au = new Aurelia()
-  .register(BasicConfiguration, DebugConfiguration,
-    I18nConfiguration
-      .customize(() => ({
-        resources: {
-          en: { translation: { whatever: "works" } }
-        }
-      })) as IRegistry
-  );
-console.log("registered");
-try {
-  au.app({ host: document.querySelector('app'), component: new App() });
-  console.log("starting");
-} catch (e) {
-  console.log(e);
-  throw e;
-}
-au.start();
-
-  // window['au'] = au;
+window['au'] = new Aurelia()
+  .register(BasicConfiguration, DebugConfiguration)
+  .app({ host: document.querySelector('app'), component: new App() })
+  .start();
--- examples/jit-webpack-ts/webpack.config.js
+++ examples/jit-webpack-ts/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = function(env, { mode }) {
   return {
     mode: production ? 'production' : 'development',
     entry: './src/startup.ts',
-    devtool: "inline-cheap-module-source-map",
+    devtool: false,
     resolve: {
       extensions: ['.ts', '.js'],
       modules: ['src', 'node_modules'],

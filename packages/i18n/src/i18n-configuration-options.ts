1ef1a9ebc019a45efa80d5dc524d8d458964f2cd
Sayan751 <sayan751@gmail.com>
2019-07-24 20:25:18 +0200

    chore(i18n): used type def for i18next plugins

--- packages/i18n/src/i18n-configuration-options.ts
+++ packages/i18n/src/i18n-configuration-options.ts
@@ -1,14 +1,20 @@
 import { DI } from '@aurelia/kernel';
 import i18next from 'i18next';
 
+export type I18nModule =
+  | i18next.BackendModule
+  | i18next.LanguageDetectorModule
+  | i18next.LanguageDetectorAsyncModule
+  | i18next.PostProcessorModule
+  | i18next.I18nFormatModule
+  | i18next.ThirdPartyModule;
+
 export const I18nConfigurationOptions = DI.createInterface<I18nConfigurationOptions>('I18nConfigurationOptions');
 export interface I18nConfigurationOptions extends i18next.InitOptions {
   /**
    * Collection of i18next plugins to use.
    */
-  // TODO: Have some basic duck typings for the i18next plugins. These come in various shapes and i18next does not provide any type definition for these plugins.
-  // tslint:disable-next-line: no-any
-  plugins?: any[];
+  plugins?: I18nModule[];
   attributes?: string[];
   skipTranslationOnMissingKey?: boolean;
 }

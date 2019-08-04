7b73409c7f972b725b2190d090952f4c1d77e116
Jürgen Wenzel <jurgen@jwenzel.se>
2019-08-05 00:42:10 +0200

    feat(router): add separator to nav

--- packages/router/src/resources/nav.ts
+++ packages/router/src/resources/nav.ts
@@ -8,6 +8,7 @@ export interface INavClasses {
   ul?: string;
   li?: string;
   a?: string;
+  span?: string;
 
   ulActive?: string;
   liActive?: string;
@@ -25,7 +26,8 @@ export interface INavClasses {
     <li repeat.for="route of routes" if.bind="route.visible" class="\${route.active ? classes.liActive : ''} \${route.hasChildren} \${classes.li}">
       <a if.bind="route.link && route.link.length" href="\${route.link}" class="\${route.active ? classes.aActive : ''} \${classes.a}" innerhtml.bind="route.title"></a>
       <a if.bind="route.execute" click.trigger="route.executeAction($event)" href="" class="\${route.active ? classes.aActive : ''} \${classes.a}" innerhtml.bind="route.title"></a>
-      <a if.bind="(!route.link || !route.link.length) && !route.execute" click.delegate="route.toggleActive()" href="" class="\${route.active ? classes.aActive : ''} \${classes.a}" innerhtml.bind="route.title"></a>
+      <span if.bind="(!route.link || !route.link.length) && !route.execute && !route.children" class="\${route.active ? classes.aActive : ''} \${classes.span} nav-separator" innerhtml.bind="route.title"></span>
+      <a if.bind="(!route.link || !route.link.length) && !route.execute && route.children" click.delegate="route.toggleActive()" href="" class="\${route.active ? classes.aActive : ''} \${classes.a}" innerhtml.bind="route.title"></a>
       <au-nav if.bind="route.children" routes.bind="route.children" level.bind="level + 1" classes.bind="classes" containerless></au-nav>
     </li>
   </ul>

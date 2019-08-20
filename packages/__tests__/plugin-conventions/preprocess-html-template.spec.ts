9b8e7f1b6a9b48db07784823817cf650004fe223
Chunpeng Huo <huocp@me.com>
2019-08-20 15:44:43 +1000

    test(plugin-conventions): test coverage on per-file shadowDOM mode

--- packages/__tests__/plugin-conventions/preprocess-html-template.spec.ts
+++ packages/__tests__/plugin-conventions/preprocess-html-template.spec.ts
@@ -119,6 +119,31 @@ export function getHTMLOnlyElement() {
     assert.equal(result.code, expected);
   });
 
+  it('processes template with css dependencies in per-file shadowDOM mode with string module wrap and explicit shadow mode', function () {
+    const html = '<import from="./hello-world.html"><use-shadow-dom><template><import from="foo"><require from="./foo-bar.scss"></require></template>';
+    const expected = `import { CustomElement } from '@aurelia/runtime';
+import * as h0 from "./hello-world.html";
+const d0 = h0.getHTMLOnlyElement();
+import * as d1 from "foo";
+import { Registration } from '@aurelia/kernel';
+import d2 from "raw-loader!./foo-bar.scss";
+export const name = "foo-bar";
+export const template = "<template></template>";
+export default template;
+export const dependencies = [ d0, d1, Registration.defer('.css', d2) ];
+export const shadowOptions = { mode: 'open' };
+let _e;
+export function getHTMLOnlyElement() {
+  if (!_e) {
+    _e = CustomElement.define({ name, template, dependencies, shadowOptions });
+  }
+  return _e;
+}
+`;
+    const result = preprocessHtmlTemplate('lo\\FooBar.html', html, undefined, id => `raw-loader!${id}`);
+    assert.equal(result.code, expected);
+  });
+
   it('turn off shadowDOM mode for one word element', function () {
     const html = '<import from="./hello-world.html"><use-shadow-dom><template><import from="foo"><require from="./foo-bar.scss"></require></template>';
     const expected = `import { CustomElement } from '@aurelia/runtime';

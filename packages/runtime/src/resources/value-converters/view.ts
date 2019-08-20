cee0f1b1eb024c15fdb98b5b154c010ba546f540
Rob Eisenberg <EisenbergEffect@users.noreply.github.com>
2019-08-20 12:53:47 -0700

    Update packages/runtime/src/resources/value-converters/view.ts
    Co-Authored-By: Fred Kleuver <fkleuver@users.noreply.github.com>

--- packages/runtime/src/resources/value-converters/view.ts
+++ packages/runtime/src/resources/value-converters/view.ts
@@ -3,7 +3,7 @@ import { valueConverter } from '../value-converter';
 
 @valueConverter('view')
 export class ViewValueConverter {
-  constructor(@IViewLocator private viewLocator: IViewLocator) {}
+  constructor(@IViewLocator private readonly viewLocator: IViewLocator) {}
 
   public toView(subject: object, viewName?: string) {
     return this.viewLocator.getViewComponentForModelInstance(

30d5f568bc4a67ff5b98c2337e6f9c26b4cd7d84
Jürgen Wenzel <jurgen@jwenzel.se>
2019-08-26 12:22:13 +0200

    test(realworld): configure router to not use url fragment hash

--- test/realworld/src/main.ts
+++ test/realworld/src/main.ts
@@ -29,7 +29,7 @@ const globalResources = [
   .register(
     BasicConfiguration,
     DebugConfiguration,
-    RouterConfiguration,
+    RouterConfiguration.customize({ useUrlFragmentHash: true }),
     ...globalResources,
   )
   .app({

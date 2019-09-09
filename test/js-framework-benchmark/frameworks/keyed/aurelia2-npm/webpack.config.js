14bbd9c36d8644e8ea44d756379d2524605e71ce
brandonseydel <brandonseydel@hotmail.com>
2019-09-08 23:03:36 -0500

    feat(bench): Add pre/post merge results

---
+++ test/js-framework-benchmark/frameworks/keyed/aurelia2-npm/webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = function (env, { mode }) {
+  const production = mode === 'production';
+  !production && console.warn('Warning this is built in a non production mode');
+  return {
+    mode: 'development',
+    devtool: false,
+    entry: './src/main.ts',
+    resolve: {
+      extensions: ['.ts', '.js'],
+      modules: ['src', 'node_modules'],
+      mainFields: ['module']
+    },
+    devServer: {
+      historyApiFallback: true,
+      open: !process.env.CI,
+      lazy: false
+    },
+    module: {
+      rules: [
+        { test: /\.css$/i, use: ["style-loader", "css-loader"] },
+        { test: /\.ts$/i, loader: 'ts-loader' },
+        { test: /\.html$/i, loader: 'html-loader' }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: 'index.ejs' }),
+      new CopyWebpackPlugin([{ from: './css', to: './css' }])
+    ]
+  }
+}

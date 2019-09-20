9c118ea5ad42e30af1a2ea0d5d9a8bb3e05a0983
bigopon.777@gmail.com <bigopon.777@gmail.com>
2019-09-21 01:51:31 +1000

    fix(custom-attr): more test cases for multi binding detection

--- packages/__tests__/jit-html/has-multi-bindings.unit.spec.ts
+++ packages/__tests__/jit-html/has-multi-bindings.unit.spec.ts
@@ -1,7 +1,7 @@
-import { hasMultipleBindings } from '@aurelia/jit-html'
+import { hasMultipleBindings } from '@aurelia/jit-html';
 import { assert } from '@aurelia/testing';
 
-describe.skip('has-multi-bindings.spec.ts', function() {
+describe.skip('has-multi-bindings.unit.spec.ts', function() {
   type IHasMultipleBindingParserTestCase = [/* attr value */string, /* has multiple bindings? */boolean];
 
   const testCases: IHasMultipleBindingParserTestCase[] = [
@@ -10,6 +10,8 @@ describe.skip('has-multi-bindings.spec.ts', function() {
     ['http\\://abc.def', false],
     ['1111', false],
     ['1.11', false],
+    // ['::Math', false], // should throw?
+    // [':::::Math', false], // should throw?
     ['${a | b:{c:b}}', false],
     ['${a & b:{c:b}}', false],
     ['${a & b:{c:b}} ${a & b:{c:b}}', false],
@@ -23,6 +25,14 @@ describe.skip('has-multi-bindings.spec.ts', function() {
     ['a: ${a | c:d} abcd; b.bind: 2', true],
     ['a: http\\:/ahbc.def; b.bind: 2', true],
     ['a-a.bind: bcd; b-b5.bind: 2', true],
+    // router v1 scenarios
+    ['route: mainRoute; params.bind: { name: name, address, id: userId }', true],
+    ['params.bind: { name: name, address, id: userId }; route: mainRoute;', true],
+    // router v1 scenarios, twisted
+    ['params.bind: { name: name, address, id: userId } | normalizeAddress; route: mainRoute', true],
+    ['params.bind: { name: name, address, id: userId } | normalizeAddress; route: mainRoute;', true],
+    ['params.bind: { name: name, address, id: userId } | normalizeAddress:`en-us`; route: mainRoute', true],
+    ['params.bind: { name: name, address, id: userId } | normalizeAddress:`en-us`; route: mainRoute;', true],
   ];
 
   for (const testCase of testCases) {

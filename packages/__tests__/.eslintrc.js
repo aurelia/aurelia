8bf3cd4aafec2a57fd2b1e036a87403a43fe846d
Bas Bosman <github@nazgul.nu>
2019-10-06 21:31:06 +0200

    chore: lint mocha/no-async-describe

--- packages/__tests__/.eslintrc.js
+++ packages/__tests__/.eslintrc.js
@@ -16,13 +16,13 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     'import/no-nodejs-modules': 'off',
     'jsdoc/require-jsdoc': 'off',
+    'mocha/no-async-describe': 'error',
     'mocha/no-hooks-for-single-case': 'off', // Disabled to avoid duplicates, because 'no-hooks' is enabled
     'max-lines-per-function': 'off',
     'no-console': 'off',
 
     // Things we maybe need to fix some day, so are marked as off for now as they're quite noisy:
     'mocha/max-top-level-suites': 'off',
-    'mocha/no-async-describe': 'off',
     'mocha/no-exclusive-tests': 'off',
     'mocha/no-hooks': 'off',
     'mocha/no-identical-title': 'off',

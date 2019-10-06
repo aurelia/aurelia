590b719277bb787e9f6a256127cf0e3cab8f6504
Bas Bosman <github@nazgul.nu>
2019-10-06 23:00:13 +0200

    chore: lint mocha/no-exclusive-tests

--- packages/__tests__/.eslintrc.js
+++ packages/__tests__/.eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
     'import/no-nodejs-modules': 'off',
     'jsdoc/require-jsdoc': 'off',
     'mocha/no-async-describe': 'error',
+    'mocha/no-exclusive-tests': 'warn',
     'mocha/no-hooks-for-single-case': 'off', // Disabled to avoid duplicates, because 'no-hooks' is enabled
     'mocha/no-identical-title': 'error',
     'mocha/no-mocha-arrows': 'error',
@@ -27,7 +28,6 @@ module.exports = {
 
     // Things we maybe need to fix some day, so are marked as off for now as they're quite noisy:
     'mocha/max-top-level-suites': 'off',
-    'mocha/no-exclusive-tests': 'off',
     'mocha/no-hooks': 'off',
     'mocha/no-setup-in-describe': 'off',
     'mocha/no-synchronous-tests': 'off'

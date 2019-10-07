a2b7e529deda566ce9eeb1cef2ea25e899873c56
Bas Bosman <github@nazgul.nu>
2019-10-07 22:45:29 +0200

    chore: disable @typescript-eslint/typedef for js files

--- .eslintrc.js
+++ .eslintrc.js
@@ -223,6 +223,7 @@ module.exports = {
       '@typescript-eslint/explicit-member-accessibility': 'off',
       '@typescript-eslint/no-require-imports': 'off',
       '@typescript-eslint/no-var-requires': 'off',
+      '@typescript-eslint/typedef': 'off',
       'compat/compat': 'off'
     }
   }, { // Specific overrides for TS files within tests as some rules don't make sense there.

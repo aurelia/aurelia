1365db5a7d054efd92590c1629ca789af7ab15a0
Jürgen Wenzel <jurgen@jwenzel.se>
2019-10-08 00:22:43 +0200

    test(router): add Author as link component in doc-example

--- packages/__tests__/router/e2e/doc-example/app/src/components/authors/authors.ts
+++ packages/__tests__/router/e2e/doc-example/app/src/components/authors/authors.ts
@@ -3,20 +3,24 @@ import { customElement, IViewModel } from '@aurelia/runtime';
 import { AuthorsRepository } from '../../repositories/authors';
 import { State } from '../../state';
 import { wait } from '../../utils';
+import { Author } from './author';
 
 @customElement({
   name: 'authors', template: `<template>
 <h3>Authors</h3>
 <ul>
   <li data-test="authors-element-item" repeat.for="author of authors">
-    <a data-test="authors-element-author-link" href="\${author.name}" au-href="author(\${author.id})">\${author.name}</a>
+    <a data-test="authors-element-author-link" href="\${author.name}" au-href.bind="{ component: Author, parameters: \`\${author.id}\` }">\${author.name}</a>
     <ul><li data-test="authors-element-book-name" repeat.for="book of author.books">\${book.title}</li></ul>
   </li>
 </ul>
 </template>` })
 @inject(AuthorsRepository, State)
 export class Authors {
-  public constructor(private readonly authorsRepository: AuthorsRepository, private readonly state: State) { }
+  public constructor(private readonly authorsRepository: AuthorsRepository, private readonly state: State) {
+    this['Author'] = Author;
+    console.log('Author', Author);
+  }
 
   get authors() { return this.authorsRepository.authors(); }
 

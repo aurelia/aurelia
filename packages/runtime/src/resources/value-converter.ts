d75aa91b026fa4bddf90807f6ccc61ac569bf529
Fred Kleuver <fred@avurad.nl>
2019-10-15 23:54:40 +0200

    refactor(value-converter): apply new metadata api

--- packages/runtime/src/resources/value-converter.ts
+++ packages/runtime/src/resources/value-converter.ts
@@ -1,73 +1,84 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
 import {
-  Class,
   Constructable,
   IContainer,
-  IResourceDefinition,
+  ResourceDefinition,
   IResourceKind,
-  IResourceType,
+  ResourceType,
   Registration,
-  Writable,
-  PLATFORM
+  Metadata,
+  Protocol,
+  PartialResourceDefinition,
 } from '@aurelia/kernel';
 import { registerAliases } from '../definitions';
 
-export interface IValueConverter {
+export type ValueConverterInstance<T extends {} = {}> = {
   toView(input: unknown, ...args: unknown[]): unknown;
   fromView?(input: unknown, ...args: unknown[]): unknown;
-}
+} & T;
 
-export interface IValueConverterDefinition extends IResourceDefinition {
-}
+export type ValueConverterType<T extends Constructable = Constructable> = ResourceType<T, ValueConverterInstance>;
+export type ValueConverterKind = IResourceKind<ValueConverterType, ValueConverterDefinition> & {
+  define<T extends Constructable>(name: string, Type: T): ValueConverterType<T>;
+};
 
-type ValueConverterStaticProperties = Required<Pick<IValueConverterDefinition, 'aliases'>>;
-export interface IValueConverterType<C extends Constructable = Constructable> extends IResourceType<IValueConverterDefinition, InstanceType<C> & IValueConverter>, ValueConverterStaticProperties { }
+export type ValueConverterDecorator = <T extends Constructable>(Type: T) => ValueConverterType<T>;
 
-export interface IValueConverterResource extends
-  IResourceKind<IValueConverterDefinition, IValueConverter, Class<IValueConverter>> {
-}
-
-export function valueConverter(definition: IValueConverterDefinition): ValueConverterDecorator;
-export function valueConverter(name: string): ValueConverterDecorator;
-export function valueConverter(nameOrDefinition: string | IValueConverterDefinition): ValueConverterDecorator;
-export function valueConverter(nameOrDefinition: string | IValueConverterDefinition): ValueConverterDecorator {
-  return target => ValueConverter.define(nameOrDefinition, target) as any; // TODO: fix this at some point
-}
-
-export const ValueConverter: Readonly<IValueConverterResource> = Object.freeze({
-  name: 'au:value-converter',
-  keyFrom(name: string): string {
-    return `${ValueConverter.name}:${name}`;
-  },
-  isType<T>(Type: T & Partial<IValueConverterType>): Type is T & IValueConverterType {
-    return Type.kind === ValueConverter;
-  },
-  define<T extends Constructable = Constructable>(nameOrDefinition: string | IValueConverterDefinition, ctor: T): T & IValueConverterType<T> {
-    const Type = ctor as T & IValueConverterType<T>;
-    const WritableType = Type as T & Writable<IValueConverterType<T>>;
-    const description = createCustomValueDescription(typeof nameOrDefinition === 'string' ? { name: nameOrDefinition } : nameOrDefinition, Type);
-
-    WritableType.kind = ValueConverter;
-    WritableType.description = description;
-    WritableType.aliases = Type.aliases == null ? PLATFORM.emptyArray : Type.aliases;
-    Type.register = function register(container: IContainer): void {
-      const aliases = description.aliases;
-      const key = ValueConverter.keyFrom(description.name);
-      Registration.singleton(key, this).register(container);
-      Registration.alias(key, this).register(container);
-      registerAliases([...aliases, ...this.aliases], ValueConverter, key, container);
-    };
-
-    return Type;
-  },
-});
-
-/** @internal */
-export function createCustomValueDescription(def: IValueConverterDefinition, Type: IValueConverterType): Required<IValueConverterDefinition> {
-  const aliases = def.aliases;
-  return {
-    name: def.name,
-    aliases: aliases == null ? PLATFORM.emptyArray : aliases,
+export function valueConverter(name: string): ValueConverterDecorator {
+  return function (target) {
+    return ValueConverter.define(name, target);
   };
 }
 
-export type ValueConverterDecorator = <T extends Constructable>(target: T) => T & IValueConverterType<T>;
+export class ValueConverterDefinition<T extends Constructable = Constructable> implements ResourceDefinition<T, ValueConverterInstance> {
+  private constructor(
+    public readonly Type: ValueConverterType<T>,
+    public readonly name: string,
+    public readonly aliases: readonly string[],
+    public readonly key: string = ValueConverter.keyFrom(name),
+  ) {}
+
+  public static create<T extends Constructable = Constructable>(
+    nameOrDefinition: string | PartialResourceDefinition,
+    Type: ValueConverterType<T>,
+  ): ValueConverterDefinition<T> {
+    let name: string;
+    let aliases: string[];
+
+    if (typeof nameOrDefinition === 'string') {
+      name = nameOrDefinition;
+      aliases = [];
+    } else {
+      name = nameOrDefinition.name;
+      aliases = nameOrDefinition.aliases === void 0 ? [] : nameOrDefinition.aliases.slice();
+    }
+
+    if (Type.aliases !== void 0) {
+      aliases.push(...Type.aliases);
+    }
+
+    return new ValueConverterDefinition(Type, name, aliases);
+  }
+
+  public register(container: IContainer): void {
+    const { Type, key, aliases } = this;
+    Registration.singleton(key, Type).register(container);
+    Registration.alias(key, Type).register(container);
+    registerAliases(aliases, ValueConverter, key, container);
+  }
+}
+
+export const ValueConverter: ValueConverterKind = {
+  name: Protocol.resource.keyFor('value-converter'),
+  keyFrom(name: string): string {
+    return `${ValueConverter.name}:${name}`;
+  },
+  define<T extends Constructable>(nameOrDefinition: string | ValueConverterDefinition, Type: T): ValueConverterType<T> {
+    const $Type = Type as ValueConverterType<T>;
+    const description = ValueConverterDefinition.create(nameOrDefinition, $Type);
+    Metadata.define(ValueConverter.name, description, Type);
+    Protocol.resource.appendTo(Type, ValueConverter.name);
+
+    return $Type;
+  },
+};

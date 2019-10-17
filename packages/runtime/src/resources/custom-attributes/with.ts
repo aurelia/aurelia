1efd9372f879817c9d1c7940cfcbbc51612cdfbb
Fred Kleuver <fred@avurad.nl>
2019-10-18 01:19:06 +0200

    refactor(with): cleanup, go back to idiomatic aurelia code

--- packages/runtime/src/resources/custom-attributes/with.ts
+++ packages/runtime/src/resources/custom-attributes/with.ts
@@ -1,90 +1,30 @@
-import {
-  IContainer,
-  Key,
-  nextId,
-  PLATFORM,
-  Registration
-} from '@aurelia/kernel';
-import {
-  HooksDefinition,
-  IAttributeDefinition
-} from '../../definitions';
-import {
-  INode,
-  IRenderLocation
-} from '../../dom';
-import {
-  BindingMode,
-  BindingStrategy,
-  LifecycleFlags,
-  State
-} from '../../flags';
-import {
-  IController,
-  IViewFactory
-} from '../../lifecycle';
-import { InlineObserversLookup } from '../../observation';
+import { nextId } from '@aurelia/kernel';
+import { INode, IRenderLocation } from '../../dom';
+import { LifecycleFlags, State } from '../../flags';
+import { IController, IViewFactory } from '../../lifecycle';
+import { templateController } from '../custom-attribute';
+import { bindable } from '../../templating/bindable';
 import { Scope } from '../../observation/binding-context';
-import { Bindable } from '../../templating/bindable';
-import {
-  CustomAttribute,
-  ICustomAttributeResource
-} from '../custom-attribute';
 
+@templateController('with')
 export class With<T extends INode = INode> {
-  public static readonly inject: readonly Key[] = [IViewFactory, IRenderLocation];
-
-  public static readonly kind: ICustomAttributeResource = CustomAttribute;
-  public static readonly description: Required<IAttributeDefinition> = Object.freeze({
-    name: 'with',
-    aliases: PLATFORM.emptyArray as typeof PLATFORM.emptyArray & string[],
-    defaultBindingMode: BindingMode.toView,
-    isTemplateController: true,
-    bindables: Object.freeze(Bindable.for({ bindables: ['value'] }).get()),
-    strategy: BindingStrategy.getterSetter,
-    hooks: Object.freeze(new HooksDefinition(With.prototype)),
-  });
-
-  public readonly id: number;
-
-  public get value(): object | undefined {
-    return this._value;
-  }
-  public set value(newValue: object | undefined) {
-    const oldValue = this._value;
-    if (oldValue !== newValue) {
-      this._value = newValue;
-      this.valueChanged(newValue, oldValue, LifecycleFlags.none);
-    }
-  }
-
-  public readonly $observers: InlineObserversLookup<this> = Object.freeze({
-    value: this,
-  });
+  public readonly id: number = nextId('au$component');
 
   public readonly view: IController<T>;
-  private readonly factory: IViewFactory<T>;
   // eslint-disable-next-line @typescript-eslint/prefer-readonly
   private $controller!: IController<T>; // This is set by the controller after this instance is constructed
 
-  private _value: object | undefined;
+  @bindable public value?: object;
 
   public constructor(
-    factory: IViewFactory<T>,
-    location: IRenderLocation<T>
+    @IViewFactory private readonly factory: IViewFactory<T>,
+    @IRenderLocation private readonly location: IRenderLocation<T>
   ) {
     this.id = nextId('au$component');
 
     this.factory = factory;
     this.view = this.factory.create();
     this.view.hold(location);
-
-    this._value = void 0;
-  }
-
-  public static register(container: IContainer): void {
-    container.register(Registration.transient('au:custom-attribute:with', this));
-    container.register(Registration.transient(this, this));
   }
 
   public valueChanged(newValue: unknown, oldValue: unknown, flags: LifecycleFlags): void {

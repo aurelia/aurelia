5083b100ce39c980674338c634f41e2813883548
Fred Kleuver <fred@avurad.nl>
2019-10-17 18:36:10 +0200

    refactor(children): normalize to similar mechanism as bindable

--- packages/runtime/src/templating/children.ts
+++ packages/runtime/src/templating/children.ts
@@ -1,15 +1,25 @@
-import { Constructable } from '@aurelia/kernel';
-import { ChildrenObserverSource, IChildrenObserverDescription, ITemplateDefinition } from '../definitions';
+/* eslint-disable @typescript-eslint/no-use-before-define */
+import { Constructable, Protocol, Metadata } from '@aurelia/kernel';
+import { ChildrenObserverSource, firstDefined } from '../definitions';
+import { INode } from '../dom';
+import { IController, IViewModel } from '../lifecycle';
+import { IElementProjector } from '../resources/custom-element';
 
-export type HasChildrenObservers = Pick<ITemplateDefinition, 'childrenObservers'>;
-export type ChildrenDecorator = <T extends InstanceType<Constructable & Partial<HasChildrenObservers>>>(target: T, prop: string) => void;
+export type PartialChildrenObserverDefinition<TNode extends INode = INode> = {
+  callback?: string;
+  property?: string;
+  options?: MutationObserverInit;
+  query?: (projector: IElementProjector<TNode>) => ArrayLike<TNode>;
+  filter?: (node: TNode, controller?: IController<TNode>, viewModel?: IViewModel<TNode>) => boolean;
+  map?: (node: TNode, controller?: IController<TNode>, viewModel?: IViewModel<TNode>) => any;
+};
 
 /**
  * Decorator: Specifies custom behavior for an array children property that synchronizes its items with child content nodes of the element.
  *
  * @param config - The overrides
  */
-export function children(config?: ChildrenObserverSource): ChildrenDecorator;
+export function children(config?: ChildrenObserverSource): PropertyDecorator | ClassDecorator;
 /**
  * Decorator: Specifies an array property on a class that synchronizes its items with child content nodes of the element.
  *
@@ -22,35 +32,29 @@ export function children(prop: string): ClassDecorator;
  * @param target - The class
  * @param prop - The property name
  */
-export function children<T extends InstanceType<Constructable & Partial<HasChildrenObservers>>>(target: T, prop: string): void;
-export function children<T extends InstanceType<Constructable & Partial<HasChildrenObservers>>>(configOrTarget?: ChildrenObserverSource | T, prop?: string): void | ChildrenDecorator | ClassDecorator {
-  let config: IChildrenObserverDescription;
+export function children(target: {}, prop: string): void;
+export function children(configOrTarget?: ChildrenObserverSource | {}, prop?: string): void | PropertyDecorator | ClassDecorator {
+  let config: PartialChildrenObserverDefinition;
 
-  const decorator = function decorate($target: T, $prop: string): void {
+  function decorator($target: {}, $prop: symbol | string): void {
     if (arguments.length > 1) {
       // Non invocation:
       // - @children
       // Invocation with or w/o opts:
       // - @children()
       // - @children({...opts})
-      config.property = $prop;
+      config.property = $prop as string;
     }
 
-    const childrenObservers =
-      ($target.constructor as HasChildrenObservers).childrenObservers || (($target.constructor as HasChildrenObservers).childrenObservers = {});
-
-    if (!config.callback) {
-      config.callback = `${config.property}Changed`;
-    }
-
-    childrenObservers[config.property!] = config;
-  };
+    Metadata.define(ChildrenObserver.name, ChildrenObserverDefinition.create($prop as string, config), $target, $prop);
+    Protocol.annotation.appendTo($target.constructor as Constructable, ChildrenObserver.keyFrom($prop as string));
+  }
 
   if (arguments.length > 1) {
     // Non invocation:
     // - @children
     config = {};
-    decorator(configOrTarget as T, prop!);
+    decorator(configOrTarget!, prop!);
     return;
   } else if (typeof configOrTarget === 'string') {
     // ClassDecorator
@@ -58,12 +62,83 @@ export function children<T extends InstanceType<Constructable & Partial<HasChild
     // Direct call:
     // - @children('bar')(Foo)
     config = {};
-    return decorator as ChildrenDecorator;
+    return decorator;
   }
 
   // Invocation with or w/o opts:
   // - @children()
   // - @children({...opts})
-  config = (configOrTarget || {}) as IChildrenObserverDescription;
-  return decorator as ChildrenDecorator;
+  config = configOrTarget === void 0 ? {} : configOrTarget;
+  return decorator;
+}
+
+function isChildrenObserverAnnotation(key: string): boolean {
+  return key.startsWith(ChildrenObserver.name);
+}
+
+export const ChildrenObserver = {
+  name: Protocol.annotation.keyFor('children-observer'),
+  keyFrom(name: string): string {
+    return `${ChildrenObserver.name}:${name}`;
+  },
+  from(...childrenObserverLists: readonly (ChildrenObserverDefinition | Record<string, PartialChildrenObserverDefinition> | readonly string[] | undefined)[]): Record<string, ChildrenObserverDefinition> {
+    const childrenObservers: Record<string, ChildrenObserverDefinition> = {};
+
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    const isArray = Array.isArray as <T>(arg: unknown) => arg is readonly T[];
+
+    function addName(name: string): void {
+      childrenObservers[name] = ChildrenObserverDefinition.create(name);
+    }
+
+    function addDescription(name: string, def: PartialChildrenObserverDefinition): void {
+      childrenObservers[name] = ChildrenObserverDefinition.create(name, def);
+    }
+
+    function addList(maybeList: ChildrenObserverDefinition | Record<string, PartialChildrenObserverDefinition> | readonly string[] | undefined): void {
+      if (isArray(maybeList)) {
+        maybeList.forEach(addName);
+      } else if (maybeList instanceof ChildrenObserverDefinition) {
+        Object.keys(maybeList).forEach(name => addDescription(name, maybeList));
+      } else if (maybeList !== void 0) {
+        Object.keys(maybeList).forEach(name => addDescription(name, maybeList));
+      }
+    }
+
+    childrenObserverLists.forEach(addList);
+
+    return childrenObservers;
+  },
+  getAll(Type: Constructable): readonly ChildrenObserverDefinition[] {
+    const propStart = ChildrenObserver.name.length + 1;
+    const keys = Protocol.annotation.getKeys(Type).filter(isChildrenObserverAnnotation);
+    const len = keys.length;
+    const defs: ChildrenObserverDefinition[] = Array(len);
+    for (let i = 0; i < len; ++i) {
+      defs[i] = Metadata.getOwn(ChildrenObserver.name, Type, keys[i].slice(propStart));
+    }
+    return defs;
+  },
+};
+
+export class ChildrenObserverDefinition<TNode extends INode = INode> {
+  private constructor(
+    public readonly callback: string,
+    public readonly property: string,
+    public readonly options?: MutationObserverInit,
+    public readonly query?: (projector: IElementProjector<TNode>) => ArrayLike<TNode>,
+    public readonly filter?: (node: TNode, controller?: IController<TNode>, viewModel?: IViewModel<TNode>) => boolean,
+    public readonly map?: (node: TNode, controller?: IController<TNode>, viewModel?: IViewModel<TNode>) => any,
+  ) {}
+
+  public static create<TNode extends INode = INode>(prop: string, def: PartialChildrenObserverDefinition<TNode> = {}): ChildrenObserverDefinition<TNode> {
+    return new ChildrenObserverDefinition(
+      firstDefined(def.callback, `${prop}Changed`),
+      firstDefined(def.property, prop),
+      def.options,
+      def.query,
+      def.filter,
+      def.map,
+    );
+  }
 }

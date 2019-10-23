9c33fbe166553116880f8a912a2c29f91cd8222e
Fred Kleuver <fred@avurad.nl>
2019-10-23 06:47:43 +0200

    refactor(attr): convert from lifecycle to scheduler

--- packages/runtime-html/src/resources/binding-behaviors/attr.ts
+++ packages/runtime-html/src/resources/binding-behaviors/attr.ts
@@ -1,10 +1,15 @@
-import { ILifecycle, IScope, LifecycleFlags, PropertyBinding, bindingBehavior } from '@aurelia/runtime';
+import { IScope, LifecycleFlags, PropertyBinding, bindingBehavior, IScheduler } from '@aurelia/runtime';
 import { DataAttributeAccessor } from '../../observation/data-attribute-accessor';
 
 @bindingBehavior('attr')
 export class AttrBindingBehavior {
   public bind(flags: LifecycleFlags, scope: IScope, binding: PropertyBinding): void {
-    binding.targetObserver = new DataAttributeAccessor(binding.locator.get(ILifecycle), flags, binding.target as HTMLElement, binding.targetProperty);
+    binding.targetObserver = new DataAttributeAccessor(
+      binding.locator.get(IScheduler),
+      flags,
+      binding.target as HTMLElement,
+      binding.targetProperty,
+    );
   }
 
   public unbind(flags: LifecycleFlags, scope: IScope, binding: PropertyBinding): void {

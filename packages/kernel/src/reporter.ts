aa78655772a8bf22725fd0e3a2ee17d57186c92e
Fred Kleuver <fred@avurad.nl>
2019-10-27 15:52:31 +0100

    refactor(reporter): improve and document log levels

--- packages/kernel/src/reporter.ts
+++ packages/kernel/src/reporter.ts
@@ -21,10 +21,36 @@ export interface ILiveLoggingOptions {
   jit?: boolean;
 }
 export const enum LogLevel {
-  error,
-  warn,
-  info,
-  debug,
+  /**
+   * The most detailed information about internal app state.
+   *
+   * Disabled by default and should never be enabled in a production environment.
+   */
+  trace = 0,
+  /**
+   * Information that is useful for debugging during development and has no long-term value.
+   */
+  debug = 1,
+  /**
+   * Information about the general flow of the application that has long-term value.
+   */
+  info = 2,
+  /**
+   * Unexpected circumstances that require attention but do not otherwise cause the current flow of execution to stop.
+   */
+  warn = 3,
+  /**
+   * Unexpected circumstances that cause the flow of execution in the current activity to stop but do not cause an app-wide failure.
+   */
+  error = 4,
+  /**
+   * Unexpected circumstances that cause an app-wide failure or otherwise require immediate attention.
+   */
+  fatal = 5,
+  /**
+   * No messages should be written.
+   */
+  none = 6,
 }
 export const Reporter = {
   level: LogLevel.warn,

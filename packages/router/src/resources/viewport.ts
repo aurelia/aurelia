dbe6a8891f59e55d51dcd75e8f5835a861a1b546
Jürgen Wenzel <jurgen@jwenzel.se>
2019-11-04 12:56:24 +0100

    refactor(router): remove viewport header

--- packages/router/src/resources/viewport.ts
+++ packages/router/src/resources/viewport.ts
@@ -11,7 +11,7 @@ import { IViewportOptions, Viewport } from '../viewport';
 
 @customElement({
   name: 'au-viewport',
-  template: `<template><div class="viewport-header" style="display: none;">Viewport: <b>\${name}</b> \${scope ? "[new scope]" : ""} : <b>\${viewport.content && viewport.content.toComponentName()}</b></div></template>`
+  template: `<template></template>`
 })
 export class ViewportCustomElement {
   @bindable public name: string = 'default';

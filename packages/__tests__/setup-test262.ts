fd76b90d2427c1b2142f5cc73a76a807859c0b13
Fred Kleuver <fred@avurad.nl>
2019-11-07 19:17:35 +0100

    test(aot): correctly configure the test cases

--- packages/__tests__/setup-test262.ts
+++ packages/__tests__/setup-test262.ts
@@ -6,13 +6,129 @@ import {
   Registration,
   IContainer,
   ILogger,
+  PLATFORM,
 } from '@aurelia/kernel';
 import {
   IFileSystem,
   NodeFileSystem,
   Realm,
+  IFile,
 } from '@aurelia/aot';
 import { resolve } from 'path';
+
+class TestMetadataNegative {
+  public constructor(
+    public readonly phase: 'parse' | 'early' | 'resolution' | 'runtime',
+    public readonly type: string,
+  ) {}
+}
+
+class TestMetadata {
+  public constructor(
+    public readonly negative: TestMetadataNegative | null,
+  ) {}
+
+  public static from(content: string): TestMetadata | null {
+    let start = content.indexOf('/*---');
+    let end = content.indexOf('---*/');
+    if (start >= 0 && end >= start) {
+      let banner = content.slice(start + '/*---'.length, end);
+      const lines = banner.split('\n');
+      const negativeIndex = lines.findIndex(l => l.startsWith('negative:'));
+      if (negativeIndex >= 0) {
+        const [, phase] = lines[negativeIndex + 1].split(': ');
+        const [, type] = lines[negativeIndex + 2].split(': ');
+        return new TestMetadata(
+          new TestMetadataNegative(phase as 'parse' | 'early' | 'resolution' | 'runtime', type),
+        );
+      } else {
+        return new TestMetadata(null);
+      }
+    }
+
+    return null;
+  }
+}
+
+class TestCase {
+  public readonly meta: TestMetadata | null;
+
+  public constructor(
+    public readonly container: IContainer,
+    public readonly logger: ILogger,
+    public readonly file: IFile,
+  ) {
+    this.meta = TestMetadata.from(file.getContentSync());
+  }
+
+  public async run(): Promise<void> {
+    const container = this.container;
+    const logger = this.logger;
+    const file = this.file;
+    const meta = this.meta;
+
+    if (meta !== null && meta.negative !== null) {
+      switch (meta.negative.phase) {
+        case 'parse': // Parse errors should be caught by TypeScript
+          logger.info(`SKIP - parse error test file: ${file.rootlessPath}`);
+          return;
+        case 'early': // Early errors should be caught by TypeScript
+          logger.info(`SKIP - early error test file: ${file.rootlessPath}`);
+          return;
+        case 'resolution': {
+          const realm = Realm.Create(container);
+          const mod = await realm.loadFile(file);
+
+          let err: Error;
+          try {
+            mod.Instantiate();
+          } catch ($err) {
+            err = $err;
+          }
+
+          if (err === void 0) {
+            throw new Error(`FAIL - Expected ${meta.negative.type} but got no error - Resolution error test file: ${file.rootlessPath}`);
+          }
+
+          if (err.name !== meta.negative.type) {
+            throw new Error(`FAIL - Expected ${meta.negative.type} but got ${err.name} - Resolution error test file: ${file.rootlessPath}`);
+          }
+
+          logger.info('PASS - Resolution error test file: ${file.rootlessPath}');
+        }
+        case 'runtime': {
+          const realm = Realm.Create(container);
+          const mod = await realm.loadFile(file);
+          mod.Instantiate();
+
+          let err: Error;
+          try {
+            mod.Evaluate();
+          } catch ($err) {
+            err = $err;
+          }
+
+          if (err === void 0) {
+            throw new Error(`FAIL - Expected ${meta.negative.type} but got no error - Runtime error test file: ${file.rootlessPath}`);
+          }
+
+          if (err.name !== meta.negative.type) {
+            throw new Error(`FAIL - Expected ${meta.negative.type} but got ${err.name} - Runtime error test file: ${file.rootlessPath}`);
+          }
+
+          logger.info(`PASS - Runtime error test file: ${file.rootlessPath}`);
+        }
+      }
+    } else {
+      const realm = Realm.Create(container);
+      const mod = await realm.loadFile(file);
+      mod.Instantiate();
+      mod.Evaluate();
+
+      logger.info(`PASS - Valid test file: ${file.rootlessPath}`);
+    }
+  }
+}
 
 class TestRunner {
   public readonly container: IContainer;
@@ -30,18 +146,18 @@ class TestRunner {
   }
 
   public async load(): Promise<void> {
-    this.logger.debug(`Loading test files`);
+    const dir = resolve(__dirname, '..', '..', '..', 'test262', 'test', 'language');
 
-    const files = await this.fs.getFiles(resolve(__dirname, '..', '..', '..', 'test262', 'test', 'language'));
+    const now = PLATFORM.now();
+    this.logger.info(`Loading test files from ${dir}`);
 
-    this.logger.debug(`Loaded ${files.length} test files`);
+    const files = await this.fs.getFiles(dir, true);
 
-    for (const file of files) {
-      this.logger.info(`Running test file: ${file.rootlessPath}`);
-      const realm = Realm.Create(this.container);
-      const mod = await realm.loadFile(file);
-      mod.Instantiate();
-      mod.Evaluate();
+    this.logger.info(`Loaded ${files.length} test files in ${Math.round(PLATFORM.now() - now)}ms`);
+
+    const testCases = files.map(x => new TestCase(this.container, this.logger, x));
+    for (const tc of testCases) {
+      await tc.run();
     }
   }
 }
@@ -50,4 +166,7 @@ class TestRunner {
   const runner = new TestRunner();
 
   await runner.load();
-})().catch(console.error);
+})().catch(err => {
+  console.error(err);
+  process.exit(-1);
+});

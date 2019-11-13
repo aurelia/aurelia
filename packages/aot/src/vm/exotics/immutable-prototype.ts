1f8e3fb0a1b062a5149c6ad3d5d8705eace67891
Fred Kleuver <fred@avurad.nl>
2019-11-13 17:18:14 +0100

    feat(aot): implement ImmutablePrototypeExoticObject

---
+++ packages/aot/src/vm/exotics/immutable-prototype.ts
@@ -0,0 +1,33 @@
+import { $Object, $Boolean, $Null } from '../value';
+import { Realm } from '../realm';
+
+// http://www.ecma-international.org/ecma-262/#sec-string-exotic-objects
+export class $ImmutablePrototypeExoticObject extends $Object<'ImmutablePrototypeExoticObject'> {
+  public readonly '[[Prototype]]': $Object | $Null;
+
+  public constructor(
+    realm: Realm,
+    proto: $Object | $Null,
+  ) {
+    super(realm, 'ImmutablePrototypeExoticObject', proto);
+  }
+
+  // http://www.ecma-international.org/ecma-262/#sec-immutable-prototype-exotic-objects-setprototypeof-v
+  public '[[SetPrototypeOf]]'(V: $Object | $Null): $Boolean {
+    // 1. Return ? SetImmutablePrototype(O, V).
+
+    // http://www.ecma-international.org/ecma-262/#sec-set-immutable-prototype
+
+    // 1. Assert: Either Type(V) is Object or Type(V) is Null.
+    // 2. Let current be ? O.[[GetPrototypeOf]]().
+    const current = super['[[GetPrototypeOf]]']();
+
+    // 3. If SameValue(V, current) is true, return true.
+    if (V.is(current)) {
+      return this.realm['[[Intrinsics]]'].true;
+    }
+
+    // 4. Return false.
+    return this.realm['[[Intrinsics]]'].false;
+  }
+}

88b9cbe9d7bc6477c26089070bdcc3c1ea250096
Fred Kleuver <fred@avurad.nl>
2019-11-14 21:26:09 +0100

    chore(aot): setup some initial virtual file system stuff

---
+++ packages/__tests__/aot/smoke-tests.spec.ts
@@ -0,0 +1,6 @@
+// NOTE: These tests are not meant to be even close to exhaustive. That's what the 262 test suite is for.
+// These tests exist solely for having an easy way to quickly test some high-level things when a feature is not yet ready for exposure to the 262 test suite.
+
+describe('AOT (smoke tests)', function () {
+
+});

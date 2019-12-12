5605d57c177b2f38584d875212366f880abdc5df
Jürgen Wenzel <jurgen@jwenzel.se>
2019-12-12 01:32:28 +0100

    refactor(router): add task queue

---
+++ packages/router/src/task-queue.ts
@@ -0,0 +1,162 @@
+// tslint:disable:max-line-length
+import { IScheduler, ITask, ILifecycleTask } from '@aurelia/runtime';
+import { bound } from '@aurelia/kernel';
+
+export interface IQueueableItem<T> {
+  execute: ((task: QueueTask<IQueueableItem<T>>) => void);
+}
+
+export class QueueTask<T> implements ILifecycleTask {
+  public done: boolean = false;
+  private readonly promise: Promise<void>;
+
+  resolve!: ((value: void | PromiseLike<void>) => void);
+  reject!: ((value: unknown | PromiseLike<unknown>) => void);
+
+  public constructor(
+    private taskQueue: TaskQueue<T>,
+    public item: IQueueableItem<T> | ((task: QueueTask<IQueueableItem<T>>) => void),
+    public cost: number = 0,
+  ) {
+    this.promise = new Promise((resolve, reject) => {
+      this.resolve = () => {
+        this.taskQueue.resolve(this, resolve);
+      };
+      this.reject = (reason: unknown) => {
+        this.taskQueue.reject(this, reject, reason);
+      };
+    });
+  }
+
+  public execute(): void {
+    if ('execute' in this.item) {
+      this.item.execute(this);
+    } else {
+      this.item(this);
+    }
+  }
+  public wait(): Promise<void> {
+    return this.promise;
+  }
+  public canCancel(): boolean {
+    return false;
+  }
+  public cancel(): void { }
+}
+
+// this.taskQueue.enqueue((task) => { this.doSomething(); task.resolve(); }).wait();
+
+export interface ITaskQueueOptions {
+  scheduler: IScheduler;
+  allowedExecutionCostWithinTick: number;
+}
+
+/**
+ * A first-in-first-out task queue that only processes the next queued item
+ * when the current one has been resolved or rejected. Sends queued items
+ * as tasks one at a time to a specified callback function. The callback
+ * function should resolve or reject the task when processing is done.
+ * Enqueued items' tasks can be awaited. Enqueued items can specify an
+ * (arbitrary) execution cost and the queue can be set up (activated) to
+ * only process a specific amount of execution cost per RAF/tick.
+ */
+export class TaskQueue<T> {
+  public get isActive(): boolean {
+    return this.task !== null;
+  }
+  public readonly pending: QueueTask<T>[] = [];
+  public processing: QueueTask<T> | null = null;
+  public allowedExecutionCostWithinTick: number | null = null;
+  public currentExecutionCostInCurrentTick: number = 0;
+  private scheduler: IScheduler | null = null;
+  private task: ITask | null = null;
+
+  public constructor(
+    private readonly callback: (task: QueueTask<T>) => void
+  ) { }
+
+  public get length(): number {
+    return this.pending.length;
+  }
+
+  public activate(options: ITaskQueueOptions): void {
+    if (this.isActive) {
+      throw new Error('TaskQueue has already been activated');
+    }
+    this.scheduler = options.scheduler;
+    this.allowedExecutionCostWithinTick = options.allowedExecutionCostWithinTick;
+    this.task = this.scheduler.queueRenderTask(this.dequeue, { persistent: true });
+  }
+  public deactivate(): void {
+    if (!this.isActive) {
+      throw new Error('TaskQueue has not been activated');
+    }
+    this.task!.cancel();
+    this.task = null;
+    this.allowedExecutionCostWithinTick = null;
+    this.clear();
+  }
+
+  public enqueue(item: IQueueableItem<T>, cost?: number): QueueTask<T>;
+  public enqueue(items: IQueueableItem<T>[], cost?: number): QueueTask<T>[];
+  public enqueue(items: IQueueableItem<T>[], costs?: number[]): QueueTask<T>[];
+  public enqueue(task: QueueTask<T>): QueueTask<T>;
+  public enqueue(tasks: QueueTask<T>[]): QueueTask<T>[];
+  public enqueue(itemOrItems: IQueueableItem<T> | IQueueableItem<T>[] | QueueTask<T> | QueueTask<T>[], costOrCosts?: number | number[]): QueueTask<T> | QueueTask<T>[] {
+    const list: boolean = Array.isArray(itemOrItems);
+    const items: (IQueueableItem<T> | QueueTask<T>)[] = (list ? itemOrItems : [itemOrItems]) as (IQueueableItem<T> | QueueTask<T>)[];
+    const costs: number[] = items
+      .map((value: IQueueableItem<T> | QueueTask<T>, index: number): number | undefined => !Array.isArray(costOrCosts) ? costOrCosts : costOrCosts[index])
+      .map((value: number | undefined): number => value !== undefined ? value : 1);
+    const tasks: QueueTask<T>[] = [];
+    for (const item of items) {
+      tasks.push(item instanceof QueueTask
+        ? item
+        : this.createQueueTask(item, costs.shift())); // TODO: Get cancellable in as well
+    }
+    this.pending.push(...tasks);
+    this.dequeue();
+    return list ? tasks : tasks[0];
+  }
+
+  public createQueueTask(item: IQueueableItem<T>, cost?: number): QueueTask<T> {
+    return new QueueTask(this, item, cost);
+  }
+
+  @bound
+  public dequeue(delta?: number): void {
+    if (this.processing !== null) {
+      return;
+    }
+    if (delta !== undefined) {
+      this.currentExecutionCostInCurrentTick = 0;
+    }
+    if (!this.pending.length) {
+      return;
+    }
+    if (this.allowedExecutionCostWithinTick !== null && delta === undefined && this.currentExecutionCostInCurrentTick + (this.pending[0].cost || 0) > this.allowedExecutionCostWithinTick) {
+      return;
+    }
+    this.processing = this.pending.shift() || null;
+    if (this.processing) {
+      this.currentExecutionCostInCurrentTick += this.processing.cost || 0;
+      // this.callback(this.processing);
+      this.processing.execute();
+    }
+  }
+
+  public clear(): void {
+    this.pending.splice(0, this.pending.length);
+  }
+
+  public resolve(task: QueueTask<T>, resolve: ((value: void | PromiseLike<void>) => void)): void {
+    resolve();
+    this.processing = null;
+    this.dequeue();
+  }
+  public reject(task: QueueTask<T>, reject: ((value: unknown | PromiseLike<unknown>) => void), reason: unknown): void {
+    reject(reason);
+    this.processing = null;
+    this.dequeue();
+  }
+}

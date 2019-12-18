386a0ea70cfbf5c97fbbb2706b2d2b5c2f42184b
Fred Kleuver <fred@avurad.nl>
2019-12-18 10:43:55 +0100

    chore(aot): skip tests for now

--- packages/__tests__/aot/smoke-tests.spec.ts
+++ packages/__tests__/aot/smoke-tests.spec.ts
@@ -6,7 +6,7 @@ import { assert } from '@aurelia/testing';
 // NOTE: These tests are not meant to be even close to exhaustive. That's what the 262 test suite is for.
 // These tests exist solely for having an easy way to quickly test some high-level things when a feature is not yet ready for exposure to the 262 test suite.
 
-describe('AOT (smoke tests)', function () {
+describe.skip('AOT (smoke tests)', function () {
   async function execute(content: string) {
     const container = DI.createContainer();
     container.register(

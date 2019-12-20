e8bd7b2904aba787364c9baa535a252a40710a4c
Fred Kleuver <fred@avurad.nl>
2019-12-21 00:21:27 +0100

    refactor(viewport-scope): use template part substitution instead of replaceable

--- packages/router/src/resources/viewport-scope.ts
+++ packages/router/src/resources/viewport-scope.ts
@@ -1,31 +1,39 @@
 import {
   bindable,
-  IController,
   INode,
   LifecycleFlags,
   customElement,
   CustomElement,
   ICompiledCustomElementController,
+  ICustomElementViewModel,
+  ICustomElementController,
+  CustomElementDefinition,
+  IDryCustomElementController,
+  PartialCustomElementDefinitionParts,
+  PartialCustomElementDefinition,
+  IController,
+  IHydratedController,
 } from '@aurelia/runtime';
 import { IRouter } from '../router';
 import { IViewportScopeOptions, ViewportScope } from '../viewport-scope';
-import { IContainer } from '@aurelia/kernel';
+import { IContainer, Writable } from '@aurelia/kernel';
 
 export const ParentViewportScope = CustomElement.createInjectable();
 
 @customElement({
   name: 'au-viewport-scope',
-  template: '<template><template replaceable></template></template>',
+  template: '<template></template>',
+  containerless: true,
   injectable: ParentViewportScope
 })
-export class ViewportScopeCustomElement {
+export class ViewportScopeCustomElement implements ICustomElementViewModel<Element> {
   @bindable public name: string = 'default';
   @bindable public catches: string = '';
   @bindable public collection: boolean = false;
   @bindable public source: unknown[] | null = null;
   public viewportScope: ViewportScope | null = null;
 
-  public $controller!: IController; // This is set by the controller after this instance is constructed
+  public readonly $controller!: ICustomElementController<Element, this>;
 
   private readonly element: Element;
 
@@ -36,8 +44,35 @@ export class ViewportScopeCustomElement {
     @INode element: INode,
     @IContainer private container: IContainer,
     @ParentViewportScope private readonly parent: ViewportScopeCustomElement,
+    @IController private readonly parentController: IHydratedController,
   ) {
     this.element = element as HTMLElement;
+  }
+
+  public create(
+    controller: IDryCustomElementController<Element, this>,
+    parentContainer: IContainer,
+    definition: CustomElementDefinition,
+    parts: PartialCustomElementDefinitionParts | undefined,
+  ): PartialCustomElementDefinition {
+    // TODO(fkleuver): describe this somewhere in the docs instead
+    // Under the condition that there is no `replace` attribute on this custom element's declaration,
+    // and this custom element is containerless, its content will be placed in a part named 'default'
+    // See packages/jit-html/src/template-binder.ts line 411 (`replace = 'default';`) for the logic that governs this.
+
+    // We could tidy this up into a formal api in the future. For now, there are two ways to do this:
+    // 1. inject the `@ITargetedInstruction` (IHydrateElementInstruction) and grab .parts['default'] from there, manually creating a view factory from that, etc.
+    // 2. what we're doing right here: grab the 'default' part from the create hook and return it as the definition, telling the render context to use that part to compile this element instead
+    // This effectively causes this element to render its declared content as if it was its own template.
+
+    // We do need to set `containerless` to true on the part definition so that the correct projector is used since parts default to non-containerless.
+    // Otherwise, the controller will try to do `appendChild` on a comment node when it has to do `insertBefore`.
+
+    // Also, in this particular scenario (specific to viewport-scope) we need to clone the part so as to prevent the resulting compiled definition
+    // from ever being cached. That's the only reason why we're spreading the part into a new object for `getOrCreate`. If we didn't clone the object, this specific element wouldn't work correctly.
+
+    const part = parts!['default'];
+    return CustomElementDefinition.getOrCreate({ ...part, containerless: true });
   }
 
   public afterCompile(controller: ICompiledCustomElementController) {
@@ -78,6 +113,9 @@ export class ViewportScopeCustomElement {
 
   public beforeBind(flags: LifecycleFlags): void {
     this.isBound = true;
+
+    (this.$controller as Writable<ICustomElementController>).scope = this.parentController.scope!;
+
     this.connect();
     if (this.viewportScope !== null) {
       this.viewportScope.beforeBind();

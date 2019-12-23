3a87fc25eb1dc0aee135ecd9d9c67d4c4b2d363c
bigopon.777@gmail.com <bigopon.777@gmail.com>
2019-12-23 20:42:10 +1100

    chore(lint): rename to fix linting issues

--- packages/__tests__/3-runtime-html/array-index-observer.spec.ts
+++ packages/__tests__/3-runtime-html/array-index-observer.spec.ts
@@ -2,27 +2,27 @@ import { TestContext, assert } from '@aurelia/testing';
 import { IInputElement, ValueAttributeObserver } from '@aurelia/runtime-html';
 import { LifecycleFlags, ArrayIndexObserver, ISubscriber } from '@aurelia/runtime';
 
-describe('3-runtime-html/array-index-observer.spec.ts', function() {
-  it('observer array index correctly', function() {
+describe('3-runtime-html/array-index-observer.spec.ts', function () {
+  it('observer array index correctly', function () {
     const { observerLocator } = createFixture();
     const arr = [1, 2, 3];
-    const index_0_observer = observerLocator.getObserver(LifecycleFlags.none, arr, '0') as ArrayIndexObserver;
-    
+    const indexZeroObserver = observerLocator.getObserver(LifecycleFlags.none, arr, '0') as ArrayIndexObserver;
+
     let callcount = 0;
-    const index_0_subscriber: ISubscriber = {
+    const indexZeroSubscriber: ISubscriber = {
       handleChange() {
         callcount++;
       }
-    }
-    index_0_observer.subscribe(index_0_subscriber);
-    assert.instanceOf(index_0_observer, ArrayIndexObserver);
+    };
+    indexZeroObserver.subscribe(indexZeroSubscriber);
+    assert.instanceOf(indexZeroObserver, ArrayIndexObserver);
     arr[0] = 5;
-    assert.strictEqual(index_0_observer.currentValue, 1);
+    assert.strictEqual(indexZeroObserver.currentValue, 1);
     arr.splice(0, 1, 4);
-    assert.strictEqual(index_0_observer.currentValue, 4);
+    assert.strictEqual(indexZeroObserver.currentValue, 4);
     assert.strictEqual(callcount, 1);
 
-    index_0_observer.setValue(0, LifecycleFlags.none);
+    indexZeroObserver.setValue(0, LifecycleFlags.none);
     assert.strictEqual(callcount, 2);
     assert.strictEqual(arr[0], 0);
   });

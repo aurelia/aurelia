57e96077a4136098c3792ec4e9f49df8868e03c2
bigopon.777@gmail.com <bigopon.777@gmail.com>
2019-12-23 20:41:47 +1100

    fix(index-observation): simplify keyed access connect

--- packages/runtime/src/binding/ast.ts
+++ packages/runtime/src/binding/ast.ts
@@ -547,17 +547,8 @@ export class AccessKeyedExpression implements IAccessKeyedExpression {
     if (obj instanceof Object) {
       this.key.connect(flags, scope, binding, part);
       const key = this.key.evaluate(flags, scope, null, part);
-
-      if (Array.isArray(obj) && isNumeric(key)) {
-        // Only observe array indexers in proxy mode
-        if (flags & LifecycleFlags.proxyStrategy) {
-          binding.observeProperty(flags, obj, key as unknown as string);
-        }
-      } else {
-        // observe the property represented by the key as long as it's not an array indexer
-        // (note: string indexers behave the same way as numeric indexers as long as they represent numbers)
-        binding.observeProperty(flags, obj, key as string);
-      }
+      // (note: string indexers behave the same way as numeric indexers as long as they represent numbers)
+      binding.observeProperty(flags, obj, key as string);
     }
   }
 

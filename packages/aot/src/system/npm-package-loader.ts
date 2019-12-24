f0045058c332c002edbcc23a815ff659a7d9c24e
Fred Kleuver <fred@avurad.nl>
2019-12-24 09:32:43 +0100

    refactor(aot): rework NPMPackageLoader to support lazy loading packages

--- packages/aot/src/system/npm-package-loader.ts
+++ packages/aot/src/system/npm-package-loader.ts
@@ -14,6 +14,7 @@ import {
 import {
   basename,
   dirname,
+  join,
 } from 'path';
 
 function countSlashes(path: string): number {
@@ -165,31 +166,52 @@ export class NPMPackageLoader {
   }
 
   public async loadEntryPackage(
-    projectDir: string,
+    pathOrFile: string | IFile,
   ): Promise<NPMPackage> {
-    const start = PLATFORM.now();
-
-    this.logger.info(`load()`);
-
-    projectDir = normalizePath(projectDir);
-
-    const entryPkg = await this.loadPackageCore(projectDir, null);
-    await entryPkg.loadDependencies();
-
-    this.pkgPromiseCache.clear();
-
-    const end = PLATFORM.now();
-
-    const packages = Array.from(this.pkgCache.values());
-    const pkgCount = packages.length;
-    const fileCount = packages.reduce((count, pkg) => count + pkg.files.length, 0);
-
-    this.logger.info(`Discovered ${fileCount} files across ${pkgCount} packages in ${Math.round(end - start)}ms`);
-    for (const pkg of packages) {
-      this.logger.info(`- ${pkg.pkgName}: ${pkg.files.length} files`);
+    let path: string;
+    let file: IFile | undefined;
+    let isFile: boolean;
+    if (typeof pathOrFile === 'string') {
+      path = pathOrFile;
+      file = void 0;
+      isFile = false;
+    } else {
+      path = pathOrFile.path;
+      file = pathOrFile;
+      isFile = true;
     }
 
-    return entryPkg;
+    this.logger.info(`loadEntryPackage(path="${path}")`);
+
+    const fs = this.fs;
+    if (!isFile) {
+      const stat = await fs.stat(path);
+      isFile = stat.isFile();
+    }
+
+    if (isFile) {
+      const originalPath = path;
+      path = dirname(path);
+
+      while (true) {
+        // eslint-disable-next-line no-await-in-loop
+        if (await fs.isReadable(join(path, 'package.json'))) {
+          break;
+        }
+
+        if (path === (path = dirname(path))) {
+          throw new Error(`No package.json could be found in the directory of "${originalPath}" or any of its parent directories.`);
+        }
+      }
+    }
+
+    path = normalizePath(path);
+
+    return this.loadPackageCore(path, null, file);
+  }
+
+  public hasCachedPackage(refName: string): boolean {
+    return this.pkgCache.has(refName);
   }
 
   public getCachedPackage(refName: string): NPMPackage {
@@ -202,35 +224,56 @@ export class NPMPackageLoader {
 
   /** @internal */
   public async loadPackage(issuer: NPMPackageDependency): Promise<NPMPackage> {
-    const pkgPromiseCache = this.pkgPromiseCache;
+    const pkgCache = this.pkgCache;
 
-    let pkgPromise = pkgPromiseCache.get(issuer.refName);
-    if (pkgPromise === void 0) {
-      pkgPromise = this.loadPackageCore(null, issuer);
-      // Multiple deps may request the same package to load before one of them finished
-      // so we store the promise to ensure the action is only invoked once.
-      pkgPromiseCache.set(issuer.refName, pkgPromise);
+    const pkg = pkgCache.get(issuer.refName);
+    if (pkg === void 0) {
+      const pkgPromiseCache = this.pkgPromiseCache;
+      let pkgPromise = pkgPromiseCache.get(issuer.refName);
+      if (pkgPromise === void 0) {
+        pkgPromise = this.loadPackageCore(null, issuer);
+        // Multiple deps may request the same package to load before one of them finished
+        // so we store the promise to ensure the action is only invoked once.
+        pkgPromiseCache.set(issuer.refName, pkgPromise);
+      }
+
+      return pkgPromise;
     }
 
-    return pkgPromise;
+    return pkg;
   }
 
-  private async loadPackageCore(dir: null, issuer: NPMPackageDependency): Promise<NPMPackage>;
-  private async loadPackageCore(dir: string, issuer: null): Promise<NPMPackage>;
-  private async loadPackageCore(dir: string | null, issuer: NPMPackageDependency | null): Promise<NPMPackage> {
-    this.logger.info(`loadPackageCore(\n  dir: ${dir},\n  issuer: ${issuer === null ? 'null' : issuer.issuer.pkgName}\n)`);
+  private async loadPackageCore(
+    dir: null,
+    issuer: NPMPackageDependency,
+    entryFile?: IFile,
+  ): Promise<NPMPackage>;
+  private async loadPackageCore(
+    dir: string,
+    issuer: null,
+    entryFile?: IFile,
+  ): Promise<NPMPackage>;
+  private async loadPackageCore(
+    dir: string | null,
+    issuer: NPMPackageDependency | null,
+    entryFile?: IFile,
+  ): Promise<NPMPackage> {
 
     const fs = this.fs;
     const pkgCache = this.pkgCache;
 
     const refName = dir === null ? issuer!.refName : dir;
+
     let pkg = pkgCache.get(refName);
     if (pkg === void 0) {
       if (dir === null) {
         dir = await this.resolvePackagePath(issuer!);
       }
       const pkgPath = joinPath(dir, 'package.json');
+
+      const start = PLATFORM.now();
       const files = await fs.getFiles(dir);
+      const end = PLATFORM.now();
 
       const pkgJsonFile = files.find(x => x.path === pkgPath);
       if (pkgJsonFile === void 0) {
@@ -238,27 +281,42 @@ export class NPMPackageLoader {
       }
 
       const pkgJsonFileContent = await pkgJsonFile.getContent();
-      pkg = new NPMPackage(this, files, issuer, pkgJsonFile, dir, pkgJsonFileContent);
+      pkg = new NPMPackage(this, files, issuer, pkgJsonFile, dir, pkgJsonFileContent, entryFile);
+      this.logger.info(`loadPackageCore(\n  dir: ${dir},\n  issuer: ${issuer === null ? 'null' : issuer.issuer.pkgName}\n  specifier: ${refName}\n) discovered ${files.length} files in ${Math.round(end - start)}ms`);
+
       pkgCache.set(refName, pkg);
+
+      // Delete the cached promise since it's not needed anymore; pkg is retrieved directly from normal cache now
+      this.pkgPromiseCache.delete(refName);
     }
 
     return pkg;
   }
 
   private async resolvePackagePath(dep: NPMPackageDependency): Promise<string> {
-    const pkgResolvePromiseCache = this.pkgResolvePromiseCache;
+    const pkgResolveCache = this.pkgResolveCache;
     const refName = dep.refName;
+    const resolved = pkgResolveCache.get(refName);
+    if (resolved === void 0) {
+      const pkgResolvePromiseCache = this.pkgResolvePromiseCache;
 
-    let resolvePromise = pkgResolvePromiseCache.get(refName);
-    if (resolvePromise === void 0) {
-      resolvePromise = this.resolvePackagePathCore(dep);
+      let resolvePromise = pkgResolvePromiseCache.get(refName);
+      if (resolvePromise === void 0) {
+        this.logger.trace(`resolvePackagePath(refName=${refName})`);
 
-      // Multiple deps may request the same refName to be resolved before one of them finished
-      // so we store the promise to ensure the action is only invoked once.
-      pkgResolvePromiseCache.set(refName, resolvePromise);
+        resolvePromise = this.resolvePackagePathCore(dep);
+
+        // Multiple deps may request the same refName to be resolved before one of them finished
+        // so we store the promise to ensure the action is only invoked once.
+        pkgResolvePromiseCache.set(refName, resolvePromise);
+      } else {
+        this.logger.trace(`resolvePackagePath(refName=${refName}) (from cached promise)`);
+      }
+
+      return resolvePromise;
     }
 
-    return resolvePromise;
+    return resolved;
   }
 
   private async resolvePackagePathCore(dep: NPMPackageDependency): Promise<string> {
@@ -269,20 +327,31 @@ export class NPMPackageLoader {
     let resolvedPath = pkgResolveCache.get(refName);
     if (resolvedPath === void 0) {
       let dir = dep.issuer.dir;
-      // eslint-disable-next-line no-constant-condition
-      while (true) {
-        resolvedPath = joinPath(dir, 'node_modules', refName, 'package.json');
+      // Try top-level node_modules first as this eliminates a lot of fs reads from traversing
+      if (dir.includes('node_modules')) {
+        const [topLevelDir] = dir.split('node_modules');
+        resolvedPath = joinPath(topLevelDir, 'node_modules', refName, 'package.json');
         // eslint-disable-next-line no-await-in-loop
-        if (await fs.isReadable(resolvedPath)) {
-          break;
+        if (!(await fs.isReadable(resolvedPath))) {
+          resolvedPath = void 0;
         }
+      }
 
-        const parent = normalizePath(dirname(dir));
-        if (parent === dir) {
-          throw new Error(`Unable to resolve npm dependency "${refName}"`);
+      if (resolvedPath === void 0) {
+        while (true) {
+          resolvedPath = joinPath(dir, 'node_modules', refName, 'package.json');
+          // eslint-disable-next-line no-await-in-loop
+          if (await fs.isReadable(resolvedPath)) {
+            break;
+          }
+
+          const parent = normalizePath(dirname(dir));
+          if (parent === dir) {
+            throw new Error(`Unable to resolve npm dependency "${refName}" (requested from ${dep.issuer.pkgJsonFile.path})`);
+          }
+
+          dir = parent;
         }
-
-        dir = parent;
       }
 
       const realPath = normalizePath(await fs.getRealPath(resolvedPath));
@@ -294,6 +363,7 @@ export class NPMPackageLoader {
       }
       resolvedPath = normalizePath(dirname(realPath));
       pkgResolveCache.set(refName, resolvedPath);
+      this.pkgResolvePromiseCache.delete(refName);
     }
 
     return resolvedPath;
@@ -301,10 +371,10 @@ export class NPMPackageLoader {
 }
 
 export class NPMPackage {
-  public readonly pkgJson: Package;
   public readonly pkgName: string;
   public readonly isAureliaPkg: boolean;
   public readonly isEntryPoint: boolean;
+  public readonly hasSpecificEntryFile: boolean;
   public readonly entryFile: IFile;
   public readonly deps: readonly NPMPackageDependency[];
 
@@ -317,36 +387,42 @@ export class NPMPackage {
     public readonly pkgJsonFile: IFile,
     public readonly dir: string,
     pkgJsonFileContent: string,
+    entryFile?: IFile,
   ) {
     this.container = loader.container;
 
-    const pkgJson = this.pkgJson = JSON.parse(pkgJsonFileContent) as Package;
+    const pkgJson = JSON.parse(pkgJsonFileContent) as Package;
     const pkgName = this.pkgName = typeof pkgJson.name === 'string' ? pkgJson.name : dir.slice(dir.lastIndexOf('/') + 1);
     const pkgModuleOrMain = typeof pkgJson.module === 'string' ? pkgJson.module : pkgJson.main;
     const isAureliaPkg = this.isAureliaPkg = pkgName.startsWith('@aurelia');
     const isEntryPoint = this.isEntryPoint = issuer === null;
 
-    let entryFilePath = isAureliaPkg ? 'src/index.ts' : pkgModuleOrMain;
-    let entryFile: IFile | undefined;
-    if (entryFilePath === void 0) {
-      entryFile = determineEntryFileByConvention(files, isEntryPoint);
-    } else {
-      if (entryFilePath.startsWith('.')) {
-        entryFilePath = entryFilePath.slice(1);
-      }
-      entryFile = files.find(x => x.path.endsWith(entryFilePath!));
-      if (entryFile === void 0) {
-        const withJs = `${entryFilePath}.js`;
-        entryFile = files.find(x => x.path.endsWith(withJs));
+    if (entryFile === void 0) {
+      this.hasSpecificEntryFile = false;
+
+      let entryFilePath = isAureliaPkg ? 'src/index.ts' : pkgModuleOrMain;
+      if (entryFilePath === void 0) {
+        entryFile = determineEntryFileByConvention(files, isEntryPoint);
+      } else {
+        if (entryFilePath.startsWith('.')) {
+          entryFilePath = entryFilePath.slice(1);
+        }
+        entryFile = files.find(x => x.path.endsWith(entryFilePath!));
         if (entryFile === void 0) {
-          const withIndexJs = joinPath(entryFilePath, 'index.js');
-          entryFile = files.find(x => x.path.endsWith(withIndexJs));
+          const withJs = `${entryFilePath}.js`;
+          entryFile = files.find(x => x.path.endsWith(withJs));
+          if (entryFile === void 0) {
+            const withIndexJs = joinPath(entryFilePath, 'index.js');
+            entryFile = files.find(x => x.path.endsWith(withIndexJs));
+          }
         }
       }
-    }
 
-    if (entryFile === void 0) {
-      throw new Error(`No entry file could be located for package ${pkgName}`);
+      if (entryFile === void 0) {
+        throw new Error(`No entry file could be located for package ${pkgName}`);
+      }
+    } else {
+      this.hasSpecificEntryFile = true;
     }
 
     this.entryFile = entryFile;
@@ -357,54 +433,11 @@ export class NPMPackage {
       this.deps = PLATFORM.emptyArray;
     }
   }
-
-  /** @internal */
-  public async loadDependencies(): Promise<void> {
-    await Promise.all(this.deps.map(loadDependency));
-  }
-}
-
-async function loadDependency(dep: NPMPackageDependency): Promise<void> {
-  await dep.load();
-  await dep.pkg.loadDependencies();
 }
 
 export class NPMPackageDependency {
-  public get pkg(): NPMPackage {
-    const pkg = this._pkg;
-    if (pkg === void 0) {
-      throw new Error(`Package ${this.refName} is not yet loaded`);
-    }
-    return pkg;
-  }
-
-  private _pkg: NPMPackage | undefined = void 0;
-  private loadPromise: Promise<void> | undefined = void 0;
-
   public constructor(
     public readonly issuer: NPMPackage,
     public readonly refName: string,
   ) {}
-
-  /** @internal */
-  public async load(): Promise<void> {
-    if (this._pkg === void 0) {
-      if (this.loadPromise === void 0) {
-        // Multiple deps may request the same package to load before one of them finished
-        // so we store the promise to ensure the action is only invoked once.
-        this.loadPromise = this.loadCore();
-      }
-
-      return this.loadPromise;
-    }
-  }
-
-  private async loadCore(): Promise<void> {
-    this._pkg = await this.issuer.loader.loadPackage(this);
-
-    this.loadPromise = void 0;
-
-    // Freeze it as an extra measure to prove that we're not getting race conditions
-    Object.freeze(this);
-  }
 }

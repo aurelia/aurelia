b0d19425637bf11e1e4264949f71eb2adccccb5c
Fred Kleuver <fred@avurad.nl>
2020-02-21 21:57:26 +0100

    refactor(runtime-html-browser): cleanup browser scheduler

--- packages/runtime-html-browser/src/browser-scheduler.ts
+++ packages/runtime-html-browser/src/browser-scheduler.ts
@@ -1,327 +1,231 @@
 /* eslint-disable no-await-in-loop */
-import { IContainer, bound } from '@aurelia/kernel';
-import { IDOM, IScheduler, TaskQueuePriority, TaskQueue, IClock, TaskCallback, QueueTaskOptions, Task, DOM, ITaskQueue, ITask, QueueTaskTargetOptions } from '@aurelia/runtime';
-import { HTMLDOM } from '@aurelia/runtime-html';
+import {
+  IContainer,
+  bound,
+  isNativeFunction,
+} from '@aurelia/kernel';
+import {
+  IDOM,
+  QueueTaskOptions,
+  TaskQueuePriority,
+  IScheduler,
+  TaskQueue,
+  IClock,
+  TaskCallback,
+  Task,
+  DOM,
+  ITaskQueue,
+  ITask,
+  QueueTaskTargetOptions,
+} from '@aurelia/runtime';
+import {
+  HTMLDOM,
+} from '@aurelia/runtime-html';
 
-// Note on the flush requestors: we're probably overdoing it here with the binds and all the wrapping.
-// Just want to start off with something as robust as possible, add many tests first so that we're sure it all works, etc.
-// Once all the proper tests are in place, we can then start simplifying and rely on those tests to tell us precisely which things are necessary and which aren't.
-// Besides looking at test results, we also need to look at benchmark results, memory usage, memory leaks, etc.
+// Create a microtask queue, trying the available options starting with the most performant
+createMicrotaskFlushRequestor.called = false;
+function createMicrotaskFlushRequestor(wnd: Window, flush: () => void) {
+  if (createMicrotaskFlushRequestor.called) {
+    throw new Error('Cannot have more than one global MicrotaskFlushRequestor');
+  }
+  createMicrotaskFlushRequestor.called = true;
 
-function createQueueMicrotaskFlushRequestor(window: Window, flush: () => void) {
-  return (function ($window: Window, $flush: () => void) {
-    const callFlush = function () {
-      $flush();
-      // eslint-disable-next-line no-extra-bind
-    }.bind(void 0);
+  function callFlush() {
+    flush();
+  }
 
+  if (
+    typeof wnd.queueMicrotask === 'function' &&
+    isNativeFunction(wnd.queueMicrotask)
+  ) {
     return function () {
-      $window.queueMicrotask(callFlush);
-      // eslint-disable-next-line no-extra-bind
-    }.bind(void 0);
-  })(window, flush);
-}
-
-function createPromiseFlushRequestor(flush: () => void) {
-  return (function ($flush: () => void) {
-    const callFlush = function () {
-      $flush();
-      // eslint-disable-next-line no-extra-bind
-    }.bind(void 0);
-
-    // eslint-disable-next-line compat/compat
+      wnd.queueMicrotask(callFlush);
+    };
+  } else if (isNativeFunction(Promise)) {
     const p = Promise.resolve();
     return function () {
       // eslint-disable-next-line @typescript-eslint/no-floating-promises
       p.then(callFlush);
-      // eslint-disable-next-line no-extra-bind
-    }.bind(void 0);
-  })(flush);
-}
+    };
+  } else {
+    const observer = new (wnd as unknown as { MutationObserver: typeof MutationObserver }).MutationObserver(callFlush);
+    const node = wnd.document.createTextNode('a');
+    const flip = { a: 'b', b: 'a' } as const;
 
-function createMutationObserverFlushRequestor(window: Window, flush: () => void) {
-  return (function ($window: Window, $flush: () => void) {
-    const callFlush = function () {
-      $flush();
-      // eslint-disable-next-line no-extra-bind
-    }.bind(void 0);
-
-    const observer = new ($window as unknown as { MutationObserver: typeof MutationObserver }).MutationObserver(callFlush);
-    const node = $window.document.createTextNode('a');
-    // eslint-disable-next-line compat/compat
-    const flip = Object.assign(Object.create(null), { a: 'b', b: 'a' });
-
-    let val = 'a';
+    let val = 'a' as 'a' | 'b';
     observer.observe(node, { characterData: true });
 
     return function () {
       node.data = val = flip[val];
-      // eslint-disable-next-line no-extra-bind
-    }.bind(void 0);
-  })(window, flush);
+    };
+  }
 }
 
-// Create a microtask queue, trying the available options starting with the most performant
-const createMicrotaskFlushRequestor = (function () {
-  // Ensure we only ever create one requestor (at least, per loaded bundle..).
-  let called = false;
+createSetTimeoutFlushRequestor.called = false;
+function createSetTimeoutFlushRequestor(wnd: Window, flush: () => void) {
+  if (createSetTimeoutFlushRequestor.called) {
+    throw new Error('Cannot have more than one global SetTimeoutFlushRequestor');
+  }
+  createSetTimeoutFlushRequestor.called = true;
 
-  return function (wnd: Window, flush: () => void) {
-    if (called) {
-      throw new Error('Cannot have more than one global MicrotaskFlushRequestor');
-    }
-    called = true;
+  let handle = -1;
 
-    if (wnd.queueMicrotask !== void 0 && wnd.queueMicrotask.toString().includes('[native code]')) {
-      return createQueueMicrotaskFlushRequestor(wnd, flush);
-    } else if (Promise.toString().includes('[native code]')) {
-      return createPromiseFlushRequestor(flush);
-    } else {
-      return createMutationObserverFlushRequestor(wnd, flush);
+  function callFlush() {
+    if (handle > -1) {
+      handle = -1;
+      flush();
     }
+  }
+
+  return {
+    cancel() {
+      if (handle > -1) {
+        wnd.clearTimeout(handle);
+        handle = -1;
+      }
+    },
+    request() {
+      if (handle === -1) {
+        handle = wnd.setTimeout(callFlush, 0);
+      }
+    },
   };
-})();
+}
 
-const createPostMessageFlushRequestor = (function () {
-  let called = false;
+createRequestAnimationFrameFlushRequestor.called = false;
+function createRequestAnimationFrameFlushRequestor(wnd: Window, flush: () => void) {
+  if (createRequestAnimationFrameFlushRequestor.called) {
+    throw new Error('Cannot have more than one global RequestAnimationFrameFlushRequestor');
+  }
+  createRequestAnimationFrameFlushRequestor.called = true;
 
-  return function (window: Window, flush: () => void) {
-    if (called) {
-      throw new Error('Cannot have more than one global PostMessageFlushRequestor');
+  let handle = -1;
+
+  function callFlush() {
+    if (handle > -1) {
+      handle = -1;
+      flush();
     }
-    called = true;
+  }
 
-    return (function ($window: Window, $flush: () => void) {
-      const handleMessage = function (event: MessageEvent) {
-        if (event.data === 'au-message' && event.source === $window) {
-          $flush();
-        }
-        // eslint-disable-next-line no-extra-bind
-      }.bind(void 0);
-
-      $window.addEventListener('message', handleMessage);
-
-      return function () {
-        $window.postMessage('au-message', $window.origin);
-        // eslint-disable-next-line no-extra-bind
-      }.bind(void 0);
-    })(window, flush);
+  return {
+    cancel() {
+      if (handle > -1) {
+        wnd.cancelAnimationFrame(handle);
+        handle = -1;
+      }
+    },
+    request() {
+      if (handle === -1) {
+        handle = wnd.requestAnimationFrame(callFlush);
+      }
+    },
   };
-})();
+}
 
-const createSetTimeoutFlushRequestor = (function () {
-  let called = false;
+createPostRequestAnimationFrameFlushRequestor.called = false;
+function createPostRequestAnimationFrameFlushRequestor(wnd: Window, flush: () => void) {
+  if (createPostRequestAnimationFrameFlushRequestor.called) {
+    throw new Error('Cannot have more than one global PostRequestAnimationFrameFlushRequestor');
+  }
+  createPostRequestAnimationFrameFlushRequestor.called = true;
 
-  return function (window: Window, flush: () => void) {
-    if (called) {
-      throw new Error('Cannot have more than one global SetTimeoutFlushRequestor');
+  let rafHandle = -1;
+  let timeoutHandle = -1;
+  function callFlush() {
+    if (timeoutHandle > -1) {
+      timeoutHandle = -1;
+      flush();
     }
-    called = true;
+  }
 
-    return (function ($window: Window, $flush: () => void) {
-      let handle = -1;
-
-      const callFlush = function () {
-        if (handle > -1) {
-          handle = -1;
-          $flush();
-        }
-        // eslint-disable-next-line no-extra-bind
-      }.bind(void 0);
-
-      const cancel = function () {
-        if (handle > -1) {
-          $window.clearTimeout(handle);
-          handle = -1;
-        }
-        // eslint-disable-next-line no-extra-bind
-      }.bind(void 0);
-
-      const request = function () {
-        if (handle === -1) {
-          handle = $window.setTimeout(callFlush, 0);
-        }
-        // eslint-disable-next-line no-extra-bind
-      }.bind(void 0);
-
-      return {
-        cancel,
-        request,
-      };
-    })(window, flush);
-  };
-})();
-
-const createRequestAnimationFrameFlushRequestor = (function () {
-  let called = false;
-
-  return function (window: Window, flush: () => void) {
-    if (called) {
-      throw new Error('Cannot have more than one global RequestAnimationFrameFlushRequestor');
+  function queueFlush() {
+    if (rafHandle > -1) {
+      rafHandle = -1;
+      if (timeoutHandle === -1) {
+        timeoutHandle = wnd.setTimeout(callFlush, 0);
+      }
     }
-    called = true;
+  }
 
-    return (function ($window: Window, $flush: () => void) {
-      let handle = -1;
-
-      const callFlush = function () {
-        if (handle > -1) {
-          handle = -1;
-          $flush();
-        }
-        // eslint-disable-next-line no-extra-bind
-      }.bind(void 0);
-
-      const cancel = function () {
-        if (handle > -1) {
-          $window.cancelAnimationFrame(handle);
-          handle = -1;
-        }
-        // eslint-disable-next-line no-extra-bind
-      }.bind(void 0);
-
-      const request = function () {
-        if (handle === -1) {
-          handle = $window.requestAnimationFrame(callFlush);
-        }
-        // eslint-disable-next-line no-extra-bind
-      }.bind(void 0);
-
-      return {
-        cancel,
-        request,
-      };
-    })(window, flush);
+  return {
+    cancel() {
+      if (rafHandle > -1) {
+        wnd.cancelAnimationFrame(rafHandle);
+        rafHandle = -1;
+      }
+      if (timeoutHandle > -1) {
+        wnd.clearTimeout(timeoutHandle);
+        timeoutHandle = -1;
+      }
+    },
+    request() {
+      if (rafHandle === -1) {
+        rafHandle = wnd.requestAnimationFrame(queueFlush);
+      }
+    },
   };
-})();
-
-const createPostRequestAnimationFrameFlushRequestor = (function () {
-  let called = false;
-
-  return function (window: Window, flush: () => void) {
-    if (called) {
-      throw new Error('Cannot have more than one global PostRequestAnimationFrameFlushRequestor');
-    }
-    called = true;
-
-    return (function ($window: Window, $flush: () => void) {
-      let rafHandle = -1;
-      let timeoutHandle = -1;
-
-      const callFlush = function () {
-        if (timeoutHandle > -1) {
-          timeoutHandle = -1;
-          $flush();
-        }
-        // eslint-disable-next-line no-extra-bind
-      }.bind(void 0);
-
-      const queueFlush = function () {
-        if (rafHandle > -1) {
-          rafHandle = -1;
-          if (timeoutHandle === -1) {
-            timeoutHandle = $window.setTimeout(callFlush, 0);
-          }
-        }
-        // eslint-disable-next-line no-extra-bind
-      }.bind(void 0);
-
-      const cancel = function () {
-        if (rafHandle > -1) {
-          $window.cancelAnimationFrame(rafHandle);
-          rafHandle = -1;
-        }
-        if (timeoutHandle > -1) {
-          $window.clearTimeout(timeoutHandle);
-          timeoutHandle = -1;
-        }
-        // eslint-disable-next-line no-extra-bind
-      }.bind(void 0);
-
-      const request = function () {
-        if (rafHandle === -1) {
-          rafHandle = $window.requestAnimationFrame(queueFlush);
-        }
-        // eslint-disable-next-line no-extra-bind
-      }.bind(void 0);
-
-      return {
-        cancel,
-        request,
-      };
-    })(window, flush);
-  };
-})();
+}
 
 type WindowWithIdleCallback = Window & {
   requestIdleCallback?(cb: () => void, options?: { timeout: number }): number;
   cancelIdleCallback?(handle: number): void;
 };
 
-const createRequestIdleCallbackFlushRequestor = (function () {
-  let called = false;
+createRequestIdleCallbackFlushRequestor.called = false;
+function createRequestIdleCallbackFlushRequestor(wnd: WindowWithIdleCallback, flush: () => void) {
+  if (createRequestIdleCallbackFlushRequestor.called) {
+    throw new Error('Cannot have more than one global RequestIdleCallbackFlushRequestor');
+  }
+  createRequestIdleCallbackFlushRequestor.called = true;
 
-  return function (window: WindowWithIdleCallback, flush: () => void) {
-    if (called) {
-      throw new Error('Cannot have more than one global RequestIdleCallbackFlushRequestor');
+  let handle = -1;
+
+  function callFlush() {
+    if (handle > -1) {
+      handle = -1;
+      flush();
     }
-    called = true;
+  }
 
-    const hasNative = window.requestIdleCallback !== void 0 && window.requestIdleCallback.toString().includes('[native code]');
-
-    return (function ($window: WindowWithIdleCallback, $flush: () => void) {
-      let handle = -1;
-
-      const options = { timeout: 1000 }; // TODO: make this configurable
-
-      const callFlush = function () {
+  if (
+    typeof wnd.requestIdleCallback === 'function' &&
+    isNativeFunction(wnd.requestIdleCallback)
+  ) {
+    return {
+      cancel() {
         if (handle > -1) {
+          // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+          wnd.cancelIdleCallback!(handle);
           handle = -1;
-          $flush();
         }
-        // eslint-disable-next-line no-extra-bind
-      }.bind(void 0);
-
-      const cancel = hasNative
-        ? function () {
-          if (handle > -1) {
-            $window.cancelIdleCallback!(handle);
-            handle = -1;
-          }
-          // eslint-disable-next-line no-extra-bind
-        }.bind(void 0)
-        : function () {
-          if (handle > -1) {
-            $window.clearTimeout(handle);
-            handle = -1;
-          }
-          // eslint-disable-next-line no-extra-bind
-        }.bind(void 0);
-
-      const request = hasNative
-        ? function () {
-          if (handle === -1) {
-            handle = $window.requestIdleCallback!(callFlush, options);
-          }
-          // eslint-disable-next-line no-extra-bind
-        }.bind(void 0)
-        : function () {
-          if (handle === -1) {
-            // Instead of trying anything fancy with event handler debouncers (we could do that if there was a request for it),
-            // we just wait 45ms which is approximately the interval in a native idleCallback loop in chrome, to at least make it look
-            // the same from a timing perspective
-            handle = $window.setTimeout(callFlush, 45);
-          }
-          // eslint-disable-next-line no-extra-bind
-        }.bind(void 0);
-
-      return {
-        cancel,
-        request,
-      };
-    })(window, flush);
-  };
-})();
+      },
+      request() {
+        if (handle === -1) {
+          // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+          handle = wnd.requestIdleCallback!(callFlush);
+        }
+      },
+    };
+  } else {
+    return {
+      cancel() {
+        if (handle > -1) {
+          wnd.clearTimeout(handle);
+          handle = -1;
+        }
+      },
+      request() {
+        if (handle === -1) {
+          // Instead of trying anything fancy with event handler debouncers (we could do that if there was a request for it),
+          // we just wait 45ms which is approximately the interval in a native idleCallback loop in chrome, to at least make it look
+          // the same from a timing perspective
+          handle = wnd.setTimeout(callFlush, 45);
+        }
+      },
+    };
+  }
+}
 
 const defaultQueueTaskOptions: Required<QueueTaskTargetOptions> = {
   delay: 0,
@@ -408,8 +312,8 @@ export class BrowserScheduler implements IScheduler {
     return this.taskQueue[priority];
   }
 
-  public yield(priority: TaskQueuePriority): Promise<void> {
-    return this.taskQueue[priority].yield();
+  public async yield(priority: TaskQueuePriority): Promise<void> {
+    await this.taskQueue[priority].yield();
   }
 
   public queueTask<T = any>(callback: TaskCallback<T>, opts?: QueueTaskTargetOptions): Task<T> {
@@ -434,24 +338,24 @@ export class BrowserScheduler implements IScheduler {
   }
 
   @bound
-  public yieldMicroTask(): Promise<void> {
-    return this.taskQueue[TaskQueuePriority.microTask].yield();
+  public async yieldMicroTask(): Promise<void> {
+    await this.taskQueue[TaskQueuePriority.microTask].yield();
   }
   @bound
-  public yieldRenderTask(): Promise<void> {
-    return this.taskQueue[TaskQueuePriority.render].yield();
+  public async yieldRenderTask(): Promise<void> {
+    await this.taskQueue[TaskQueuePriority.render].yield();
   }
   @bound
-  public yieldMacroTask(): Promise<void> {
-    return this.taskQueue[TaskQueuePriority.macroTask].yield();
+  public async yieldMacroTask(): Promise<void> {
+    await this.taskQueue[TaskQueuePriority.macroTask].yield();
   }
   @bound
-  public yieldPostRenderTask(): Promise<void> {
-    return this.taskQueue[TaskQueuePriority.postRender].yield();
+  public async yieldPostRenderTask(): Promise<void> {
+    await this.taskQueue[TaskQueuePriority.postRender].yield();
   }
   @bound
-  public yieldIdleTask(): Promise<void> {
-    return this.taskQueue[TaskQueuePriority.idle].yield();
+  public async yieldIdleTask(): Promise<void> {
+    await this.taskQueue[TaskQueuePriority.idle].yield();
   }
   @bound
   public async yieldAll(repeat: number = 1): Promise<void> {

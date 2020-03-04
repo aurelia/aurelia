b1377a913db53be3eb2bedbf6ea21e949420c4d9
hamedfathi <noeffect@outlook.com>
2020-03-04 10:52:46 +0330

    fix(typeMapper): The excluded paths are removed from typeMapper.

--- tools/api-doc-generator/src/helpers/type-mapper.ts
+++ tools/api-doc-generator/src/helpers/type-mapper.ts
@@ -1,14 +1,23 @@
 import { TypeInfo } from '../api/models/type/type-info';
 // import { TemplateConfiguration } from '../templates/configurations';
 import { sourceFileLocator } from './source-file-locator';
+import { ApiConfiguration as config } from '../api';
 
 export function typeMapper(typeInfo: TypeInfo): string {
 
+    let excludes = config.exports.excludes;
     let value = typeInfo.value;
     // 'typeInfo' has imports.
     if (typeInfo.imports) {
         for (let index = 0; index < typeInfo.imports.length; index++) {
             const path = typeInfo.imports[index].path;
+            for (let index = 0; index < excludes.length; index++) {
+                const ex = excludes[index];
+                if(path.includes(ex))
+                {
+                    return value;
+                }
+            }
             const category = typeInfo.imports[index].typeCategory;//.toString().toLowerCase();
             const name = typeInfo.imports[index].name;
             const url = sourceFileLocator(path, name, category);

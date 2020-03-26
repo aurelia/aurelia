7afc3a7058d4addb42eb584f519aeea156940ceb
Fred Kleuver <fred@avurad.nl>
2020-03-26 15:04:33 +0100

    chore(router): re-export route-recognizer with specific types

--- packages/router/src/route-recognizer.ts
+++ packages/router/src/route-recognizer.ts
@@ -8,8 +8,18 @@ import {
 
 import { IRoute } from './interfaces';
 
-export declare class RouteRecognizer extends $RouteRecognizer<IRoute> {}
+declare class $$RouteRecognizer extends $RouteRecognizer<IRoute> {}
+declare class $$ConfigurableRoute extends $ConfigurableRoute<IRoute> {}
+declare class $$RecognizedRoute extends $RecognizedRoute<IRoute> {}
+declare class $$Endpoint extends $Endpoint<IRoute> {}
+
+export interface RouteRecognizer extends $RouteRecognizer<IRoute> {}
 export type IConfigurableRoute = $IConfigurableRoute<IRoute>;
-export declare class ConfigurableRoute extends $ConfigurableRoute<IRoute> {}
-export declare class RecognizedRoute extends $RecognizedRoute<IRoute> {}
-export declare class Endpoint extends $Endpoint<IRoute> {}
+export interface ConfigurableRoute extends $ConfigurableRoute<IRoute> {}
+export interface RecognizedRoute extends $RecognizedRoute<IRoute> {}
+export interface Endpoint extends $Endpoint<IRoute> {}
+
+export const RouteRecognizer = $RouteRecognizer as typeof $$RouteRecognizer;
+export const ConfigurableRoute = $ConfigurableRoute as typeof $$ConfigurableRoute;
+export const RecognizedRoute = $RecognizedRoute as typeof $$RecognizedRoute;
+export const Endpoint = $Endpoint as typeof $$Endpoint;

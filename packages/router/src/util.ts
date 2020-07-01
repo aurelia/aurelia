278da264161b9d5a62b17d038006d21d4edb27aa
Fred Kleuver <fred@avurad.nl>
2020-07-01 17:04:03 +0200

    chore(router): small memleak fix and promise optimization

--- packages/router/src/util.ts
+++ packages/router/src/util.ts
@@ -1,4 +1,3 @@
-import { PLATFORM } from '@aurelia/kernel';
 import { RouteNode } from './route-tree';
 import { ViewportAgent } from './viewport-agent';
 
@@ -45,14 +44,24 @@ export function resolveAll(
     case 1:
       return promises[0];
     default:
-      return Promise.all(promises) as unknown as Promise<void>;
+      return Promise.all(promises).then() as unknown as Promise<void>;
   }
 }
 
 export function walkViewportTree<R extends void | Promise<void>>(
   nodes: RouteNode[],
   callback: (viewportAgent: ViewportAgent) => R,
-  seen: WeakMap<ViewportAgent, void | Promise<void>> = new WeakMap(),
+): R {
+  const seen = new Map<ViewportAgent, void | Promise<void>>();
+  return onResolve($walkViewportTree(nodes, callback, seen), function () {
+    seen.clear();
+  }) as R;
+}
+
+function $walkViewportTree<R extends void | Promise<void>>(
+  nodes: RouteNode[],
+  callback: (viewportAgent: ViewportAgent) => R,
+  seen: Map<ViewportAgent, void | Promise<void>>,
 ): R {
   return resolveAll(nodes.map(function (node) {
     const vpa = node.context.vpa;
@@ -60,13 +69,25 @@ export function walkViewportTree<R extends void | Promise<void>>(
     if (seen.has(vpa)) {
       result = seen.get(vpa);
     } else {
-      seen.set(
-        vpa,
-        result = callback(vpa),
-      );
+      result = callback(vpa);
+      if (result instanceof Promise) {
+        seen.set(vpa, result);
+        // It can't throw and nothing needs to wait for it
+        // eslint-disable-next-line @typescript-eslint/no-floating-promises
+        result.then(function () {
+          // Just check that we haven't been cleared yet, so we can't cause minor mem leaks
+          if (seen.size > 0) {
+            // Set to void after the promise resolves, so that we don't wait unnecessary ticks later down the road
+            seen.set(vpa, void 0);
+          }
+        });
+      } else {
+        // Don't care about anything other than undefined if it's not a promise, so to prevent potential megamorphism, set to undefined
+        seen.set(vpa, void 0);
+      }
     }
     return onResolve(result, function () {
-      return walkViewportTree(node.children, callback, seen);
+      return $walkViewportTree(node.children, callback, seen);
     });
   })) as R;
 }

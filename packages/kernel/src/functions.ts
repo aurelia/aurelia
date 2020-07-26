8f0e8d9052a50f8905a1a253d55092c4b4a64194
Fred Kleuver <fred@avurad.nl>
2020-07-26 21:37:46 +0200

    feat(kernel): add onResolve and resolveAll functions

--- packages/kernel/src/functions.ts
+++ packages/kernel/src/functions.ts
@@ -533,3 +533,55 @@ export const isNativeFunction = (function () {
     return isNative;
   };
 })();
+
+type UnwrapPromise<T> = T extends Promise<infer R> ? R : T;
+type MaybePromise<T> = T extends Promise<infer R> ? (T | R) : (T | Promise<T>);
+
+/**
+ * Normalize a potential promise via a callback, to ensure things stay synchronous when they can.
+ *
+ * If the value is a promise, it is `then`ed before the callback is invoked. Otherwise the callback is invoked synchronously.
+ */
+export function onResolve<TValue, TRet>(
+  maybePromise: TValue,
+  resolveCallback: (value: UnwrapPromise<TValue>) => TRet,
+): MaybePromise<TRet> {
+  if (maybePromise instanceof Promise) {
+    return maybePromise.then(resolveCallback) as MaybePromise<TRet>;
+  }
+  return resolveCallback(maybePromise as UnwrapPromise<TValue>) as MaybePromise<TRet>;
+}
+
+/**
+ * Normalize an array of potential promises, to ensure things stay synchronous when they can.
+ *
+ * If exactly one value is a promise, then that promise is returned.
+ *
+ * If more than one value is a promise, a new `Promise.all` is returned.
+ *
+ * If none of the values is a promise, nothing is returned, to indicate that things can stay synchronous.
+ */
+export function resolveAll(
+  maybePromises: (void | Promise<void>)[],
+): void | Promise<void> {
+  let maybePromise: Promise<void> | void = void 0;
+  let firstPromise: Promise<void> | void = void 0;
+  let promises: Promise<void>[] | undefined = void 0;
+  for (let i = 0, ii = maybePromises.length; i < ii; ++i) {
+    maybePromise = maybePromises[i];
+    if ((maybePromise = maybePromises[i]) instanceof Promise) {
+      if (firstPromise === void 0) {
+        firstPromise = maybePromise;
+      } else if (promises === void 0) {
+        promises = [firstPromise, maybePromise];
+      } else {
+        promises.push(maybePromise);
+      }
+    }
+  }
+
+  if (promises === void 0) {
+    return firstPromise;
+  }
+  return Promise.all(promises) as unknown as Promise<void>;
+}

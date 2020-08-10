7d18a19abfe908a9ea38ed1439e89305c57c705f
Vildan Softic <zewa666@gmail.com>
2020-08-10 12:25:45 +0200

    test(store): adapt dispatchify tests

--- packages/__tests__/store/dispatchify.spec.ts
+++ packages/__tests__/store/dispatchify.spec.ts
@@ -1,95 +1,88 @@
-import { DI, Registration } from '@aurelia/kernel';
-import { expect } from 'chai';
 import { skip } from 'rxjs/operators';
-import { STORE, dispatchify, Store } from '@aurelia/store';
+import { DI, Registration } from '@aurelia/kernel';
+import { assert } from '@aurelia/testing';
+import { STORE, dispatchify, Store, UnregisteredActionError } from '@aurelia/store';
+
 import { createTestStore, testState } from './helpers';
 
-describe('dispatchify', () => {
-  it('should help create dispatchifyable functions', done => {
-    const container = DI.createContainer();
-    const { store } = createTestStore();
+function arrange() {
+  const container = DI.createContainer();
+  const { store } = createTestStore();
 
-    const fakeAction = (currentState: testState, param1: number, param2: number) => {
-      return {...currentState,  foo: param1 + param2};
-    };
+  const fakeAction = (currentState: testState, param1: number, param2: number) => {
+    return { ...currentState, foo: param1 + param2 };
+  };
 
-    STORE.container = container;
+  STORE.container = container;
+
+  return { store, container, fakeAction };
+}
+
+describe('dispatchify', function () {
+  it('should help create dispatchifyable functions', function (done) {
+    const { store, container, fakeAction } = arrange();
 
     store.registerAction('FakeAction', fakeAction as any);
     container.register(Registration.instance(Store, store));
 
-    dispatchify((fakeAction as any))(1, 2);
+    dispatchify((fakeAction as any))(1, 2).catch(() => { /**/ });
 
     store.state.pipe(
       skip(1)
     ).subscribe((state: any) => {
-      expect(state.foo).to.equal(3);
+      assert.equal(state.foo, 3);
       done();
     });
   });
 
-  it('should return the promise from dispatched calls', async () => {
-    const container = DI.createContainer();
-    const { store } = createTestStore();
-
-    const fakeAction = (currentState: testState, param1: number, param2: number) => {
-      return {...currentState,  foo: param1 + param2};
-    };
-
-    STORE.container = container;
+  it('should return the promise from dispatched calls', async function () {
+    const { store, container, fakeAction } = arrange();
 
     store.registerAction('FakeAction', fakeAction as any);
     container.register(Registration.instance(Store, store));
 
     const result = dispatchify((fakeAction as any))(1, 2);
-    expect(result.then).not.to.equal(undefined);
+    assert.notEqual(result.then, undefined);
 
     await result;
   });
 
-  it('should accept the reducers registered name', done => {
-    const container = DI.createContainer();
-    const { store } = createTestStore();
-
-    const fakeAction = (currentState: testState, param1: number, param2: number) => {
-      return {...currentState,  foo: param1 + param2};
-    };
-
-    STORE.container = container;
+  it('should accept the reducers registered name', function (done) {
+    const { store, container, fakeAction } = arrange();
 
     const fakeActionRegisteredName = 'FakeAction';
 
     store.registerAction(fakeActionRegisteredName, fakeAction as any);
     container.register(Registration.instance(Store, store));
 
-    dispatchify(fakeActionRegisteredName)('A', 'B');
+    dispatchify(fakeActionRegisteredName)('A', 'B').catch(() => { /**/ });
 
     store.state.pipe(
       skip(1)
     ).subscribe((state: any) => {
-      expect(state.foo).to.equal('AB');
+      assert.equal(state.foo, 'AB');
       done();
     });
   });
 
-  // FIX: This test won't currently pass, needs to be fixed
-  /*
-  it('should throw if any string given that doesn\'t reflect a registered action name', async (done) => {
+  it('should throw if any string given that doesn\'t reflect a registered action name', async function () {
     const container = DI.createContainer();
     const { store } = createTestStore();
 
     STORE.container = container;
 
     const fakeAction = (currentState: testState, param1: number, param2: number) => {
-      return {...currentState,  foo: param1 + param2};
+      return { ...currentState, foo: param1 + param2 };
     };
     const fakeActionRegisteredName = 'FakeAction';
 
     store.registerAction(fakeActionRegisteredName, fakeAction as any);
     container.register(Registration.instance(Store, store));
 
-    // tslint:disable-next-line
-    expect(dispatchify('ABC')('A', 'B')).throw;
+    try {
+      await dispatchify('ABC')('A', 'B');
+    } catch(e) {
+      assert.equal(e instanceof UnregisteredActionError, true);
+    }
   });
-  */
 });

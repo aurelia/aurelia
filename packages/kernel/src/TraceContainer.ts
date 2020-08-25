55967e1d22ce54ac1786229d0e4d1d2ae24465a3
Caleb Cushing <xenoterracide@gmail.com>
2020-08-25 16:19:25 -0500

    refactor: try suggestion
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- packages/kernel/src/TraceContainer.ts
+++ packages/kernel/src/TraceContainer.ts
@@ -6,10 +6,19 @@ export class TraceContainer extends Container {
   public constructor(parent: Container | null, config: ContainerConfiguration) {
     super(parent, config);
   }
-  public get<K extends Key>(key: K | Key): Resolved<K> {
-    if (key !== ILogger) {
-      this.get(ILogger).trace('getting ', key);
+
+  private _logger: ILogger | null = null;
+
+  private get logger(): ILogger | null {
+    if (this._logger === null && super.has(ILogger, true)) {
+      this._logger = super.get(ILogger);
     }
+    return this._logger;
+  }
+
+  public get<K extends Key>(key: K | Key): Resolved<K> {
+    this.logger?.trace('getting ', key);
+
     return this.get(key);
   }
 }

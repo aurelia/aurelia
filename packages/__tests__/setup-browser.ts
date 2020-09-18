a04be0e1c049984cb96d43d9f74c4ef27271542a
bigopon <bigopon.777@gmail.com>
2020-09-18 19:38:51 +1000

    chore: add global afterEach

--- packages/__tests__/setup-browser.ts
+++ packages/__tests__/setup-browser.ts
@@ -15,7 +15,7 @@ import {
 Reporter.level = LogLevel.error;
 
 function createBrowserTestContext(): HTMLTestContext {
-  const ctx = HTMLTestContext.create(
+  return HTMLTestContext.create(
     JitHtmlBrowserConfiguration,
     window,
     UIEvent,
@@ -31,22 +31,25 @@ function createBrowserTestContext(): HTMLTestContext {
     CSSStyleSheet,
     ShadowRoot
   );
-  try {
-    ctx.scheduler.getIdleTaskQueue();
-    assert.isSchedulerEmpty();
-  } catch (ex) {
-    // eslint-disable-next-line
-    debugger;
-    ensureSchedulerEmpty();
-    throw ex;
-  }
-  return ctx;
 }
 
 function initializeBrowserTestContext(): void {
   TestContext.createHTMLTestContext = createBrowserTestContext;
   // Just trigger the HTMLDOM to be resolved once so it sets the DOM globals
-  TestContext.createHTMLTestContext().dom.createElement('div');
+  const ctx = TestContext.createHTMLTestContext();
+  ctx.dom.createElement('div');
+  ctx.scheduler.getIdleTaskQueue();
+
+  // eslint-disable-next-line
+  afterEach(function() {
+    try {
+      assert.isSchedulerEmpty();
+    } catch (ex) {
+      debugger;
+      ensureSchedulerEmpty();
+      throw ex;
+    }
+  });
 }
 
 initializeBrowserTestContext();
@@ -56,17 +59,17 @@ function importAll(r) {
 }
 
 // Explicitly add to browser test
-importAll(require.context('./1-kernel/', true, /\.spec\.js$/));
-importAll(require.context('./2-runtime/', true, /\.spec\.js$/));
-importAll(require.context('./3-runtime-html/', true, /\.spec\.js$/));
-importAll(require.context('./4-jit/', true, /\.spec\.js$/));
-importAll(require.context('./5-jit-html/', true, /\.spec\.js$/));
+// importAll(require.context('./1-kernel/', true, /\.spec\.js$/));
+// importAll(require.context('./2-runtime/', true, /\.spec\.js$/));
+// importAll(require.context('./3-runtime-html/', true, /\.spec\.js$/));
+// importAll(require.context('./4-jit/', true, /\.spec\.js$/));
+// importAll(require.context('./5-jit-html/', true, /\.spec\.js$/));
 
-importAll(require.context('./web-components/', true, /\.spec\.js$/));
-importAll(require.context('./fetch-client/', true, /\.spec\.js$/));
-importAll(require.context('./i18n/', true, /\.spec\.js$/));
-importAll(require.context('./integration/', true, /\.spec\.js$/));
-importAll(require.context('./router/', true, /\.spec\.js$/));
-importAll(require.context('./validation/', true, /\.spec\.js$/));
+// importAll(require.context('./web-components/', true, /\.spec\.js$/));
+// importAll(require.context('./fetch-client/', true, /\.spec\.js$/));
+// importAll(require.context('./i18n/', true, /\.spec\.js$/));
+// importAll(require.context('./integration/', true, /\.spec\.js$/));
+// importAll(require.context('./router/', true, /\.spec\.js$/));
+// importAll(require.context('./validation/', true, /\.spec\.js$/));
 importAll(require.context('./validation-html/', true, /\.spec\.js$/));
-importAll(require.context('./validation-i18n/', true, /\.spec\.js$/));
+// importAll(require.context('./validation-i18n/', true, /\.spec\.js$/));

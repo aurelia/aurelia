854925639a7385b566392519dcee163222a53068
Fred Kleuver <fred@avurad.nl>
2020-10-06 00:45:57 +0200

    chore(binding-mode-behavior): cleanup unneeded type

--- packages/runtime/src/resources/binding-behaviors/binding-mode.ts
+++ packages/runtime/src/resources/binding-behaviors/binding-mode.ts
@@ -4,27 +4,26 @@ import { BindingMode, LifecycleFlags } from '../../flags';
 import { IScope } from '../../observation';
 import { bindingBehavior } from '../binding-behavior';
 
-export type WithMode = { mode: BindingMode };
-
 export abstract class BindingModeBehavior {
-  private readonly originalModes: WeakMap<PropertyBinding, BindingMode> = new WeakMap();
+  private readonly originalModes: Map<PropertyBinding, BindingMode> = new Map();
 
   public constructor(
     protected readonly logger: ILogger,
     private readonly mode: BindingMode,
   ) {}
 
-  public bind(flags: LifecycleFlags, scope: IScope, hostScope: IScope | null, binding: PropertyBinding & WithMode): void {
+  public bind(flags: LifecycleFlags, scope: IScope, hostScope: IScope | null, binding: PropertyBinding): void {
     this.logger.trace(`bind(%s, scope, hostScope, binding)`, flags);
 
     this.originalModes.set(binding, binding.mode);
     binding.mode = this.mode;
   }
 
-  public unbind(flags: LifecycleFlags, scope: IScope, hostScope: IScope | null, binding: PropertyBinding & WithMode): void {
+  public unbind(flags: LifecycleFlags, scope: IScope, hostScope: IScope | null, binding: PropertyBinding): void {
     this.logger.trace(`unbind(%s, scope, hostScope, binding)`, flags);
 
-    binding.mode = this.originalModes.get(binding) as BindingMode;
+    binding.mode = this.originalModes.get(binding)!;
+    this.originalModes.delete(binding);
   }
 }
 

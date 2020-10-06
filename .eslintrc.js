c63136fffe27fa75fc9972d59ece771c1e34ee78
Fred Kleuver <fred@avurad.nl>
2020-10-06 21:43:57 +0200

    chore(eslint): make new error rules into warnings for now

--- .eslintrc.js
+++ .eslintrc.js
@@ -193,6 +193,10 @@ module.exports = {
     }],
 
     // Things we maybe need to fix some day, so are marked as warnings for now:
+    '@typescript-eslint/no-unsafe-assignment': 'warn',
+    '@typescript-eslint/no-unsafe-member-access': 'warn',
+    '@typescript-eslint/no-unsafe-call': 'warn',
+    '@typescript-eslint/no-unsafe-return': 'warn',
     '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-this-alias': 'warn',
     '@typescript-eslint/no-unnecessary-condition': 'off', // Only false positives seen so far

094bcd3aa49e3358d61721703851a9129ac9f666
Sayan751 <sayan751@gmail.com>
2020-10-23 19:19:12 +0200

    test: more integration tests

--- packages/__tests__/3-runtime-html/integration.spec.ts
+++ packages/__tests__/3-runtime-html/integration.spec.ts
@@ -95,22 +95,23 @@ describe('runtime-html.integration', function () {
 
   function* getTestData() {
     {
-      @customElement({ name: 'app', isStrictBinding: true, template: '<div ref="container" id="cr">1</div><child ref="child" id="child"></child>' })
+      @customElement({ name: 'app', isStrictBinding: true, template: '<div ref="container" id="cr">1</div><child ref="child" id="child"></child><div ref="container2" id="cr2">11</div>' })
       class App {
         public readonly container: HTMLElement = void 0;
         public readonly child: HTMLElement;
       }
-      @customElement({ name: 'child', isStrictBinding: true, template: '<div ref="container" id="cc">2</div><grand-child ref="grandChild" id="grandChild"></grand-child>' })
+      @customElement({ name: 'child', isStrictBinding: true, template: '<div ref="container" id="cc">2</div><grand-child ref="grandChild" id="grandChild"></grand-child><div ref="container2" id="cc2">22</div>' })
       class Child {
         public readonly container: HTMLElement = void 0;
         public readonly grandChild: HTMLElement;
       }
-      @customElement({ name: 'grand-child', isStrictBinding: true, template: '<div ref="container" id="cgc">3</div>' })
+      @customElement({ name: 'grand-child', isStrictBinding: true, template: '<div ref="container" id="cgc">3</div><div ref="container2" id="cgc2">33</div>' })
       class GrandChild {
         public readonly container: HTMLElement = void 0;
       }
       yield new TestData(
-        'ref-binding',
+        // depending on TS config, explicitly uninitialized, and non-defined properties might or might not be same.
+        'ref-binding with initialized, uninitialized, and non-defined properties',
         App,
         [Child, GrandChild],
         function (ctx) {
@@ -118,6 +119,7 @@ describe('runtime-html.integration', function () {
           const container = app.container;
           const host = ctx.host;
           assert.strictEqual(container, host.querySelector('#cr'));
+          assert.strictEqual(app['container2'], host.querySelector('#cr2'));
           assert.html.textContent(container, '1');
 
           const childEl = host.querySelector('#child');
@@ -126,6 +128,7 @@ describe('runtime-html.integration', function () {
           const childVm = CustomElement.for<Element, Child>(childEl).viewModel;
           const childContainer = childVm.container;
           assert.strictEqual(childEl.querySelector('#cc'), childContainer);
+          assert.strictEqual(childVm['container2'], childEl.querySelector('#cc2'));
           assert.html.textContent(childContainer, '2');
 
           const grandChildEl = childEl.querySelector('#grandChild');
@@ -134,7 +137,45 @@ describe('runtime-html.integration', function () {
           const grandChildVm = CustomElement.for<Element, GrandChild>(grandChildEl).viewModel;
           const grandChildContainer = grandChildVm.container;
           assert.strictEqual(grandChildEl.querySelector('#cgc'), grandChildContainer);
+          assert.strictEqual(grandChildVm['container2'], grandChildEl.querySelector('#cgc2'));
           assert.html.textContent(grandChildContainer, '3');
+        }
+      );
+    }
+    {
+      @customElement({
+        name: 'app',
+        isStrictBinding: true,
+        template: `
+        <child view-model.ref="c1" id="c1"></child>
+        <child view-model.ref="c2" id="c2"></child>
+        <child view-model.ref="c3" id="c3"></child>`
+      })
+      class App {
+        public readonly c1: Child = void 0;
+        public readonly c2: Child;
+      }
+      @customElement({ name: 'child', isStrictBinding: true, template: '' })
+      class Child {
+        private static id = 1;
+        public readonly id = Child.id++;
+      }
+      yield new TestData(
+        // depending on TS config, explicitly uninitialized, and non-defined properties might or might not be same.
+        'view-model.ref-binding with initialized, uninitialized, and non-defined properties',
+        App,
+        [Child],
+        function (ctx) {
+          const app = ctx.app;
+          const c1 = app.c1;
+          const c2 = app.c2;
+          const c3 = app['c3'];
+          assert.strictEqual(c1.id, 1);
+          assert.instanceOf(c1, Child);
+          assert.strictEqual(c2.id, 2);
+          assert.instanceOf(c2, Child);
+          assert.strictEqual(c3.id, 3);
+          assert.instanceOf(c3, Child);
         }
       );
     }
@@ -402,6 +443,80 @@ describe('runtime-html.integration', function () {
           assert.html.textContent(cr, '42');
           assert.strictEqual(childVm.value, 42);
           assert.strictEqual(app.value, 42);
+        }
+      );
+    }
+    const templates = [
+      `<template>
+          <template repeat.for="i of 3">
+            <template repeat.for="i of 3">
+              <template repeat.for="i of 3">
+                \${$parent.$parent.i + $parent.i + i}
+              </template>
+            </template>
+          </template>
+        </template>`,
+
+      `<template>
+          <template repeat.for="i of 3">
+            <template repeat.for="i of 3">
+              <let gp.bind="$parent"></let>
+              <template repeat.for="i of 3">
+                <let p.bind="$parent"></let>
+                \${gp.i + p.i + i}
+              </template>
+            </template>
+          </template>
+        </template>`,
+
+      `<template>
+          <template repeat.for="i of 3">
+            <template repeat.for="i of 3">
+              <template repeat.for="i of 3">
+                <let gp.bind="$parent.$parent" p.bind="$parent"></let>
+                \${gp.i + p.i + i}
+              </template>
+            </template>
+          </template>
+        </template>`,
+
+      `<template>
+          <template repeat.for="i of 3">
+            <template repeat.for="i of 3">
+              <template repeat.for="i of 3">
+                <let gp.bind="$parent.$parent" p.bind="$parent" k.bind="gp.i" j.bind="p.i"></let>
+                \${k + j + i}
+              </template>
+            </template>
+          </template>
+        </template>`,
+
+      // The following template does not supported; kept here for docs purpose.
+      // `<template>
+      //     <template repeat.for="i of 3">
+      //       <template repeat.for="i of 3">
+      //         <template repeat.for="i of 3">
+      //           <let p.bind="$parent" gp.bind="p.$parent"></let>
+      //           \${gp.i + p.i + i}
+      //         </template>
+      //       </template>
+      //     </template>
+      //   </template>`,
+    ];
+    for (let i = 0, ii = templates.length; i < ii; i++) {
+      @customElement({
+        name: 'app',
+        isStrictBinding: true,
+        template: templates[i]
+      })
+      class App { }
+      yield new TestData(
+        `repeater + $parent - #${i + 1}`,
+        App,
+        [],
+        async function (ctx) {
+          const host = ctx.host;
+          assert.html.textContent(host, '0 1 2 1 2 3 2 3 4 1 2 3 2 3 4 3 4 5 2 3 4 3 4 5 4 5 6');
         }
       );
     }

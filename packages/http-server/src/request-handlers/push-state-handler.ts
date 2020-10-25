f02999c30832c811efcdf1c0a4b58548ca2a30d8
Sayan751 <sayan751@gmail.com>
2020-10-25 20:07:30 +0100

    chore(http-server): fixed linting error

--- packages/http-server/src/request-handlers/push-state-handler.ts
+++ packages/http-server/src/request-handlers/push-state-handler.ts
@@ -3,15 +3,16 @@ import { IHttpContext } from '../http-context';
 import { IRequestHandler } from '../interfaces';
 
 export class PushStateHandler implements IRequestHandler {
+  // eslint-disable-next-line @typescript-eslint/require-await
   public async handleRequest(context: IHttpContext): Promise<void> {
     const request = context.request;
     const url = context.requestUrl.href;
 
     /**
      * Ignore the request if one of the following is condition holds:
-     *   1. Not a GET request.
-     *   2. Client does not accept html.
-     *   3. The path has a dot (.) in the last fragment; dot rule.
+     * 1. Not a GET request.
+     * 2. Client does not accept html.
+     * 3. The path has a dot (.) in the last fragment; dot rule.
      */
     if (
       request.method !== 'GET'

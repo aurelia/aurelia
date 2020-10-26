3262cb10b73114ab0fba135493eae5a77f15ee85
bigopon <bigopon.777@gmail.com>
2020-10-26 20:45:43 +1100

    chore: fix working directory

--- test/build-test-apps.js
+++
@@ -1,9 +0,0 @@
-const { execSync } = require('child_process');
-
-for (const appPath of [
-  '1kcomponents',
-  'fractals-tree',
-  'sierpinski-triangle,'
-]) {
-  execSync(`cd ${appPath} && npm run build`);
-}

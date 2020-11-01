fc0e5eef75905ff4ceffd59eac243a27c3199c7a
Fred Kleuver <fred@avurad.nl>
2020-11-01 10:22:58 +0100

    chore(test): re-enable tests

--- packages/__tests__/setup-browser.ts
+++ packages/__tests__/setup-browser.ts
@@ -11,15 +11,14 @@ function importAll(r) {
 }
 
 // Explicitly add to browser test
-// importAll(require.context('./1-kernel/', true, /\.spec\.js$/));
-// importAll(require.context('./2-runtime/', true, /\.spec\.js$/));
-// importAll(require.context('./3-runtime-html/', true, /\.spec\.js$/));
+importAll(require.context('./1-kernel/', true, /\.spec\.js$/));
+importAll(require.context('./2-runtime/', true, /\.spec\.js$/));
+importAll(require.context('./3-runtime-html/', true, /\.spec\.js$/));
 
-// importAll(require.context('./fetch-client/', true, /\.spec\.js$/));
-// importAll(require.context('./i18n/t/', true, /\.spec\.js$/));
-// importAll(require.context('./integration/', true, /\.spec\.js$/));
-// importAll(require.context('./router/', true, /\.spec\.js$/));
-// importAll(require.context('./validation/', true, /\.spec\.js$/));
-// importAll(require.context('./validation-html/', true, /\.spec\.js$/));
-// importAll(require.context('./validation-i18n/', true, /\.spec\.js$/));
-importAll(require.context('./3-runtime-html/', false, /styles\.spec\.js$/));
+importAll(require.context('./fetch-client/', true, /\.spec\.js$/));
+importAll(require.context('./i18n/t/', true, /\.spec\.js$/));
+importAll(require.context('./integration/', true, /\.spec\.js$/));
+importAll(require.context('./router/', true, /\.spec\.js$/));
+importAll(require.context('./validation/', true, /\.spec\.js$/));
+importAll(require.context('./validation-html/', true, /\.spec\.js$/));
+importAll(require.context('./validation-i18n/', true, /\.spec\.js$/));

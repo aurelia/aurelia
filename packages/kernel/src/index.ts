ce5e17df5449503204e190f39113213c7cb7f184
Fred Kleuver <fred@avurad.nl>
2020-11-02 07:31:19 +0100

    feat(kernel): add inheritParentResources config option

--- packages/kernel/src/index.ts
+++ packages/kernel/src/index.ts
@@ -42,7 +42,7 @@ export {
   Transformer,
   newInstanceForScope,
   newInstanceOf,
-  DefaultContainerConfiguration,
+  ContainerConfiguration,
   DefaultResolver,
   IContainerConfiguration,
 } from './di';

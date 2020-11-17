a7be2b8c54d8a4ef9278ce87baec87c22516b237
Brandon seydel <brandonseydel@hotmail.com>
2020-11-17 17:01:23 -0600

    Update platform.ts
    remove more formatting nonsense

--- packages/kernel/src/platform.ts
+++ packages/kernel/src/platform.ts
@@ -4,4 +4,4 @@ export const customPropertyPrefix: string = '--';
 export const emptyObject: any = Object.freeze({}) as any;
 /* eslint-enable @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-explicit-any */
 // eslint-disable-next-line @typescript-eslint/no-empty-function
-export function noop(): void { }
+export function noop(): void {}

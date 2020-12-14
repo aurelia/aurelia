da08c74d1cf486ecf7f7808b74abe747dd580f22
Fred Kleuver <fred@avurad.nl>
2020-12-14 12:31:35 +0100

    chore(lint): fix lint error

--- test/i18n/webpack.config.js
+++ test/i18n/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
-module.exports = function(env, { mode }) {
+module.exports = function (env, { mode }) {
   const production = mode === 'production';
   return {
     mode: production ? 'production' : 'development',

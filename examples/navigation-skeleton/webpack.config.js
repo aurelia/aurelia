4d88af2c8b389109f2ef6b4f983610a2a3cb5963
Fred Kleuver <fred@avurad.nl>
2020-12-16 17:59:33 +0100

    chore(navigation-skeleton): update webpack config

--- examples/navigation-skeleton/webpack.config.js
+++ examples/navigation-skeleton/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = function (env, { mode }) {
       rules: [
         { test: /\.ts$/i, loader: 'ts-loader' },
         { test: /\.html$/i, loader: 'html-loader' },
-        { test: /\.css$/i, loader: ['style-loader', 'css-loader'] },
+        { test: /\.css$/i, use: [{ loader: 'style-loader' }, { loader: 'css-loader' }] },
         { test: /\.woff2(\?v=[0-9]\.[0-9]\.[0-9])?$/i, loader: "url-loader", options: { limit: 10000, mimetype: "application/font-woff2" } },
         { test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/i, loader: "url-loader", options: { limit: 10000, mimetype: "application/font-woff" } },
         { test: /\.(ttf|eot|svg|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/i, loader: "file-loader" },

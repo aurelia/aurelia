4897ae7b7a917b9979976682dc0032c68294f429
bigopon <bigopon.777@gmail.com>
2020-12-23 18:57:45 +1100

    chore: temporarily disable bindable test

--- packages/__tests__/3-runtime-html/effect.spec.ts
+++ packages/__tests__/3-runtime-html/effect.spec.ts
@@ -48,15 +48,13 @@ describe('3-runtime-html/effect.spec.ts', function () {
     assert.strictEqual(div.textContent, '1, 2');
   });
 
-  it('runs effect with @bindable', async function () {
+  it.skip('runs effect with @bindable', async function () {
     let runCount = 0;
 
     @customAttribute('mouse-tracker')
     class MouseTracker {
       public static inject = [INode, IEffectRunner];
-      @bindable({
-        set: v => v instanceof Array ? v : [0, 0]
-      })
+      @bindable({ set: v => v })
       public coord: [number, number] = [0, 0];
       private effect?: IEffect;
 
@@ -72,6 +70,7 @@ describe('3-runtime-html/effect.spec.ts', function () {
       public binding() {
         this.effect = this.effectRunner.run(() => {
           runCount++;
+          debugger
           this.host.textContent = this.coord.join(', ');
         });
       }
@@ -106,5 +105,51 @@ describe('3-runtime-html/effect.spec.ts', function () {
     mouseTracker.pretendMouseMove(5, 6);
     assert.strictEqual(runCount, 2);
     assert.strictEqual(div.textContent, '1, 2');
+  });
+
+  it('does not track @observable accessed outside of effect', async function () {
+    const { ctx, component, startPromise, tearDown } = createFixture('<div ref="div"></div>', class App {
+      public div: HTMLDivElement;
+    });
+
+    await startPromise;
+
+    class MouseTracker {
+      @observable()
+      public coord: [number, number] = [0, 0];
+
+      public pretendMouseMove(x: number, y: number): void {
+        this.coord = [x, y];
+      }
+    }
+
+    assert.instanceOf(component.div, ctx.Element);
+
+    let runCount = 0;
+    const div = component.div;
+    const runner = ctx.container.get(IEffectRunner);
+    const mouseTracker = new MouseTracker();
+    const { coord } = mouseTracker;
+    const effect = runner.run(() => {
+      runCount++;
+      div.textContent = coord.join(', ');
+    });
+    assert.strictEqual(runCount, 1);
+    assert.strictEqual(div.textContent, '0, 0');
+
+    mouseTracker.pretendMouseMove(1, 2);
+    assert.strictEqual(runCount, 1);
+    assert.strictEqual(div.textContent, '0, 0');
+
+    effect.stop();
+    mouseTracker.pretendMouseMove(3, 4);
+    assert.strictEqual(runCount, 1);
+    assert.strictEqual(div.textContent, '0, 0');
+
+    await tearDown();
+
+    mouseTracker.pretendMouseMove(5, 6);
+    assert.strictEqual(runCount, 1);
+    assert.strictEqual(div.textContent, '0, 0');
   });
 });

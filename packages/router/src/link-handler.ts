8b9fa299dddac3d223f6211c4511096e5e22d4b6
Fred Kleuver <fred@avurad.nl>
2020-12-26 19:50:56 +0100

    fix(router): fix several issues in link handler and add trace logging

--- packages/router/src/link-handler.ts
+++ packages/router/src/link-handler.ts
@@ -1,4 +1,4 @@
-import { bound, DI } from '@aurelia/kernel';
+import { DI, ILogger } from '@aurelia/kernel';
 import { CustomAttribute, IWindow } from '@aurelia/runtime-html';
 
 import { LoadCustomAttribute } from './resources/load.js';
@@ -7,26 +7,6 @@ import { IRouter } from './router.js';
 export interface IMouseEvent extends MouseEvent {}
 export interface IElement extends Element {}
 export interface IHTMLElement extends HTMLElement {}
-
-/**
- * Provides information about how to handle an anchor event.
- *
- * @internal
- */
-export interface AnchorEventInfo {
-  /**
-   * Indicates whether the event should be handled or not.
-   */
-  shouldHandleEvent: boolean;
-  /**
-   * The instruction (href) of the link or null if not-applicable.
-   */
-  instruction: string | null;
-  /**
-   * The anchor element or null if not-applicable.
-   */
-  anchor: Element | null;
-}
 
 export interface ILinkHandler extends LinkHandler {}
 export const ILinkHandler = DI.createInterface<ILinkHandler>('ILinkHandler', x => x.singleton(LinkHandler));
@@ -40,74 +20,67 @@ export class LinkHandler {
   public constructor(
     @IWindow private readonly window: IWindow,
     @IRouter private readonly router: IRouter,
-  ) {}
-
-  /**
-   * Gets the href and a "should handle" recommendation, given an Event.
-   *
-   * @param event - The Event to inspect for target anchor and href.
-   */
-  private getEventInfo(event: IMouseEvent): AnchorEventInfo {
-    const info: AnchorEventInfo = {
-      shouldHandleEvent: false,
-      instruction: null,
-      anchor: null
-    };
-
-    const target = info.anchor = event.currentTarget as Element | null;
-    // Switch to this for delegation:
-    // const target = info.anchor = LinkHandler.closestAnchor(event.target as Element);
-    if (target === null || !this.targetIsThisWindow(target)) {
-      return info;
-    }
-
-    if (target.hasAttribute('external')) {
-      return info;
-    }
-
-    if (event.altKey || event.ctrlKey || event.metaKey || event.shiftKey) {
-      return info;
-    }
-
-    const loadAttr = CustomAttribute.for<LoadCustomAttribute>(target, 'load');
-    const load = (loadAttr?.viewModel.value ?? null) as string | null;
-    const href = this.router.options.useHref && target.hasAttribute('href') ? target.getAttribute('href') : null;
-    if ((load === null || load.length === 0) && (href === null || href.length === 0)) {
-      return info;
-    }
-
-    info.anchor = target;
-    info.instruction = load ?? href;
-
-    const leftButtonClicked: boolean = event.button === 0;
-
-    info.shouldHandleEvent = leftButtonClicked;
-    return info;
+    @ILogger private readonly logger: ILogger,
+  ) {
+    this.logger = logger.scopeTo('LinkHandler');
   }
 
-  /**
-   * Gets a value indicating whether or not an anchor targets the current window.
-   *
-   * @param target - The anchor element whose target should be inspected.
-   * @returns True if the target of the link element is this window; false otherwise.
-   */
-  private targetIsThisWindow(target: IElement): boolean {
-    const targetWindow = target.getAttribute('target');
-
-    return (
-      targetWindow === null ||
-      targetWindow === this.window.name ||
-      targetWindow === '_self'
-    );
-  }
-
-  @bound
-  public onClick(e: IMouseEvent): void {
-    const info = this.getEventInfo(e);
-
-    if (info.shouldHandleEvent) {
-      e.preventDefault();
-      this.router.load(info.instruction ?? '', { context: info.anchor }).catch(err => Promise.reject(err));
+  public readonly onClick = (e: IMouseEvent): void => {
+    // Ensure this is an ordinary left-button click.
+    if (e.altKey || e.ctrlKey || e.metaKey || e.shiftKey || e.button !== 0) {
+      this.logger.trace(`ignoring mouse event with (altKey:${e.altKey},ctrlKey:${e.ctrlKey},metaKey:${e.metaKey},shiftKey:${e.shiftKey},button:${e.button})`);
+      return;
     }
-  }
+
+    // Ensure there is an anchor tag in the composed path.
+    const path = e.composedPath() as Node[];
+    const anchor = path.find(isAnchor);
+    if (anchor === void 0) {
+      this.logger.warn(`No anchor tag in ComposedPath for event target ${(e.target as Node).nodeName}`);
+      return;
+    }
+
+    // Ensure the anchor is not explicitly marked as external.
+    if (anchor.hasAttribute('external') || anchor.hasAttribute('data-external')) {
+      this.logger.trace(`ignoring mouse event because anchor is marked as external`);
+      return;
+    }
+
+    // Ensure the anchor targets the current window.
+    switch (anchor.getAttribute('target')) {
+      case null:
+      case this.window.name:
+      case '_self':
+        break;
+      default:
+        this.logger.trace(`ignoring mouse event because anchor does not target the current window`);
+        return;
+    }
+
+    let instruction: string | null = null;
+    // See if there's a `load` attribute on the anchor, if so, use its value instead of the href (if any).
+    const load = CustomAttribute.for<LoadCustomAttribute>(anchor, 'load')?.viewModel.value;
+    if (typeof load === 'string' && load.length > 0) {
+      // TODO: are we sure that a `load` instruction with length 0 should be ignored?
+      instruction = load;
+    } else if (this.router.options.useHref) {
+      // TODO: are we sure that a `href` attribute should be used even if its length is 0?
+      instruction = anchor.getAttribute('href');
+    }
+
+    if (instruction === null) {
+      this.logger.trace(`ignoring mouse event because the instruction is null`);
+      return;
+    }
+
+    this.logger.trace(`loading instruction '${instruction}'`);
+
+    e.preventDefault();
+    // Floating promises from `Router#load` are ok because the router keeps track of state and handles the errors, etc.
+    void this.router.load(instruction, { context: anchor });
+  };
+}
+
+function isAnchor(target: Node): target is HTMLAnchorElement {
+  return target.nodeName === 'A';
 }

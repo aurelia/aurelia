d8de3b1b298d6a58238be75e045bcbea8b14a115
Jürgen Wenzel <jurgen@jwenzel.se>
2021-02-05 15:37:16 +0100

    refactor(router): use resource utils

--- packages/router/src/resources/viewport.ts
+++ packages/router/src/resources/viewport.ts
@@ -1,4 +1,3 @@
-/* eslint-disable compat/compat */
 import { IContainer, IEventAggregator } from '@aurelia/kernel';
 import {
   bindable,
@@ -17,7 +16,7 @@ import { IRouter } from '../index.js';
 import { Viewport, IViewportOptions } from '../viewport.js';
 import { Runner, Step } from '../utilities/runner.js';
 import { IRoutingController } from '../endpoints/endpoint.js';
-import { RouterStartEvent } from '../router.js';
+import { waitForRouterStart, getValueOrAttribute } from './utils.js';
 
 export const ParentViewport = CustomElement.createInjectable();
 
@@ -107,7 +106,7 @@ export class ViewportCustomElement implements ICustomElementViewModel {
 
     return Runner.run<void>(null,
       // The first viewport(s) might be hydrated before the router is started
-      () => this.waitForRouterStart(),
+      () => waitForRouterStart(this.router, this.ea),
       () => {
         // Only call connect this early if we need to
         if (this.router.isRestrictedNavigation) {
@@ -121,7 +120,7 @@ export class ViewportCustomElement implements ICustomElementViewModel {
     this.isBound = true;
     return Runner.run(null,
       // The first viewport(s) might be bound before the router is started
-      () => this.waitForRouterStart(),
+      () => waitForRouterStart(this.router, this.ea),
       () => {
         // Prefer to connect here since we've got bound data in component
         if (!this.router.isRestrictedNavigation) {
@@ -173,41 +172,56 @@ export class ViewportCustomElement implements ICustomElementViewModel {
    * Connect this custom element to a router endpoint (Viewport).
    */
   public connect(): void {
-
     // Collect custom element options from either properties (if the custom
     // element has been bound) or from html attributes
-    const name: string = this.getAttribute('name', this.name) as string;
-    let value: string | boolean | undefined = this.getAttribute('no-scope', this.noScope);
+    const name: string = getValueOrAttribute('name', this.name, this.isBound, this.element) as string;
+    const options: IViewportOptions = {};
     // Endpoint property is `scope` but html attribute is `no-scope` so negate it
-    const options: IViewportOptions = { scope: value === void 0 || !(value as boolean) ? true : false };
-    value = this.getAttribute('used-by', this.usedBy);
-    if (value !== void 0) {
-      options.usedBy = value as string;
-    }
-    value = this.getAttribute('default', this.default);
-    if (value !== void 0) {
-      options.default = value as string;
-    }
-    value = this.getAttribute('fallback', this.fallback);
-    if (value !== void 0) {
-      options.fallback = value as string;
-    }
-    value = this.getAttribute('no-link', this.noLink, true);
-    if (value !== void 0) {
-      options.noLink = value as boolean;
-    }
-    value = this.getAttribute('no-title', this.noTitle, true);
-    if (value !== void 0) {
-      options.noTitle = value as boolean;
-    }
-    value = this.getAttribute('no-history', this.noHistory, true);
-    if (value !== void 0) {
-      options.noHistory = value as boolean;
-    }
-    value = this.getAttribute('stateful', this.stateful, true);
-    if (value !== void 0) {
-      options.stateful = value as boolean;
-    }
+    options.scope = !(getValueOrAttribute('no-scope', this.noScope, false, this.element, true) as boolean);
+    options.usedBy = getValueOrAttribute('used-by', this.usedBy, this.isBound, this.element) as string;
+    options.default = getValueOrAttribute('default', this.default, this.isBound, this.element) as string;
+    options.fallback = getValueOrAttribute('fallback', this.fallback, this.isBound, this.element) as string;
+    options.noLink = getValueOrAttribute('no-link', this.noLink, this.isBound, this.element, true) as boolean;
+    options.noTitle = getValueOrAttribute('no-title', this.noTitle, this.isBound, this.element, true) as boolean;
+    options.noHistory = getValueOrAttribute('no-history', this.noHistory, this.isBound, this.element, true) as boolean;
+    options.stateful = getValueOrAttribute('stateful', this.stateful, this.isBound, this.element, true) as boolean;
+
+    // let value: string | boolean | undefined = this.getAttribute('no-scope', this.noScope);
+    // // Endpoint property is `scope` but html attribute is `no-scope` so negate it
+    // const options: IViewportOptions = { scope: value === void 0 || !(value as boolean) ? true : false };
+    // value = this.getAttribute('used-by', this.usedBy);
+    // if (value !== void 0) {
+    //   options.usedBy = value as string;
+    // }
+    // value = this.getAttribute('default', this.default);
+    // if (value !== void 0) {
+    //   options.default = value as string;
+    // }
+    // value = this.getAttribute('fallback', this.fallback);
+    // if (value !== void 0) {
+    //   options.fallback = value as string;
+    // }
+    // value = this.getAttribute('no-link', this.noLink, true);
+    // if (value !== void 0) {
+    //   options.noLink = value as boolean;
+    // }
+    // value = this.getAttribute('no-title', this.noTitle, true);
+    // if (value !== void 0) {
+    //   options.noTitle = value as boolean;
+    // }
+    // value = this.getAttribute('no-history', this.noHistory, true);
+    // if (value !== void 0) {
+    //   options.noHistory = value as boolean;
+    // }
+    // value = this.getAttribute('stateful', this.stateful, true);
+    // if (value !== void 0) {
+    //   options.stateful = value as boolean;
+    // }
+
+    // Delete all keys with undefined value
+    // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
+    Object.keys(options).forEach(key => options[key as keyof typeof options] === undefined && delete options[key as keyof typeof options]);
+
     this.controller.routingContainer = this.container;
     this.viewport = this.router.connectEndpoint(this.viewport, 'Viewport', this, name, options) as Viewport;
   }
@@ -232,49 +246,5 @@ export class ViewportCustomElement implements ICustomElementViewModel {
     } else {
       this.element.classList.remove('viewport-active');
     }
-  }
-
-  /**
-   * Get values from either properties or html attributes, depending on
-   * whether the custom element is bound or not.
-   *
-   * @param key - Property key
-   * @param value - The value that's used if custom element is bound
-   * @param checkExists - Whether only the existance of the html attribute
-   * should be checked and returned as a boolean
-   */
-  private getAttribute(key: string, value: string | boolean, checkExists: boolean = false): string | boolean | undefined {
-    if (this.isBound && !checkExists) {
-      return value;
-    } else {
-      if (this.element.hasAttribute(key)) {
-        if (checkExists) {
-          return true;
-        } else {
-          value = this.element.getAttribute(key) as string;
-          if (value.length > 0) {
-            return value;
-          }
-        }
-      }
-    }
-    return value;
-  }
-
-  /**
-   * Make it possible to wait for router start by subscribing to the
-   * router start event and return a promise that's resolved when
-   * the router start event fires.
-   */
-  private waitForRouterStart(): void | Promise<void> {
-    if (this.router.isActive) {
-      return;
-    }
-    return new Promise((resolve) => {
-      const subscription = this.ea.subscribe(RouterStartEvent.eventName, () => {
-        resolve();
-        subscription.dispose();
-      });
-    });
   }
 }

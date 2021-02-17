3b2e832338c9b5fb76634871dbab807c23bb3160
Jürgen Wenzel <jurgen@jwenzel.se>
2021-02-17 16:13:36 +0100

    refactor(router): comment & clean up

--- packages/router/src/utilities/open-promise.ts
+++ packages/router/src/utilities/open-promise.ts
@@ -1,28 +1,51 @@
 /**
- *
- * NOTE: This file is still WIP and will go through at least one more iteration of refactoring, commenting and clean up!
- * In its current state, it is NOT a good source for learning about the inner workings and design of the router.
- *
+ * The OpenPromise provides an open API to a promise.
  */
 export class OpenPromise<T = void> {
+  /**
+   * Whether the promise is still pending (not settled)
+   */
   public isPending: boolean = true;
+
+  /**
+   * The actual promise
+   */
   public promise!: Promise<T>;
-  public res!: (value?: T | PromiseLike<T>) => void;
-  public rej!: (value?: T | PromiseLike<T>) => void;
+
+  /**
+   * The resolve method of the promise
+   */
+  private _resolve!: (value?: T | PromiseLike<T>) => void;
+
+  /**
+   * The reject method of the promise
+   */
+  private _reject!: (reason?: any) => void;
+
   public constructor() {
-    this.promise = new Promise((res, rej) => {
-      this.res = res;
-      this.rej = rej;
+    this.promise = new Promise((resolve, reject) => {
+      this._resolve = resolve as (value?: T | PromiseLike<T>) => void;
+      this._reject = reject;
     });
   }
 
+  /**
+   * Resolve the (open) promise.
+   *
+   * @param value - The value to resolve with
+   */
   public resolve(value?: T | PromiseLike<T>): void {
-    this.res(value);
+    this._resolve(value);
     this.isPending = false;
   }
-  public reject(value?: T | PromiseLike<T>): void {
-    this.rej(value);
+
+  /**
+   * Reject the (open) promise.
+   *
+   * @param reason - The reason the promise is rejected
+   */
+  public reject(reason?: any): void {
+    this._reject(reason);
     this.isPending = false;
   }
 }
-

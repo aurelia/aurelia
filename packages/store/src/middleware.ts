f9e7620654d6228ea16be930ea5ba3a790a70cdc
Vildan Softic <zewa666@gmail.com>
2021-03-16 20:51:33 +0100

    feat(store): use STORE.container for global DOM api

--- packages/store/src/middleware.ts
+++ packages/store/src/middleware.ts
@@ -1,3 +1,7 @@
+import { IPlatform } from '@aurelia/kernel';
+import { IWindow } from '@aurelia/runtime-html';
+import { STORE } from './store';
+
 export const DEFAULT_LOCAL_STORAGE_KEY = "aurelia-store-state";
 
 export interface CallingAction {
@@ -16,33 +20,37 @@ export enum MiddlewarePlacement {
   After = "after"
 }
 
-export function logMiddleware(state: unknown, _: unknown, settings?: { logType: "debug" | "error" | "info" | "log" | "trace" | "warn" }) {
-  // eslint-disable-next-line no-undef
-  const logType = settings?.logType !== undefined && console?.hasOwnProperty(settings.logType) ? settings.logType : "log";
-  // eslint-disable-next-line no-console, no-undef
-  console[logType]("New state: ", state);
+export function logMiddleware(state: unknown, _: unknown, settings?: { logType: "debug" | "error" | "info" | "log" | "trace" | "warn" }): void {
+  const cons = STORE.container.get(IPlatform).console;
+  const logType = settings?.logType !== undefined && cons[settings.logType] !== undefined ? settings.logType : "log";
+
+  cons[logType]("New state: ", state);
 }
 
-export function localStorageMiddleware(state: unknown, _: unknown, settings?: { key: string }) {
-  if (globalThis.localStorage !== undefined) {
+export function localStorageMiddleware(state: unknown, _: unknown, settings?: { key: string }): void {
+  const localStorage = STORE.container.get(IWindow).localStorage;
+
+  if (localStorage !== undefined) {
     const key = settings?.key !== undefined ? settings.key : DEFAULT_LOCAL_STORAGE_KEY;
 
-    globalThis.localStorage.setItem(key, JSON.stringify(state));
+    localStorage.setItem(key, JSON.stringify(state));
   }
 }
 
-export function rehydrateFromLocalStorage<T>(state: T, key?: string) {
-  if (globalThis.localStorage === undefined) {
+export function rehydrateFromLocalStorage<T>(state: T, key?: string): T {
+  const localStorage = STORE.container.get(IWindow).localStorage;
+
+  if (localStorage === undefined) {
     return state;
   }
 
-  const storedState = globalThis.localStorage.getItem(key === undefined ? DEFAULT_LOCAL_STORAGE_KEY : key);
+  const storedState = localStorage.getItem(key === undefined ? DEFAULT_LOCAL_STORAGE_KEY : key);
   if (storedState === null || storedState === "") {
     return state;
   }
 
   try {
-    return JSON.parse(storedState);
+    return JSON.parse(storedState) as T;
   } catch { /**/ }
 
   return state;

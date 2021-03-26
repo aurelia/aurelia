eb92a73134b9548a58aea7cb9e3b286bea5b73dd
bigopon <bigopon.777@gmail.com>
2021-03-26 21:03:23 +1100

    feat(dialog): add default IDIalogService

--- packages/runtime-html/src/templating/dialog/dialog-configuration.ts
+++ packages/runtime-html/src/templating/dialog/dialog-configuration.ts
@@ -1,8 +1,9 @@
-import { IContainer, IRegistry, noop, onResolve } from '@aurelia/kernel';
+import { IContainer, IRegistry, noop } from '@aurelia/kernel';
 
 import { IGlobalDialogSettings } from './dialog-interfaces.js';
-import { DefaultGlobalSettings, DefaultDialogAnimator, DefaultDialogDomRenderer } from './dialog-default-impl';
+import { DefaultGlobalSettings, DefaultDialogAnimator, DefaultDialogDomRenderer } from './dialog-default-impl.js';
 import { AppTask } from '../../app-task.js';
+import { DialogService } from './dialog-service.js';
 
 export type DialogConfigurationProvider = (settings: IGlobalDialogSettings) => void | Promise<unknown>;
 
@@ -15,17 +16,13 @@ export interface IDialogConfiguration extends IRegistry {
 function createConfiguration(settingsProvider: DialogConfigurationProvider, registrations: IRegistry[] = []): IDialogConfiguration {
   return {
     settingsProvider: settingsProvider,
-    register(container: IContainer) {
-      return container.register(
-        AppTask
-          .with(IContainer)
-          .beforeCreate()
-          .call(c => onResolve(
-            settingsProvider(c.get(IGlobalDialogSettings)),
-            () => { c.register(...registrations); },
-          ))
-      );
-    },
+    register: (ctn: IContainer) => ctn.register(
+      ...registrations,
+      AppTask
+        .with(IContainer)
+        .beforeCreate()
+        .call(c => settingsProvider(c.get(IGlobalDialogSettings)) as void)
+    ),
     customize(cb: DialogConfigurationProvider, regs?: IRegistry[]) {
       return createConfiguration(cb, regs ?? registrations);
     },
@@ -43,12 +40,13 @@ DialogConfiguration.customize(settings => {
 export const DialogConfiguration = createConfiguration(() => {
   throw new Error('Invalid dialog configuration. ' +
     'Specify the implementations for ' +
-    '<IGlobalDialogSettings>, <IDialogDomRenderer> and <IDialogAnimator>, ' +
+    '<IDialogService>, <IGlobalDialogSettings>, <IDialogDomRenderer> and <IDialogAnimator>, ' +
     'or use the DefaultDialogConfiguration export.'
   );
 });
 
 export const DefaultDialogConfiguration = createConfiguration(noop, [
+  DialogService,
   DefaultGlobalSettings,
   DefaultDialogDomRenderer,
   DefaultDialogAnimator,

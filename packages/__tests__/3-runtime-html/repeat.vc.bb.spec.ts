445f912364cbd16f8b7a55ed7c0995bbeeaacb31
Sayan751 <sayan751@gmail.com>
2021-06-06 20:04:01 +0530

    test: repeatervc-bb integration

--- packages/__tests__/3-runtime-html/repeat.vc.bb.spec.ts
+++ packages/__tests__/3-runtime-html/repeat.vc.bb.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-await-in-loop */
 import { IContainer } from '@aurelia/kernel';
 import { Aurelia, CustomElement, ICustomElementController, IPlatform, valueConverter } from '@aurelia/runtime-html';
 import { assert, TestContext } from '@aurelia/testing';
@@ -8,7 +9,7 @@ describe.only('repeat value-converter integration', function () {
     template: string;
     registrations: any[];
   }
-  class AuSlotTestExecutionContext implements TestExecutionContext<any> {
+  class RepeaterTestExecutionContext implements TestExecutionContext<any> {
     private _scheduler: IPlatform;
     public constructor(
       public ctx: TestContext,
@@ -22,7 +23,7 @@ describe.only('repeat value-converter integration', function () {
   }
 
   async function testRepeaterVC(
-    testFunction: TestFunction<AuSlotTestExecutionContext>,
+    testFunction: TestFunction<RepeaterTestExecutionContext>,
     { template, registrations = [] }: Partial<TestSetupContext> = {}
   ) {
     const ctx = TestContext.create();
@@ -55,7 +56,7 @@ describe.only('repeat value-converter integration', function () {
 
     assert.strictEqual(error, null);
 
-    await testFunction(new AuSlotTestExecutionContext(ctx, au, container, host, app, error));
+    await testFunction(new RepeaterTestExecutionContext(ctx, au, container, host, app, error));
 
     if (error === null) {
       await au.stop();
@@ -97,226 +98,186 @@ describe.only('repeat value-converter integration', function () {
   const oddExpected = Array.from({ length: 10 }, (_, i) => i % 2 === 0 ? `<span>${i + 1}</span>` : '').join('');
   const evenExpected = Array.from({ length: 10 }, (_, i) => i % 2 === 1 ? `<span>${i + 1}</span>` : '').join('');
 
-  $it('change to inner array - identity VC',
-    async function (ctx) {
-      const host = ctx.host;
-      assert.html.innerEqual(host, identityExpected, 'initial');
-      ctx.app.arr.push(11);
-      await ctx.platform.domWriteQueue.yield();
-      assert.html.innerEqual(host, `${identityExpected}<span>11</span>`, 'after mutation');
-    },
-    {
-      template: `<span repeat.for="item of arr | identity">\${item}</span>`
-    });
+  type Change = [change: (ctx: RepeaterTestExecutionContext) => void | Promise<void>, expected: string];
 
-  $it('inner array instance change - identity VC',
-    async function (ctx) {
-      const host = ctx.host;
-      assert.html.innerEqual(host, identityExpected, 'initial');
-      ctx.app.arr = Array.from({ length: 5 }, (_, i) => i * 5);
-      await ctx.platform.domWriteQueue.yield();
-      assert.html.innerEqual(host, `<span>0</span><span>5</span><span>10</span><span>15</span><span>20</span>`, 'after change');
-    },
-    {
-      template: `<span repeat.for="item of arr | identity">\${item}</span>`
-    });
+  class TestData {
+    public readonly changes: Change[];
+    public constructor(
+      public readonly name: string,
+      public readonly template: string,
+      public readonly expectedInitial: string,
+      ...changes: Change[]
+    ) { this.changes = changes; }
+  }
 
-  $it('inner array instance change followed by array mutation - identity VC',
-    async function (ctx) {
-      const host = ctx.host;
-      assert.html.innerEqual(host, identityExpected, 'initial');
-      ctx.app.arr = Array.from({ length: 5 }, (_, i) => i * 5);
-      await ctx.platform.domWriteQueue.yield();
-      assert.html.innerEqual(host, `<span>0</span><span>5</span><span>10</span><span>15</span><span>20</span>`, 'after change');
-      ctx.app.arr.push(25, 30);
-      await ctx.platform.domWriteQueue.yield();
-      assert.html.innerEqual(host, `<span>0</span><span>5</span><span>10</span><span>15</span><span>20</span><span>25</span><span>30</span>`, 'after change');
-    },
-    {
-      template: `<span repeat.for="item of arr | identity">\${item}</span>`
-    });
+  function* getTestData() {
+    const identityArrTmplt = `<span repeat.for="item of arr | identity">\${item}</span>`;
+    yield new TestData(
+      'array mutation - identity VC',
+      identityArrTmplt,
+      identityExpected,
+      [(ctx) => { ctx.app.arr.push(11); }, `${identityExpected}<span>11</span>`],
+      [(ctx) => { ctx.app.arr.unshift(12); }, `<span>12</span>${identityExpected}<span>11</span>`],
+      [(ctx) => { ctx.app.arr.pop(); }, `<span>12</span>${identityExpected}`],
+      [(ctx) => { ctx.app.arr.shift(); }, identityExpected],
+    );
+    yield new TestData(
+      'array instance change - identity VC',
+      identityArrTmplt,
+      identityExpected,
+      [
+        (ctx) => { ctx.app.arr = Array.from({ length: 5 }, (_, i) => i * 5); },
+        `<span>0</span><span>5</span><span>10</span><span>15</span><span>20</span>`
+      ],
+    );
+    yield new TestData(
+      'array instance change -> array mutation - identity VC',
+      identityArrTmplt,
+      identityExpected,
+      [
+        (ctx) => { ctx.app.arr = Array.from({ length: 5 }, (_, i) => i * 5); },
+        `<span>0</span><span>5</span><span>10</span><span>15</span><span>20</span>`
+      ],
+      [
+        (ctx) => { ctx.app.arr.push(25, 30); },
+        `<span>0</span><span>5</span><span>10</span><span>15</span><span>20</span><span>25</span><span>30</span>`
+      ],
+    );
 
-  $it('change to inner array - oddEven VC',
-    async function (ctx) {
-      const host = ctx.host;
-      assert.html.innerEqual(host, evenExpected, 'initial');
-      ctx.app.arr.push(11, 12);
-      await ctx.platform.domWriteQueue.yield();
-      assert.html.innerEqual(host, `${evenExpected}<span>12</span>`, 'after mutation #1');
-      ctx.app.arr.push(13, 14);
-      await ctx.platform.domWriteQueue.yield();
-      assert.html.innerEqual(host, `${evenExpected}<span>12</span><span>14</span>`, 'after mutation #2');
-    },
-    {
-      template: `<span repeat.for="item of arr | oddEven:true">\${item}</span>`
-    });
+    const oeVcTrueTmplt = `<span repeat.for="item of arr | oddEven:true">\${item}</span>`;
+    yield new TestData(
+      'array mutation - oddEven VC',
+      oeVcTrueTmplt,
+      evenExpected,
+      [(ctx) => { ctx.app.arr.push(11, 12); }, `${evenExpected}<span>12</span>`],
+      [(ctx) => { ctx.app.arr.unshift(14, 13); }, `<span>14</span>${evenExpected}<span>12</span>`],
+      [(ctx) => { ctx.app.arr.pop(); }, `<span>14</span>${evenExpected}`],
+      [(ctx) => { ctx.app.arr.shift(); }, evenExpected],
+    );
 
-  $it('inner array instance change - oddEven VC',
-    async function (ctx) {
-      const host = ctx.host;
-      assert.html.innerEqual(host, evenExpected, 'initial');
-      ctx.app.arr = Array.from({ length: 5 }, (_, i) => i * 5);
-      await ctx.platform.domWriteQueue.yield();
-      assert.html.innerEqual(host, `<span>0</span><span>10</span><span>20</span>`, 'after change');
-      ctx.app.arr = Array.from({ length: 5 }, (_, i) => i * 7);
-      await ctx.platform.domWriteQueue.yield();
-      assert.html.innerEqual(host, `<span>0</span><span>14</span><span>28</span>`, 'after change');
-    },
-    {
-      template: `<span repeat.for="item of arr | oddEven:true">\${item}</span>`
-    });
+    yield new TestData(
+      'array instance change - oddEven VC',
+      oeVcTrueTmplt,
+      evenExpected,
+      [(ctx) => { ctx.app.arr = Array.from({ length: 5 }, (_, i) => i * 5); }, `<span>0</span><span>10</span><span>20</span>`],
+      [(ctx) => { ctx.app.arr = Array.from({ length: 5 }, (_, i) => i * 7); }, `<span>0</span><span>14</span><span>28</span>`],
+    );
 
-  $it('change to VC arg - oddEven VC',
-    async function (ctx) {
-      const host = ctx.host;
-      assert.html.innerEqual(host, evenExpected, 'initial');
-      ctx.app.$controller.scope.overrideContext.even = false;
-      await ctx.platform.domWriteQueue.yield();
-      assert.html.innerEqual(host, oddExpected, 'after change in VC arg');
-    },
-    {
-      template: `<let even.bind="true"></let><span repeat.for="item of arr | oddEven:even">\${item}</span>`
-    });
+    yield new TestData(
+      'change to VC arg - oddEven VC',
+      `<let even.bind="true"></let><span repeat.for="item of arr | oddEven:even">\${item}</span>`,
+      evenExpected,
+      [(ctx) => { ctx.app.$controller.scope.overrideContext.even = false; }, oddExpected,],
+    );
 
-  $it('change to inner array - oddEven VC - multipleOf VC',
-    async function (ctx) {
-      const host = ctx.host;
-      assert.html.innerEqual(host, '<span>6</span>', 'initial');
-      ctx.app.arr.push(11, 12);
-      await ctx.platform.domWriteQueue.yield();
-      assert.html.innerEqual(host, '<span>6</span><span>12</span>', 'after mutation #1');
-      ctx.app.arr.push(13, 14, 18, 19);
-      await ctx.platform.domWriteQueue.yield();
-      assert.html.innerEqual(host, '<span>6</span><span>12</span><span>18</span>', 'after mutation #2');
-    },
-    {
-      template: `<span repeat.for="item of arr | oddEven:true | multipleOf:3">\${item}</span>`
-    });
+    const twoVcTmplt = `<span repeat.for="item of arr | oddEven:true | multipleOf:3">\${item}</span>`;
+    yield new TestData(
+      'array mutation - oddEven VC - multipleOf VC',
+      twoVcTmplt,
+      '<span>6</span>',
+      [(ctx) => { ctx.app.arr.push(11, 12); }, '<span>6</span><span>12</span>',],
+      [(ctx) => { ctx.app.arr.unshift(18, 19, 13, 14); }, '<span>18</span><span>6</span><span>12</span>',],
+      [(ctx) => { ctx.app.arr.pop(); }, '<span>18</span><span>6</span>',],
+      [(ctx) => { ctx.app.arr.shift(); }, '<span>6</span>',],
+    );
 
-  $it('inner array instance change - oddEven VC - multipleOf VC',
-    async function (ctx) {
-      const host = ctx.host;
-      assert.html.innerEqual(host, '<span>6</span>', 'initial');
-      ctx.app.arr = Array.from({ length: 10 }, (_, i) => i + 11);
-      await ctx.platform.domWriteQueue.yield();
-      assert.html.innerEqual(host, '<span>12</span><span>18</span>', 'after change');
-      ctx.app.arr = Array.from({ length: 10 }, (_, i) => i + 21);
-      await ctx.platform.domWriteQueue.yield();
-      assert.html.innerEqual(host, '<span>24</span><span>30</span>', 'after change');
-    },
-    {
-      template: `<span repeat.for="item of arr | oddEven:true | multipleOf:3">\${item}</span>`
-    });
+    yield new TestData(
+      'array instance change - oddEven VC - multipleOf VC',
+      twoVcTmplt,
+      '<span>6</span>',
+      [(ctx) => { ctx.app.arr = Array.from({ length: 10 }, (_, i) => i + 11); }, '<span>12</span><span>18</span>',],
+      [(ctx) => { ctx.app.arr = Array.from({ length: 10 }, (_, i) => i + 21); }, '<span>24</span><span>30</span>',],
+    );
 
-  $it('change to VC arg - oddEven VC - multipleOf VC',
-    async function (ctx) {
-      const host = ctx.host;
-      assert.html.innerEqual(host, '<span>3</span><span>9</span>', 'initial');
-      const oc = ctx.app.$controller.scope.overrideContext;
-      oc.even = true;
-      oc.n = 4;
-      await ctx.platform.domWriteQueue.yield();
-      assert.html.innerEqual(host, '<span>4</span><span>8</span>', 'after change in VC arg');
-    },
-    {
-      template: `<let even.bind="false" n.bind="3"></let><span repeat.for="item of arr | oddEven:even | multipleOf:n">\${item}</span>`
-    });
+    yield new TestData(
+      'change to VC arg - oddEven VC - multipleOf VC',
+      `<let even.bind="false" n.bind="3"></let><span repeat.for="item of arr | oddEven:even | multipleOf:n">\${item}</span>`,
+      '<span>3</span><span>9</span>',
+      [
+        (ctx) => {
+          const oc = ctx.app.$controller.scope.overrideContext;
+          oc.even = true;
+          oc.n = 4;
+        },
+        '<span>4</span><span>8</span>',
+      ],
+    );
 
-  $it('change to inner array - noop BB',
-    async function (ctx) {
-      const host = ctx.host;
-      assert.html.innerEqual(host, identityExpected, 'initial');
-      ctx.app.arr.push(11);
-      await ctx.platform.domWriteQueue.yield();
-      assert.html.innerEqual(host, `${identityExpected}<span>11</span>`, 'after mutation');
-    },
-    {
-      template: `<span repeat.for="item of arr | noop">\${item}</span>`
-    });
+    const noopBBTmplt = `<span repeat.for="item of arr | noop">\${item}</span>`;
+    yield new TestData(
+      'array mutation - noop BB',
+      noopBBTmplt,
+      identityExpected,
+      [(ctx) => { ctx.app.arr.push(11); }, `${identityExpected}<span>11</span>`,],
+    );
+    yield new TestData(
+      'array instance change - noop BB',
+      noopBBTmplt,
+      identityExpected,
+      [(ctx) => { ctx.app.arr = Array.from({ length: 5 }, (_, i) => i * 5); }, `<span>0</span><span>5</span><span>10</span><span>15</span><span>20</span>`,],
+    );
 
-  $it('inner array instance change - noop BB',
-    async function (ctx) {
-      const host = ctx.host;
-      assert.html.innerEqual(host, identityExpected, 'initial');
-      ctx.app.arr = Array.from({ length: 5 }, (_, i) => i * 5);
-      await ctx.platform.domWriteQueue.yield();
-      assert.html.innerEqual(host, `<span>0</span><span>5</span><span>10</span><span>15</span><span>20</span>`, 'after change');
-    },
-    {
-      template: `<span repeat.for="item of arr | noop">\${item}</span>`
-    });
+    const twoVcBBTmplt = `<span repeat.for="item of arr | oddEven:true | multipleOf:3 & noop">\${item}</span>`;
+    yield new TestData(
+      'array mutation - oddEven VC - multipleOf VC - noop BB',
+      twoVcBBTmplt,
+      '<span>6</span>',
+      [(ctx) => { ctx.app.arr.push(11, 12); }, '<span>6</span><span>12</span>',],
+      [(ctx) => { ctx.app.arr.push(13, 14, 18, 19); }, '<span>6</span><span>12</span><span>18</span>',],
+    );
+    yield new TestData(
+      'array instance change - oddEven VC - multipleOf VC - noop BB',
+      twoVcBBTmplt,
+      '<span>6</span>',
+      [(ctx) => { ctx.app.arr = Array.from({ length: 10 }, (_, i) => i + 11); }, '<span>12</span><span>18</span>',],
+      [(ctx) => { ctx.app.arr = Array.from({ length: 10 }, (_, i) => i + 21); }, '<span>24</span><span>30</span>',],
+    );
 
-  $it('change to inner array - oddEven VC - multipleOf VC - noop BB',
-    async function (ctx) {
-      const host = ctx.host;
-      assert.html.innerEqual(host, '<span>6</span>', 'initial');
-      ctx.app.arr.push(11, 12);
-      await ctx.platform.domWriteQueue.yield();
-      assert.html.innerEqual(host, '<span>6</span><span>12</span>', 'after mutation #1');
-      ctx.app.arr.push(13, 14, 18, 19);
-      await ctx.platform.domWriteQueue.yield();
-      assert.html.innerEqual(host, '<span>6</span><span>12</span><span>18</span>', 'after mutation #2');
-    },
-    {
-      template: `<span repeat.for="item of arr | oddEven:true | multipleOf:3 & noop">\${item}</span>`
-    });
+    yield new TestData(
+      'change to VC arg - oddEven VC - multipleOf VC - noop BB',
+      `<let even.bind="false" n.bind="3"></let><span repeat.for="item of arr | oddEven:even | multipleOf:n & noop">\${item}</span>`,
+      '<span>3</span><span>9</span>',
+      [
+        (ctx) => {
+          const oc = ctx.app.$controller.scope.overrideContext;
+          oc.even = true;
+          oc.n = 4;
+        },
+        '<span>4</span><span>8</span>',
+      ],
+    );
 
-  $it('inner array instance change - oddEven VC - multipleOf VC - noop BB',
-    async function (ctx) {
-      const host = ctx.host;
-      assert.html.innerEqual(host, '<span>6</span>', 'initial');
-      ctx.app.arr = Array.from({ length: 10 }, (_, i) => i + 11);
-      await ctx.platform.domWriteQueue.yield();
-      assert.html.innerEqual(host, '<span>12</span><span>18</span>', 'after change');
-      ctx.app.arr = Array.from({ length: 10 }, (_, i) => i + 21);
-      await ctx.platform.domWriteQueue.yield();
-      assert.html.innerEqual(host, '<span>24</span><span>30</span>', 'after change');
-    },
-    {
-      template: `<span repeat.for="item of arr | oddEven:true | multipleOf:3 & noop">\${item}</span>`
-    });
+    yield new TestData(
+      'array instance change - array reverse - oddEven VC',
+      `<span repeat.for="item of arr.reverse() | oddEven:true">\${item}</span>`,
+      '<span>10</span><span>8</span><span>6</span><span>4</span><span>2</span>',
+      [(ctx) => { ctx.app.arr = Array.from({ length: 5 }, (_, i) => i * 5); }, `<span>20</span><span>10</span><span>0</span>`,],
+      // alternative to push; function call is treated pure, a push won't work here
+      [(ctx) => { ctx.app.arr = [...ctx.app.arr, 42]; }, `<span>42</span><span>0</span><span>10</span><span>20</span>`,],
+      [(ctx) => { ctx.app.arr = Array.from({ length: 5 }, (_, i) => i * 7); }, `<span>28</span><span>14</span><span>0</span>`,],
+    );
 
-  $it('change to VC arg - oddEven VC - multipleOf VC - noop BB',
-    async function (ctx) {
-      const host = ctx.host;
-      assert.html.innerEqual(host, '<span>3</span><span>9</span>', 'initial');
-      const oc = ctx.app.$controller.scope.overrideContext;
-      oc.even = true;
-      oc.n = 4;
-      await ctx.platform.domWriteQueue.yield();
-      assert.html.innerEqual(host, '<span>4</span><span>8</span>', 'after change in VC arg');
-    },
-    {
-      template: `<let even.bind="false" n.bind="3"></let><span repeat.for="item of arr | oddEven:even | multipleOf:n & noop">\${item}</span>`
-    });
+    yield new TestData(
+      'array instance change - array slice - oddEven VC',
+      `<span repeat.for="item of arr.slice(0, 4) | oddEven:true">\${item}</span>`,
+      '<span>2</span><span>4</span>',
+      [(ctx) => { ctx.app.arr = Array.from({ length: 6 }, (_, i) => i * 5); }, `<span>0</span><span>10</span>`,],
+      // alternative to push; function call is treated pure, a push won't work here
+      [(ctx) => { ctx.app.arr = [42, 43, ...ctx.app.arr]; }, `<span>42</span><span>0</span>`,],
+      [(ctx) => { ctx.app.arr = Array.from({ length: 5 }, (_, i) => i * 7); }, `<span>0</span><span>14</span>`,],
+    );
+  }
 
-  $it('inner array instance change - array reverse - oddEven VC',
-    async function (ctx) {
+  for (const { name, template, expectedInitial, changes } of getTestData()) {
+    $it(name, async function (ctx) {
       const host = ctx.host;
-      assert.html.innerEqual(host, '<span>10</span><span>8</span><span>6</span><span>4</span><span>2</span>', 'initial');
-      ctx.app.arr = Array.from({ length: 5 }, (_, i) => i * 5);
-      await ctx.platform.domWriteQueue.yield();
-      assert.html.innerEqual(host, `<span>20</span><span>10</span><span>0</span>`, 'after change');
-      ctx.app.arr = Array.from({ length: 5 }, (_, i) => i * 7);
-      await ctx.platform.domWriteQueue.yield();
-      assert.html.innerEqual(host, `<span>28</span><span>14</span><span>0</span>`, 'after change');
-    },
-    {
-      template: `<span repeat.for="item of arr.reverse() | oddEven:true">\${item}</span>`
-    });
-
-  $it('inner array instance change - array slice - oddEven VC',
-    async function (ctx) {
-      const host = ctx.host;
-      assert.html.innerEqual(host, '<span>2</span><span>4</span>', 'initial');
-      ctx.app.arr = Array.from({ length: 5 }, (_, i) => i * 5);
-      await ctx.platform.domWriteQueue.yield();
-      assert.html.innerEqual(host, `<span>0</span><span>10</span>`, 'after change');
-      ctx.app.arr = Array.from({ length: 5 }, (_, i) => i * 7);
-      await ctx.platform.domWriteQueue.yield();
-      assert.html.innerEqual(host, `<span>0</span><span>14</span>`, 'after change');
-    },
-    {
-      template: `<span repeat.for="item of arr.slice(0, 4) | oddEven:true">\${item}</span>`
-    });
+      assert.html.innerEqual(host, expectedInitial, 'initial');
+      let i = 1;
+      for (const [change, expected] of changes) {
+        await change(ctx);
+        await ctx.platform.domWriteQueue.yield();
+        assert.html.innerEqual(host, expected, `post-mutation#${i++}`);
+      }
+    }, { template });
+  }
 });

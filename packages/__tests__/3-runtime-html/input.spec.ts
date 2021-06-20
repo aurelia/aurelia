a5ef635eb5e24a14f351e88e70b01cd823e434dc
bigopon <bigopon.777@gmail.com>
2021-06-21 07:02:28 +1000

    chore(tests): do not test value-as-number in jsdom

--- packages/__tests__/3-runtime-html/input.spec.ts
+++ packages/__tests__/3-runtime-html/input.spec.ts
@@ -1,6 +1,8 @@
-import { assert, createFixture } from '@aurelia/testing';
+import { assert, createFixture, PLATFORM } from '@aurelia/testing';
 
 describe('3-runtime-html/input.spec.ts', function () {
+  const isTestingInNode = PLATFORM.navigator.userAgent.includes('jsdom');
+
   it('works: input[text] value.bind', async function () {
     const { startPromise, appHost, tearDown, component, ctx } = createFixture(
       `<input value.bind="message">`,
@@ -24,25 +26,27 @@ describe('3-runtime-html/input.spec.ts', function () {
     await tearDown();
   });
 
-  it('works: input[number] + value-as-number.bind', async function () {
-    const { startPromise, appHost, tearDown, component, ctx } = createFixture(
-      `<input type=number value-as-number.bind="count">`,
-      class App {
-        public count = 0;
-      }
-    );
-    await startPromise;
+  if (!isTestingInNode) {
+    it('works: input[number] + value-as-number.bind', async function () {
+      const { startPromise, appHost, tearDown, component, ctx } = createFixture(
+        `<input type=number value-as-number.bind="count">`,
+        class App {
+          public count = 0;
+        }
+      );
+      await startPromise;
 
-    const input= appHost.querySelector('input');
-    assert.strictEqual(input.value, '0');
-    assert.strictEqual(input.valueAsNumber, 0);
+      const input= appHost.querySelector('input');
+      assert.strictEqual(input.value, '0');
+      assert.strictEqual(input.valueAsNumber, 0);
 
-    input.value = '100';
-    input.dispatchEvent(new ctx.Event('change'));
-    assert.strictEqual(component.count, 100);
+      input.value = '100';
+      input.dispatchEvent(new ctx.Event('change'));
+      assert.strictEqual(component.count, 100);
 
-    await tearDown();
-  });
+      await tearDown();
+    });
+  }
 
   it('works: textarea + value.bind', async function () {
     const { startPromise, appHost, tearDown, component, ctx } = createFixture(

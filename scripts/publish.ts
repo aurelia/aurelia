7b81eebc4d55973f61da6610258a1e9380e010f1
bigopon <bigopon.777@gmail.com>
2021-07-08 16:07:02 +1000

    chore(dist): ensure ts node available during publish

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -1,5 +1,5 @@
-import { c, createLogger } from './logger';
-import { loadPackageJson, savePackageJson } from './package.json';
+import { createLogger } from './logger';
+import { loadPackageJson } from './package.json';
 import project from './project';
 import { execSync } from 'child_process';
 import { join } from 'path';
@@ -7,7 +7,7 @@ import { join } from 'path';
 const log = createLogger('publish');
 
 (async function (): Promise<void> {
-  const [, , type] = process.argv;
+  const [, , channel/* dev/latest */] = process.argv;
 
   for (const { name, folder }
     of project.packages
@@ -22,27 +22,7 @@ const log = createLogger('publish');
         continue;
     }
 
-    execSync(`cd ${join(folder, name.kebab)} && npm run publish:${type}`);
-
-    // const isCjsPackage = folder.includes('packages-cjs');
-    // const refs = isCjsPackage ? cjsRefs : webRefs;
-    // for (const field of fields) {
-    //   pkg[field] = refs[ref][field];
-    // }
-    // if (type) {
-    //   if (type === 'none') {
-    //     if (pkg.bin) {
-    //       log(`saw a 'bin' field, so leaving the package.json "type" field as-is for: ${c.magentaBright(name.npm)}`);
-    //     } else {
-    //       log(`removing the package.json "type" field for: ${c.magentaBright(name.npm)}`);
-    //       pkg['type'] = void 0;
-    //     }
-    //   } else {
-    //     log(`changing package.json "type" to ${type} for: ${c.magentaBright(name.npm)}`);
-    //     pkg['type'] = type;
-    //   }
-    // }
-    // await savePackageJson(pkg, folder, name.kebab);
+    execSync(`cd ${join(folder, name.kebab)} && npm run publish:${channel}`);
   }
 
   log('Done.');

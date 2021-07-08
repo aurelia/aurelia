d66126ecf2cbdcbbcbe94a8d1529976932b14b09
bigopon <bigopon.777@gmail.com>
2021-07-08 13:40:51 +1000

    chore(bench): fix http-server path

--- test/benchmarking-apps/runner/src/start-app-and-bench.ts
+++ test/benchmarking-apps/runner/src/start-app-and-bench.ts
@@ -85,7 +85,7 @@ async function startApp(fxName: string, appPath: string, port: string) {
     const app = spawn(
       'node',
       [
-        '../../../packages/http-server/dist/esm/cli.js',
+        '../../../packages/http-server/dist/bundle/cli.js',
         '--root',
         join(appPath, 'dist'),
         '--port',

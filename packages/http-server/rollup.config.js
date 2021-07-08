4154938861f04cdb45e95a7ebbfc266f9d02ef1f
bigopon <bigopon.777@gmail.com>
2021-07-08 11:28:44 +1000

    chore(dist): fix http-server bin file ref path

--- packages/http-server/rollup.config.js
+++ packages/http-server/rollup.config.js
@@ -1,30 +1,49 @@
 import typescript from '@rollup/plugin-typescript';
 import pkg from './package.json';
 
-export default {
-  input: 'src/index.ts',
-  external: Object.keys(pkg.dependencies).concat('os', 'path', 'fs', 'http', 'https', 'http2'),
-  output: [
-    {
-      file: 'dist/bundle/index.js',
-      format: 'es',
-      sourcemap: true
-    },
-    {
-      file: 'dist/bundle/index.esm.js',
-      format: 'es',
-      sourcemap: true
-    },
-    {
-      file: 'dist/bundle/index.cjs.js',
-      format: 'cjs',
-      sourcemap: true
-    },
-  ],
-  plugins: [
-    typescript({
-      tsconfig: 'tsconfig.build.json',
-      inlineSources: true,
-    })
-  ]
-};
+export default [
+  {
+    input: 'src/index.ts',
+    external: Object.keys(pkg.dependencies).concat('os', 'path', 'fs', 'http', 'https', 'http2', 'url'),
+    output: [
+      {
+        file: 'dist/bundle/index.js',
+        format: 'es',
+        sourcemap: true
+      },
+      {
+        file: 'dist/bundle/index.esm.js',
+        format: 'es',
+        sourcemap: true
+      },
+      {
+        file: 'dist/bundle/index.cjs.js',
+        format: 'cjs',
+        sourcemap: true
+      },
+    ],
+    plugins: [
+      typescript({
+        tsconfig: 'tsconfig.build.json',
+        inlineSources: true,
+      })
+    ]
+  },
+  {
+    input: 'src/cli.ts',
+    external: Object.keys(pkg.dependencies).concat('os', 'path', 'fs', 'http', 'https', 'http2', 'url'),
+    output: [
+      {
+        file: 'dist/bundle/cli.js',
+        format: 'es',
+        sourcemap: true
+      },
+    ],
+    plugins: [
+      typescript({
+        tsconfig: 'tsconfig.build.json',
+        inlineSources: true,
+      })
+    ]
+  }
+];

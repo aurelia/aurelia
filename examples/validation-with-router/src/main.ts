7bc55212beb961c16967cda1c08206a88e4e0c82
Sayan751 <sayan751@gmail.com>
2021-07-22 22:00:28 +0200

    chore: adding example app for validation with routing

---
+++ examples/validation-with-router/src/main.ts
@@ -0,0 +1,18 @@
+import { RouterConfiguration } from '@aurelia/router';
+import { Aurelia, StandardConfiguration } from '@aurelia/runtime-html';
+import { ValidationHtmlConfiguration } from '@aurelia/validation-html';
+import { AppRootCustomElement as component } from './app-root';
+
+(async function () {
+  const host = document.querySelector<HTMLElement>('app');
+
+  const au = new Aurelia()
+    .register(
+      StandardConfiguration,
+      ValidationHtmlConfiguration,
+      RouterConfiguration.customize({ useUrlFragmentHash: false }),
+    );
+  au.app({ host, component });
+
+  await au.start();
+})().catch(console.error);

6d800b9000fb6c000d877f5cd7638566ef9cc7bd
bigopon <bigopon.777@gmail.com>
2021-09-03 08:15:14 +1000

    chore(deps): upgrade @types/node

--- packages/__e2e__/src/index.ts
+++ packages/__e2e__/src/index.ts
@@ -1,4 +1,5 @@
-import Aurelia, { RouterConfiguration } from 'aurelia';
+import Aurelia from 'aurelia';
+import { RouterConfiguration } from 'aurelia-direct-router';
 import { App } from './app';
 
 Aurelia

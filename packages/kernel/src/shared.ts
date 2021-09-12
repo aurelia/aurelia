009562ba1412af23dc9c53141060ad0752ab08ba
bigopon <bigopon.777@gmail.com>
2021-09-12 23:37:12 +1000

    refactor(kernel): use isType utilities for fn & string

--- packages/kernel/src/shared.ts
+++
@@ -1,8 +0,0 @@
-import { Metadata } from '@aurelia/metadata';
-
-/** @internal */
-export const getOwnMetadata = Metadata.getOwn;
-/** @internal */
-export const hasOwnMetadata = Metadata.hasOwn;
-/** @internal */
-export const defineMetadata = Metadata.define;

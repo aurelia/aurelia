d65f425175ddc67214c4ff3fca07e33d5aa8274c
bigopon <bigopon.777@gmail.com>
2022-03-08 00:25:43 +1100

    chore(tests): upgrade test script file matching, add more shortcuts

--- packages/__tests__/karma.conf.cjs
+++ packages/__tests__/karma.conf.cjs
@@ -68,7 +68,7 @@ module.exports = function (config) {
 
   const testFilePatterns = cliArgs.length > 0
     ? cliArgs.flatMap(arg => [
-        `${baseUrl}/**/*${arg.replace(/(?:\.spec(?:\.[tj]s)?)?$/, '.spec.js')}`,
+        `${baseUrl}/**/*${arg.replace(/(?:\.spec(?:\.[tj]s)?)?$/, '*.spec.js')}`,
         `${baseUrl}/**/${arg}/**/*.spec.js`,
     ])
     : [`${baseUrl}/**/*.spec.js`];

ba95a5d05c067f4f63320674143819047b029b99
Chunpeng Huo <huocp@me.com>
2022-04-05 19:20:12 +1000

    feat: parcel2 integration (#1376)

---
+++ packages-cjs/parcel-transformer/src/index.ts
@@ -0,0 +1,45 @@
+import { Transformer } from '@parcel/plugin';
+import SourceMap from '@parcel/source-map';
+import { IOptionalPreprocessOptions, preprocess } from '@aurelia/plugin-conventions';
+// eslint-disable-next-line import/no-nodejs-modules
+import { relative } from 'path';
+
+export default new Transformer({
+  async loadConfig({config}) {
+    try {
+      const conf = await config.getConfig([], {packageKey: 'aurelia'});
+      return conf ? conf.contents : {};
+    } catch (e) {
+      return {};
+    }
+  },
+  async transform({asset, config, options}) {
+    // parcel conventions puts app's index.html inside src/ folder.
+    if (asset.filePath.endsWith('src/index.html')) return [asset];
+
+    const source = await asset.getCode();
+    const result = preprocess(
+      {
+        path: relative(options.projectRoot, asset.filePath.slice()),
+        contents: source
+      },
+      config as IOptionalPreprocessOptions
+    );
+
+    if (!result) {
+      return [asset];
+    }
+
+    asset.setCode(result.code);
+    const map = new SourceMap();
+    map.addVLQMap(result.map);
+    asset.setMap(map);
+
+    if (asset.type === 'html') {
+      asset.type = 'js';
+    }
+
+    // Return the asset
+    return [asset];
+  }
+});

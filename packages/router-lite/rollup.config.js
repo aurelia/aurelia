19f0f40c79b58c6f0d6c89f125f04c4a71141b92
bigopon <bigopon.777@gmail.com>
2022-04-08 02:58:46 +1000

    refactor(router-lite): use extracted rollup scripts

--- packages/router-lite/rollup.config.js
+++ packages/router-lite/rollup.config.js
@@ -1,78 +1,11 @@
-// @ts-check
-import typescript from '@rollup/plugin-typescript';
-import replace from '@rollup/plugin-replace';
-import { terser } from 'rollup-plugin-terser';
+// // @ts-check
 import pkg from './package.json';
-import { exec } from 'child_process';
+import { getRollupConfig } from '../rollup-utils';
 
-// todo: fix the issue with the tests getting affected by minification
-const noMinified = process.env.NO_MINIFIED === 'true';
-
-const tsPluginConfig = typescript({
-  tsconfig: 'tsconfig.build.json',
-  sourceMap: true,
-  include: ['../global.d.ts', 'src/**/*.ts'],
-  noEmitOnError: false,
+export default getRollupConfig(pkg, (config, dev, envVars) => {
+  if (envVars.NO_MINIFIED === 'true') {
+    (Array.isArray(config.output) ? config.output : [config.output])
+      .forEach(output => output.plugins = []);
+  }
+  return config;
 });
-const replacePluginCfg = replace({
-  values: {
-    __DEV__: process.env.__DEV__
-  },
-  preventAssignment: true,
-});
-const terserPluginCfg = terser({
-  compress: {
-    defaults: false,
-    drop_console: false,
-    drop_debugger: false
-  },
-  mangle: {
-    properties: {
-      regex: /^_/,
-      reserved: ['__esModule']
-    }
-  },
-  format: {
-    beautify: true,
-  },
-  keep_classnames: true,
-});
-
-export default {
-  input: 'src/index.ts',
-  external: Object.keys(pkg.dependencies),
-  output: [
-    {
-      file: `dist/esm/index.dev.js`,
-      format: 'es',
-      sourcemap: true,
-    },
-    {
-      file: `dist/esm/index.js`,
-      format: 'es',
-      sourcemap: true,
-      plugins: noMinified ? [] : [terserPluginCfg]
-    },
-    {
-      file: `dist/cjs/index.dev.js`,
-      format: 'cjs',
-      sourcemap: true,
-      esModule: true,
-    },
-    {
-      file: `dist/cjs/index.js`,
-      format: 'cjs',
-      sourcemap: true,
-      plugins: noMinified ? [] : [terserPluginCfg]
-    },
-  ],
-  plugins: [
-    tsPluginConfig,
-    replacePluginCfg,
-    {
-      closeBundle() {
-        exec('npm run postrollup')
-      }
-    }
-  ].filter(Boolean),
-};

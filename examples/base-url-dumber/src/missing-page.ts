ece2bde74d2d06d4f9f8605cfd2b1c7992e3dcce
Jürgen Wenzel <jurgen@jwenzel.se>
2022-04-08 01:47:54 +0200

    fix(router): delete old base-url-dumber

--- examples/base-url-dumber/src/missing-page.ts
+++
@@ -1,8 +0,0 @@
-export class MissingPage {
-  public static parameters = ['id'];
-  public missingComponent: string ;
-
-  public enter(parameters: {id: string}): void {
-    this.missingComponent = parameters.id;
-  }
-}

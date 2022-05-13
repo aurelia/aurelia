7b123a450ca3c3ab1478da4ff46274953773f254
bigopon <bigopon.777@gmail.com>
2022-05-13 20:13:22 +1000

    build(tooling): change to cjs/mjs extension for dist

--- packages-tooling/ts-jest/rollup.config.js
+++ packages-tooling/ts-jest/rollup.config.js
@@ -1,32 +1,34 @@
-import typescript from '@rollup/plugin-typescript';
+import { getRollupConfig } from '../rollup-utils.js';
 import pkg from './package.json';
 
-export default {
-  input: 'src/index.ts',
-  external: Object.keys(pkg.dependencies)
-    .concat(Object.keys(pkg.devDependencies))
-    .concat('os', 'path', 'fs', 'http', 'https', 'http2', 'url', 'stream'),
-  output: [
-    {
-      file: 'dist/esm/index.js',
-      format: 'es',
-      sourcemap: true
-    },
-    {
-      file: 'dist/cjs/index.js',
-      format: 'cjs',
-      sourcemap: true,
-      exports: 'named'
-    },
-  ],
-  plugins: [
-    typescript({
-      tsconfig: 'tsconfig.build.json',
-      inlineSources: true,
-    })
-  ],
-  onwarn: (warning, warn) => {
-    if (warning.code === 'CIRCULAR_DEPENDENCY') return;
-    warn(warning)
-  }
-};
+export default getRollupConfig(pkg);
+
+// export default {
+//   input: 'src/index.ts',
+//   external: Object.keys(pkg.dependencies)
+//     .concat(Object.keys(pkg.devDependencies))
+//     .concat('os', 'path', 'fs', 'http', 'https', 'http2', 'url', 'stream'),
+//   output: [
+//     {
+//       file: 'dist/esm/index.mjs',
+//       format: 'es',
+//       sourcemap: true
+//     },
+//     {
+//       file: 'dist/cjs/index.cjs',
+//       format: 'cjs',
+//       sourcemap: true,
+//       exports: 'named'
+//     },
+//   ],
+//   plugins: [
+//     typescript({
+//       tsconfig: 'tsconfig.build.json',
+//       inlineSources: true,
+//     })
+//   ],
+//   onwarn: (warning, warn) => {
+//     if (warning.code === 'CIRCULAR_DEPENDENCY') return;
+//     warn(warning)
+//   }
+// };

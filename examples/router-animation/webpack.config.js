01d7ecca4d04c48703476c0f27c4d43dce4e3f55
bigopon <bigopon.777@gmail.com>
2022-05-14 00:25:27 +1000

    refactor(example): tweak config for router animation test

--- examples/router-animation/webpack.config.js
+++ examples/router-animation/webpack.config.js
@@ -3,25 +3,36 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 const cssLoader = 'css-loader';
+const postcssLoader = {
+  loader: 'postcss-loader',
+  options: {
+    postcssOptions: {
+      plugins: ['autoprefixer']
+    }
+  }
+};
 
 module.exports = function (env, { analyze }) {
   const production = env === 'production' || process.env.NODE_ENV === 'production';
   return {
     mode: production ? 'production' : 'development',
-    devtool: production ? 'source-maps' : 'inline-source-map',
     entry: './src/main.ts',
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'entry-bundle.js'
-    },
+    devtool: production ? 'source-map' : 'inline-source-map',
     resolve: {
       extensions: ['.ts', '.js'],
-      modules: [path.resolve(__dirname, 'src'), 'node_modules']
+      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+      mainFields: ['module', 'main'],
+      // sadly these fallbacks are required to run the app via webpack-dev-server
+      fallback: {
+        'html-entities': require.resolve('html-entities'),
+        'url': require.resolve('url'),
+        'events': require.resolve('events'),
+      },
     },
     devServer: {
       historyApiFallback: true,
       open: !process.env.CI,
-      port: 9035
+      ...(process.env.WEBPACK_PORT ? { port: Number(process.env.WEBPACK_PORT) } : { port: 9035 }),
     },
     module: {
       rules: [
@@ -29,7 +40,7 @@ module.exports = function (env, { analyze }) {
         { test: /\.woff2(\?v=[0-9]\.[0-9]\.[0-9])?$/i, loader: 'url-loader', options: { limit: 10000, mimetype: 'application/font-woff2' } },
         { test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/i, loader: 'url-loader', options: { limit: 10000, mimetype: 'application/font-woff' } },
         { test: /\.(ttf|eot|svg|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/i, loader: 'file-loader' },
-        { test: /\.css$/i, use: ['style-loader', cssLoader] },
+        { test: /\.css$/i, use: ['style-loader', cssLoader, postcssLoader] },
         { test: /\.ts$/i, use: ['ts-loader'], exclude: /node_modules/ },
         { test: /\.html$/i, use: 'html-loader', exclude: /node_modules/ }
       ]

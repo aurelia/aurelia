4ece6fdca74f7aa9b2239d622739a118f29f0f7e
bigopon <bigopon.777@gmail.com>
2022-07-29 22:27:00 +1000

    refactor: rename tests, add before assertion

--- examples/hmr-webpack-e2e/playwright/app.spec.ts
+++ examples/hmr-webpack-e2e/playwright/app.spec.ts
@@ -2,20 +2,22 @@ import { test, expect } from '@playwright/test';
 import * as fs from 'fs';
 import * as path from 'path';
 
-const appFilePath = path.resolve(__dirname, '../src/app.ts');
-const originalContent = fs.readFileSync(appFilePath, { encoding: 'utf-8' });
-test.describe('examples', function () {
+test.describe('examples/hmr-webpack-e2e/app.spec.ts', function () {
+
+  const appFilePath = path.resolve(__dirname, '../src/app.ts');
+  const originalContent = fs.readFileSync(appFilePath, { encoding: 'utf-8' });
 
   test.afterEach(() => {
     fs.writeFileSync(appFilePath, originalContent);
   })
 
-  test(`renders 'Hello World!'`, async function ({ page }) {
+  test(`rerenders without flushing <input> state`, async function ({ page }) {
     test.setTimeout(15000);
     await page.goto('http://localhost:9000', { waitUntil: 'domcontentloaded' });
 
     await expect(page.locator('app > div')).toHaveText('Hello World!');
     await page.type('input', 'abc');
+    await expect(page.locator('input')).toHaveValue('abc');
 
     const newContent = `import { IEventAggregator } from '@aurelia/kernel';
 
@@ -28,7 +30,7 @@ test.describe('examples', function () {
     }
     `;
     fs.writeFileSync(appFilePath, newContent, { encoding: 'utf-8' });
-    
+
     await expect(page.locator('app > div')).toHaveText('New Hello World!');
     await expect(page.locator('input')).toHaveValue('abc');
   });

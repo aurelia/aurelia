54a03e3a405846aebb4f3a909dcbd4a39cb21817
bigopon <9994529+bigopon@users.noreply.github.com>
2022-08-01 21:56:43 +1000

    build(tests): add e2e test for hmr (#1484)

---
+++ examples/hmr-webpack-e2e/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = function (env, { mode }) {
+  const production = mode === 'production';
+  return {
+    mode: production ? 'production' : 'development',
+    entry: './src/startup.ts',
+    devtool: false,
+    resolve: {
+      extensions: ['.ts', '.js'],
+      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+      mainFields: ['module', 'main'],
+      // sadly these fallbacks are required to run the app via webpack-dev-server
+      fallback: {
+        'html-entities': require.resolve('html-entities'),
+        'url': require.resolve('url'),
+        'events': require.resolve('events'),
+      },
+    },
+    devServer: {
+      hot: true,
+      port: 9000,
+      historyApiFallback: true,
+      open: !process.env.CI,
+    },
+    module: {
+      rules: [
+        { test: /\.ts$/i, use: ['ts-loader', '@aurelia/webpack-loader'], exclude: /node_modules/ },
+        { test: /\.html$/i, use: '@aurelia/webpack-loader', exclude: /node_modules/ }
+      ]
+    },
+    plugins: [new HtmlWebpackPlugin({ template: 'index.ejs' })]
+  };
+};

b5abb0947733dab9828c77361a482c10228c0366
bigopon <bigopon.777@gmail.com>
2022-09-01 10:49:19 +1000

    chore: collect coverage from e2e tests

--- packages/__e2e__/playwright-coverage.ts
+++ packages/__e2e__/playwright-coverage.ts
@@ -1,6 +1,6 @@
 import { test } from '@playwright/test';
 import fs from 'fs';
-import { resolve } from 'path';
+import { sep as separator, posix, resolve, normalize, basename, dirname, } from 'path';
 import v8toIstanbul from 'v8-to-istanbul';
 import istanbul from 'istanbul-lib-coverage';
 
@@ -17,7 +17,7 @@ function getExitingCoverage(cwd) {
 }
 
 export function addCoverage() {
-  const cwd = process.cwd();
+  const projectRoot = resolve(__dirname, '../../').split(separator).join(posix.sep);
   const coverages = istanbul.createCoverageMap({});
 
   test.beforeEach(async ({ page }) => {
@@ -27,12 +27,18 @@ export function addCoverage() {
   test.afterEach(async ({ page }) => {
     const coverage = await page.coverage.stopJSCoverage();
     for (const entry of coverage) {
-      // console.log(entry.url.includes(cwd));
-      console.log(entry.url);
-      if (!entry.url.includes(cwd) || /* !entry.url.includes('/@fs/') || */ !entry.url.includes('/packages/')) {
+      if (!entry.url.includes(projectRoot) || /* !entry.url.includes('/@fs/') || */ !entry.url.includes('/packages/')) {
         continue;
       }
-      const scriptPath = entry.url.replace(/^.*@fs\//, '');
+      // vite paths look like this:
+      // win: localhost:9001/@fs/C:/....
+      // nix: localhost:9001/@fs/home
+      // doing a replacement of /@fs is not enough,
+      // and it's not safe to just assume that it's /@fs/[A-Z]:/ (is it?)
+      // so going around a bit
+
+      const rootIndex = entry.url.indexOf(projectRoot);
+      const scriptPath = `../../../${entry.url.slice(rootIndex).replace(projectRoot, '').replace(/^\//, '')}`;
       const converter = v8toIstanbul(scriptPath, 0, { source: entry.source! });
       // eslint-disable-next-line no-await-in-loop
       await converter.load();

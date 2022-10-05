f1907c6d8c5b577bf86ac796975e7f6d8e9bd370
bigopon <bigopon.777@gmail.com>
2022-10-06 09:00:09 +1100

    chore: add bench to gh action

--- benchmarks/app.js
+++ benchmarks/app.js
@@ -1,13 +1,28 @@
 import { Aurelia, CustomElement, StandardConfiguration } from '@aurelia/runtime-html';
+import { createItems } from './utils/data.js';
+
+let $count = 0;
 
 const App = CustomElement.define({
     name: 'app',
-    template: '<div repeat.for="i of items">${i}</div>'
+    template: '<div repeat.for="i of items">${i.message}</div>'
 }, class {
-    items = [1, 2, 3];
+    constructor() {
+        this.newItems($count);
+    }
+
+    newItems(count, seed = 0) {
+        this.items = createItems(count, seed);
+    }
+
+    updateItems() {
+        const len = this.items.length;
+        this.items.forEach((i, idx) => i.update(len - idx + 1))
+    }
 });
 
-export const start = (host) => {
+export const start = (host, count = 0) => {
+    $count = count;
     const au = new Aurelia().register(StandardConfiguration).app({
         component: App,
         host

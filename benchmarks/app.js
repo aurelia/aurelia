1aa6e8c1c482a050bf035c78476403f5b2127775
bigopon <9994529+bigopon@users.noreply.github.com>
2022-10-06 09:34:54 +1100

    chore: add tacho bench (#1588)

---
+++ benchmarks/app.js
@@ -0,0 +1,33 @@
+import { Aurelia, CustomElement, StandardConfiguration } from '@aurelia/runtime-html';
+import { createItems } from './utils/data.js';
+
+let $count = 0;
+
+const App = CustomElement.define({
+    name: 'app',
+    template: '<div repeat.for="i of items">${i.message}</div>'
+}, class {
+    constructor() {
+        this.newItems($count);
+    }
+
+    newItems(count, seed = 0) {
+        this.items = createItems(count, seed);
+    }
+
+    updateItems() {
+        const count = this.items.length;
+        this.items.forEach((i, idx) => i.update(count - idx + 1))
+    }
+});
+
+export const start = (host, count = 0) => {
+    $count = count;
+    const au = new Aurelia().register(StandardConfiguration).app({
+        component: App,
+        host
+    });
+    au.start();
+
+    return au;
+};

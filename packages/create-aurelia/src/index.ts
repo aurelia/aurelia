5118a5ef1cec99ea92b6c70a6ed84ba11e29d012
Brandon Seydel <brandonseydel@hotmail.com>
2023-01-09 15:45:58 -0600

    fix(lint): no child_process for now

--- packages/create-aurelia/src/index.ts
+++ packages/create-aurelia/src/index.ts
@@ -1,4 +1,4 @@
-import { execSync } from 'child_process';
+// import { execSync } from 'child_process';
 
-execSync("npx --yes makes aurelia --no-install --shell");
+// execSync("npx --yes makes aurelia --no-install --shell");
 console.info('Fix this up');

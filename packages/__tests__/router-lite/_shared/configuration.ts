615e9e5c7dd63c24d6021b22e88f144de2f18679
Sayan751 <sayan751@gmail.com>
2023-02-18 22:37:38 +0100

    chore: deepscan issue

--- packages/__tests__/router-lite/_shared/configuration.ts
+++ packages/__tests__/router-lite/_shared/configuration.ts
@@ -1,7 +1,6 @@
 import { IContainer, Registration, IRegistry, LoggerConfiguration, LogLevel, ColorOptions, ConsoleSink, ISink, Class } from '@aurelia/kernel';
 import { MockBrowserHistoryLocation } from '@aurelia/testing';
-import { ILocationManager } from '@aurelia/router-lite';
-import { AppTask, IHistory, ILocation } from '@aurelia/runtime-html';
+import { IHistory, ILocation } from '@aurelia/runtime-html';
 
 export const TestRouterConfiguration = {
   for(logLevel: LogLevel = LogLevel.warn, sinks: Class<ISink>[] = [ConsoleSink]): IRegistry {

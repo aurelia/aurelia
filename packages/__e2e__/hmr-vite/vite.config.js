f1cb6c40977c89614f3fccc37d3035a091ca0515
bigopon <bigopon.777@gmail.com>
2023-04-05 10:13:46 +1000

    chore(ci): fix config for hmr vite

--- packages/__e2e__/hmr-vite/vite.config.js
+++ packages/__e2e__/hmr-vite/vite.config.js
@@ -5,34 +5,6 @@ export default defineConfig({
   server: {
     port: process.env.APP_PORT ?? 5173,
   },
-  resolve: {
-    alias: (() => {
-      const aliases = {};
-      [
-        'aurelia',
-        'fetch-client',
-        'router-lite',
-        'platform',
-        'platform-browser',
-        'router',
-        'kernel',
-        'metadata',
-        'i18n',
-        'state',
-        'route-recognizer',
-        'compat-v1',
-        'dialog',
-        'runtime',
-        'runtime-html',
-        'router-lite',
-      ].forEach((pkg) => {
-        const name = pkg === 'aurelia' ? pkg : `@aurelia/${pkg}`;
-        const packageLocation = require.resolve(name);
-        aliases[name] = require('path').resolve(packageLocation, `../../esm/index.dev.mjs`);
-      });
-      return aliases;
-    })()
-  },
   build: {
     minify: false
   },

4bb63ea9676433adb753d1bc2f552c2871637db6
Chunpeng Huo <huocp@me.com>
2023-05-24 10:27:24 +1000

    chore: update hmr-parcel setup

--- packages/__e2e__/hmr-parcel/playwright/app.spec.ts
+++ packages/__e2e__/hmr-parcel/playwright/app.spec.ts
@@ -1,12 +1,16 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('examples/hmr-parcel/app.spec.ts', function () {
+  test.beforeEach(async ({ page, baseURL }) => {
+    test.setTimeout(15000);
+    await page.goto(baseURL!, { waitUntil: 'domcontentloaded' });
+  });
 
   test(`rerenders without flushing <input> state`, async function ({ page }) {
     page.on('console', msg => console.log(msg.text()));
 
-    await expect(page.locator('app > div')).toHaveText('Hello World!');
-    await page.type('input', 'abc');
+    await expect(page.locator('my-app > div')).toHaveText('Hello World!');
+    await page.locator('input').fill('abc');
     await expect(page.locator('div')).toHaveText('abc');
   });
 });

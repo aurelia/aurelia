85b630553c6877e7ac257bd685d728a28f623491
Brandon Seydel <brandonseydel@hotmail.com>
2023-10-12 17:27:35 -0500

    feat(router): pair the router features down and make same

--- packages/router-lite/src/route-callback.ts
+++ packages/router-lite/src/route-callback.ts
@@ -1,10 +1,9 @@
 import { DI, Registration } from '@aurelia/kernel';
 import { batch } from '@aurelia/runtime';
 import { IWindow, AppTask } from '@aurelia/runtime-html';
-import { Route, RouteType } from './route';
-import { IRouterEvents } from './router-events';
-import { IRouteViewModel } from './component-agent';
-import { IRouteConfig } from './options';
+import { IRouterEvents, NavigationEndEvent } from './router-events';
+import { IRouter } from './router';
+import { Params, ViewportInstructionTree } from './instructions';
 
 export type IRoute<TParams = Record<string, string | undefined>, TQueryParams = Record<string, string | undefined>> = {
     title: string;
@@ -12,44 +11,43 @@ export type IRoute<TParams = Record<string, string | undefined>, TQueryParams = 
     url: string;
     params: TParams;
     queryParams: TQueryParams;
-    components: { component: IRouteViewModel; viewport?: string | null; config: IRouteConfig }[];
+    event: Partial<NavigationEndEvent>
   };
   export const IRoute = DI.createInterface<IRoute>();
   
-
+const flattenParams = (arr:ViewportInstructionTree['children']):Readonly<Params> => {
+  return arr.reduce(function(done,curr){
+    return  {...done, ...curr.recognizedRoute?.route.params, ...flattenParams(curr.children)};
+    }, {} as Readonly<Params>) ;
+}
+  
 export const RouteCallback =  Registration.cachedCallback(IRoute, (container) => {
     const route: IRoute = {
       path: '',
       url: '',
       queryParams: {},
       params: {},
-      components: [],
       title: '',
+      event: {}
     };
 
     const localWindow = container.get(IWindow);
     const routerEvents = container.get(IRouterEvents);
+    const router = container.get(IRouter);
 
-    const navEndSubscription = routerEvents.subscribe('au:router:navigation-end', ({ finalInstructions }) => {
-      const { children, queryParams } = finalInstructions;
+    const navEndSubscription = routerEvents.subscribe('au:router:navigation-end', (event) => {
+      const { finalInstructions, finalInstructions: { children, queryParams} } = event;
 
       batch(() => {
         route.path = finalInstructions.toPath();
-        route.url = finalInstructions.toUrl();
-        route.components = children.map((x) => ({
-          component: x.component,
-          viewport: x.viewport,
-          config: Route.getConfig(x.component as unknown as RouteType),
-        }));
+        route.url = finalInstructions.toUrl(true, router.options._urlParser);
         route.title = localWindow.document.title;
         route.queryParams = Array.from(queryParams).reduce<Record<string, string>>((acc, [key, value]) => {
           acc[key] = value;
           return acc;
         }, {});
-        route.params = children.reduce((acc, { recognizedRoute }) => {
-          const params = recognizedRoute?.route.params ?? ({} as Record<string, string | undefined>);
-          return { ...acc, ...params };
-        }, {});
+        route.params = flattenParams(children);
+        route.event = event;
       });
     });
     container.register(AppTask.deactivated(() => navEndSubscription.dispose()));

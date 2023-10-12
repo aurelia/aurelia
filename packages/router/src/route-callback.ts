e9c57043640d20bb47aa33f664220c559fbc85a6
Brandon Seydel <brandonseydel@hotmail.com>
2023-10-12 17:27:35 -0500

    feat(router): pair the router features down and make same

--- packages/router/src/route-callback.ts
+++ packages/router/src/route-callback.ts
@@ -1,8 +1,7 @@
 import { DI, IEventAggregator, Registration } from '@aurelia/kernel';
 import { batch } from '@aurelia/runtime';
 import { IWindow, AppTask } from '@aurelia/runtime-html';
-import { Route, RouteType } from './route';
-import { IRouteableComponent, IRouterOptions, RouteableComponentType, RouterNavigationEndEvent } from '.';
+import { RouterNavigationEndEvent } from '.';
 
 export type IRoute<TParams = Record<string, string | undefined>, TQueryParams = Record<string, string | undefined>> = {
     title: string;
@@ -10,46 +9,38 @@ export type IRoute<TParams = Record<string, string | undefined>, TQueryParams = 
     url: string;
     params: TParams;
     queryParams: TQueryParams;
-    components: { component: IRouteableComponent; viewport?: string | null; config: IRouterOptions }[];
+    event: Partial<RouterNavigationEndEvent>;
   };
   
 export const IRoute = DI.createInterface<IRoute>();
   
-
 export const RouteCallback =  Registration.cachedCallback(IRoute, (container) => {
     const route: IRoute = {
       path: '',
       url: '',
       queryParams: {},
       params: {},
-      components: [],
       title: '',
+      event: {}
     };
 
     const localWindow = container.get(IWindow);
     const eventAggregator = container.get(IEventAggregator);
 
-    const navEndSubscription = eventAggregator.subscribe('au:router:navigation-end', ({ navigation }:RouterNavigationEndEvent) => {
+    const navEndSubscription = eventAggregator.subscribe('au:router:navigation-end', (event:RouterNavigationEndEvent) => {
+
+      const {navigation} = event;
 
       batch(() => {
         route.path = String(navigation.instruction) ?? '';
         route.url = navigation.path ?? '';
-        route.components = Array.isArray(navigation.fullStateInstruction)
-          ? navigation.fullStateInstruction
-              .flatMap((x) => [x, ...(x.nextScopeInstructions ?? [])])
-              .filter((x) => x.component.type)
-              .map((x) => ({
-                component: x.component.type ?? {},
-                viewport: x.viewport?.name,
-                config: Route.getConfiguration(x.component.type as unknown as RouteableComponentType),
-              }))
-          : [];
         route.title = navigation.title ?? localWindow.document.title;
         route.queryParams = Array.from(new URLSearchParams(navigation.query)).reduce<Record<string, string>>((acc, [key, value]) => {
           acc[key] = value;
           return acc;
         }, {});
-        route.params = navigation.parameters as Record<string, string>;
+        route.params =  navigation.parameters as Record<string, string>;
+        route.event = event;
       });
     });
     container.register(AppTask.deactivated(() => navEndSubscription.dispose()));

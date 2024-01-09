6fee57332a4fae0f0c28ef29dd4ce71adc75ed4f
Sayan751 <sayan751@gmail.com>
2024-01-09 22:40:07 +0100

    fix(runtime-html): template wrapping

--- packages/runtime-html/src/compiler/template-element-factory.ts
+++ packages/runtime-html/src/compiler/template-element-factory.ts
@@ -36,13 +36,13 @@ export class TemplateElementFactory {
         const node = template.content.firstElementChild;
         // if the input is either not wrapped in a template or there is more than one node,
         // return the whole template that wraps it/them (and create a new one for the next input)
-        if (node == null || node.nodeName !== 'TEMPLATE' || node.nextElementSibling != null) {
+        if (needsWrapping(node)) {
           this._template = this.t();
           result = template;
         } else {
           // the node to return is both a template and the only node, so return just the node
           // and clean up the template for the next input
-          template.content.removeChild(node);
+          template.content.removeChild(node!);
           result = node as HTMLTemplateElement;
         }
 
@@ -61,5 +61,35 @@ export class TemplateElementFactory {
     // do any other processing
     input.parentNode?.removeChild(input);
     return input.cloneNode(true) as HTMLTemplateElement;
+
+    function needsWrapping(node: Element | null | undefined): boolean {
+      if (node == null) return true;
+      if (node.nodeName !== 'TEMPLATE') return true;
+      // At this point the node is a template element.
+      // If the template has meaningful siblings, then it needs wrapping.
+      const prevSibling = node.previousSibling;
+      if (prevSibling != null) {
+        switch (prevSibling.nodeType) {
+          case 1: // Element
+            return true;
+          case 3: // Text
+            return prevSibling.textContent!.trim().length > 0;
+        }
+      }
+
+      // the previous sibling was not meaningful, so check the next sibling
+      const nextSibling = node.nextSibling;
+      if (nextSibling != null) {
+        switch (nextSibling.nodeType) {
+          case 1: // Element
+            return true;
+          case 3: // Text
+            return nextSibling.textContent!.trim().length > 0;
+        }
+      }
+
+      // neither the previous nor the next sibling was meaningful, hence the template does not need wrapping
+      return false;
+    }
   }
 }

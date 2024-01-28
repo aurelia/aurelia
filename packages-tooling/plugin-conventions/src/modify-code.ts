67e6a37bb3217775e5d1a90bc97f847cfea9fa7e
bigopon <bigopon.777@gmail.com>
2024-01-28 20:46:39 +1100

    fix: handle esm/cjs issues

--- packages-tooling/plugin-conventions/src/modify-code.ts
+++ packages-tooling/plugin-conventions/src/modify-code.ts
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import $modifyCode from 'modify-code';
 import * as ModifyCode from 'modify-code';
 
-// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
 export const modifyCode = (typeof $modifyCode === 'function'
     ? $modifyCode
-    : typeof ModifyCode === 'function'
-        ? ModifyCode
-        : ModifyCode.default) as typeof $modifyCode;
+    : typeof ($modifyCode as any).default === 'function'
+        ? ($modifyCode as any).default
+        : typeof ModifyCode === 'function'
+            ? ModifyCode
+            : ModifyCode.default) as typeof $modifyCode;

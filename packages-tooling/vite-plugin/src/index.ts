a416807e079393a0ab10b23d5da820699c4f85bb
Chunpeng Huo <huocp@me.com>
2024-04-04 12:47:05 +1000

    fix(vite-plugin): missed some default options in "load" preprocess
    closes #1935

--- packages-tooling/vite-plugin/src/index.ts
+++ packages-tooling/vite-plugin/src/index.ts
@@ -107,7 +107,9 @@ export default function au(options: {
         contents: code,
       }, {
         hmrModule: 'import.meta',
-        transformHtmlImportSpecifier: s => s.replace(/\.html$/, '.$au.ts')
+        transformHtmlImportSpecifier: s => s.replace(/\.html$/, '.$au.ts'),
+        stringModuleWrap: (id) => `${id}?inline`,
+        ...additionalOptions
       });
       return result!.code;
     }

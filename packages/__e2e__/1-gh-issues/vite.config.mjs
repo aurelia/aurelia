1971a57abbb2ffcb79f4c489ad8a5369f773b655
bigopon <bigopon.777@gmail.com>
2024-04-30 21:23:18 +1000

    refactor: rename & add order to e2e tests

---
+++ packages/__e2e__/1-gh-issues/vite.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite';
+import au from '@aurelia/vite-plugin';
+import { createFilter } from '@rollup/pluginutils';
+import { resolve } from 'path';
+
+export default defineConfig({
+  server: {
+    port: process.env.APP_PORT ?? 5173,
+  },
+  resolve: {
+    alias: {
+      ...[
+        'fetch-client',
+        'router-lite',
+        'kernel',
+        'expression-parser',
+        'runtime',
+        'template-compiler',
+        'runtime-html',
+        'router-lite',
+      ].reduce((map, pkg) => {
+        const name = `@aurelia/${pkg}`;
+        map[name] = resolve(__dirname, `../../../node_modules/${name}/dist/esm/index.dev.mjs`);
+        return map;
+      }, {})
+    }
+  },
+  plugins: [
+    au({ useDev: true })
+  ]
+});

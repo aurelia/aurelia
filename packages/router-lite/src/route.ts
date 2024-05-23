d2a6cae14188c3851bd1f8b375d65abc2c606f1f
Sayan751 <sayan751@gmail.com>
2024-05-23 19:48:41 +0200

    chore: fixed broken tests

--- packages/router-lite/src/route.ts
+++ packages/router-lite/src/route.ts
@@ -14,9 +14,6 @@ import { RESIDUE } from '@aurelia/route-recognizer';
 
 export const noRoutes = emptyArray as RouteConfig['routes'];
 
-function cleanPath(path: string): string { return path.replace(`/*${RESIDUE}`, ''); }
-function hasSamePath(nodeA: RouteNode, nodeB: RouteNode): boolean { return cleanPath(nodeA.finalPath) === cleanPath(nodeB.finalPath); }
-
 // Every kind of route configurations are normalized to this `RouteConfig` class.
 export class RouteConfig implements IRouteConfig, IChildRouteConfig {
   /** @internal */
@@ -146,6 +143,15 @@ export class RouteConfig implements IRouteConfig, IChildRouteConfig {
 
     const plan = this.transitionPlan ?? 'replace';
     return typeof plan === 'function' ? plan(cur, next) : plan;
+
+    function cleanPath(path: string): string { return path.replace(`/*${RESIDUE}`, ''); }
+    function hasSamePath(nodeA: RouteNode, nodeB: RouteNode): boolean {
+      const pathA = nodeA.finalPath;
+      const pathB = nodeB.finalPath;
+      // As this function is invoked when the components are same, we are giving a benefit of doubt for empty paths.
+      // It is seems like a sensible assumption that a transition from '' to '/p1' (assuming p1 is same as the empty path) does not require a non-none transition.
+      return pathA.length === 0 || pathB.length === 0 || cleanPath(pathA) === cleanPath(pathB);
+    }
   }
 
   /** @internal */

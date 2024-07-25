21b171e145193c81aa2313f8e59323782bcbc75c
Jürgen Wenzel <jurgen@jwenzel.se>
2024-07-25 23:46:29 +0200

    test(*): restore sus non-router tests

--- packages/__tests__/src/3-runtime-html/switch.spec.ts
+++ packages/__tests__/src/3-runtime-html/switch.spec.ts
@@ -42,7 +42,7 @@ import {
   TestFunction,
 } from '../util.js';
 
-describe.skip('3-runtime-html/switch.spec.ts', function () {
+describe('3-runtime-html/switch.spec.ts', function () {
 
   const enum Status {
     unknown = 'unknown',

14a3f77261a3f89cdc1f0c2ba19e886ea9b22ba6
fkleuver <fred@avurad.nl>
2024-12-11 21:56:05 +0100

    test(queue): impl global-filters-panel

--- packages/__tests__/src/3-runtime-html/commerce/app/global-filters-panel.ts
+++ packages/__tests__/src/3-runtime-html/commerce/app/global-filters-panel.ts
@@ -4,7 +4,13 @@ import { GlobalFilters, DashboardState } from '../domain';
 @customElement({
   name: 'global-filters-panel',
   template: `
-
+    <input ref="startDateInput" type="date" value.bind="filters.startDate"></input>
+    <input ref="endDateInput" type="date" value.bind="filters.endDate"></input>
+    <div ref="selectedCategoryIdsDiv">
+      <input type="checkbox" repeat.for="id of filters.selectedCategoryIds"></input>
+    </div>
+    <input ref="showProjectedTrendsCheckbox" type="checkbox" checked.bind="showProjectedTrends"></input>
+    <input ref="enableAutoRestockCheckbox" type="checkbox" checked.bind="enableAutoRestock"></input>
   `
 })
 export class GlobalFiltersPanel {

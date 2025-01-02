7b8490a06782892c4d668efcb0948f2d1ec67a9f
Sayan751 <sayan751@gmail.com>
2025-01-02 21:07:02 +0100

    chore: fixed build issues

--- scripts/logger.ts
+++ scripts/logger.ts
@@ -1,11 +1,10 @@
-import * as c from 'chalk';
+import c from 'chalk';
 import * as l from 'fancy-log';
 
-const chalk = ((c as {default?: unknown}).default || c) as typeof import('chalk');
 const log = ((l as {default?: unknown}).default || l) as typeof import('fancy-log');
 
 export function createLogger(name: string): typeof log {
-  const prefix = `> ${chalk.green(name)} `;
+  const prefix = `> ${c.green(name)} `;
   const logger: typeof log = log.bind(log, prefix);
   logger.info = log.info.bind(log, prefix);
   logger.dir = log.dir.bind(log, prefix);
@@ -14,4 +13,4 @@ export function createLogger(name: string): typeof log {
   return logger;
 }
 
-export { chalk as c };
+export { c };

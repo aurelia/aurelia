489c269788d5d11ca2da4b0c975da070d7bc681f
Sayan751 <sayan751@gmail.com>
2025-01-02 21:43:21 +0100

    chore: reverted changes

--- scripts/logger.ts
+++ scripts/logger.ts
@@ -5,7 +5,7 @@ const chalk = ((c as {default?: unknown}).default || c) as typeof import('chalk'
 const log = ((l as {default?: unknown}).default || l) as typeof import('fancy-log');
 
 export function createLogger(name: string): typeof log {
-  const prefix = `> ${c.green(name)} `;
+  const prefix = `> ${chalk.green(name)} `;
   const logger: typeof log = log.bind(log, prefix);
   logger.info = log.info.bind(log, prefix);
   logger.dir = log.dir.bind(log, prefix);

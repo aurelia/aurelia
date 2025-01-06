8e1674dbf1717bedaa8340cc46873175bd8736b0
fkleuver <fred@avurad.nl>
2025-01-06 18:53:36 +0100

    test(repeat): add more duplicate obj edge cases

--- packages/__tests__/src/3-runtime-html/repeat.duplicates.spec.ts
+++ packages/__tests__/src/3-runtime-html/repeat.duplicates.spec.ts
@@ -102,19 +102,93 @@ describe("3-runtime-html/repeat.duplicates.spec.ts", function () {
     });
 
     it('object duplication unshift (issue 2078)', function () {
-      const objA = { toString() { return 'a'; } };
-      const objB = { toString() { return 'b'; } };
+      const objA = { toString() { return 'A'; } };
+      const objB = { toString() { return 'B'; } };
 
       const { assertText, component, flush } = createFixture(
         `<div repeat.for="i of items">\${$index}-\${i} </div>`,
         class { items = [objA, objB]; }
       );
-      assertText('0-a 1-b ');
+      assertText('0-A 1-B ');
 
       component.items.unshift(objB);
       flush();
 
-      assertText('0-b 1-a 2-b ');
+      assertText('0-B 1-A 2-B ');
+    });
+
+    it('object duplication push with multiple references', function () {
+      const objA = { toString() { return 'A'; } };
+      const objB = { toString() { return 'B'; } };
+
+      const { assertText, component, flush } = createFixture(
+        `<div repeat.for="i of items">\${$index}-\${i} </div>`,
+        class { items = [objA, objB]; }
+      );
+      assertText('0-A 1-B ');
+
+      component.items.push(objA, objB);
+      flush();
+
+      assertText('0-A 1-B 2-A 3-B ');
+    });
+
+    it('object duplication splice in the middle', function () {
+      const objA = { toString() { return 'A'; } };
+      const objB = { toString() { return 'B'; } };
+      const objC = { toString() { return 'C'; } };
+
+      const { assertText, component, flush } = createFixture(
+        `<div repeat.for="i of items">\${$index}-\${i} </div>`,
+        class { items = [objA, objB, objC]; }
+      );
+      assertText('0-A 1-B 2-C ');
+
+      component.items.splice(1, 1, objA, objA);
+      flush();
+
+      assertText('0-A 1-A 2-A 3-C ');
+    });
+
+    it('object duplication sort (ascending vs. descending)', function () {
+      const obj1 = { id: 3, toString() { return `X${this.id}`; } };
+      const obj2 = { id: 1, toString() { return `X${this.id}`; } };
+      const obj3 = { id: 2, toString() { return `X${this.id}`; } };
+      const obj2Dup = obj2;
+
+      const { assertText, component, flush } = createFixture(
+        `<div repeat.for="i of items">\${$index}-\${i} </div>`,
+        class { items = [obj1, obj2, obj3, obj2Dup]; }
+      );
+      assertText('0-X3 1-X1 2-X2 3-X1 ');
+
+      component.items.sort((a, b) => a.id - b.id);
+      flush();
+
+      assertText('0-X1 1-X1 2-X2 3-X3 ');
+
+      component.items.sort((a, b) => b.id - a.id);
+      flush();
+
+      assertText('0-X3 1-X2 2-X1 3-X1 ');
+    });
+
+    it('unshift multiple duplicates', function () {
+      const objA = { toString() { return 'A'; } };
+
+      const { assertText, component, flush } = createFixture(
+        `<div repeat.for="i of items">\${$index}-\${i} </div>`,
+        class { items = [objA, objA]; }
+      );
+      assertText('0-A 1-A ');
+
+      component.items.unshift(objA);
+      flush();
+      assertText('0-A 1-A 2-A ');
+
+      component.items.unshift(objA);
+      flush();
+      assertText('0-A 1-A 2-A 3-A ');
     });
   });
 });

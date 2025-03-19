9064a438b0e7a7c406b53b32c248246eafdc5abf
Sayan751 <sayan751@gmail.com>
2025-03-19 19:56:29 +0100

    chore: added failing example

--- packages/__e2e__/6-router-lite/src/index.ts
+++ packages/__e2e__/6-router-lite/src/index.ts
@@ -2,11 +2,15 @@ import Aurelia from 'aurelia';
 import { RouterConfiguration } from '@aurelia/router-lite';
 import { App } from './app';
 import { IRouterEventLoggerService } from './router-event-logger-service';
+import { C1, C2, Promisified } from './pages/promisified';
 
 Aurelia
   .register(
     RouterConfiguration,
     IRouterEventLoggerService,
+    Promisified,
+    C1,
+    C2,
   )
   .app({
     host: document.body,
---
+++ packages/__e2e__/6-router-lite/src/pages/promisified.ts
@@ -0,0 +1,43 @@
+import { IRouteViewModel, NavigationInstruction, Params, route, RouteNode } from '@aurelia/router-lite';
+import { customElement } from 'aurelia';
+
+@customElement({
+  name: 'c-1',
+  template: `child one`
+})
+export class C1 { }
+
+@customElement({
+  name: 'c-2',
+  template: `child two`
+})
+export class C2 { }
+
+@route({ routes: [C1, C2] })
+@customElement({
+  name: 'pro-misified',
+  template: `<template>
+  <template promise.bind="loadingPromise">
+    <template pending>
+      Please wait...
+      <button click.trigger="promiseResolver()">Resolve</button>
+      <button click.trigger="promiseRejector()">Cancel</button>
+    </template>
+    <au-viewport then></au-viewport>
+  </template>
+</template>`
+})
+export class Promisified implements IRouteViewModel {
+  private loadingPromise!: Promise<void>;
+  private promiseResolver: () => void;
+  private promiseRejector: () => void;
+
+  public canLoad(_params: Params, _next: RouteNode, _current: RouteNode | null): boolean | NavigationInstruction | NavigationInstruction[] | Promise<boolean | NavigationInstruction | NavigationInstruction[]> {
+    this.loadingPromise = new Promise((resolve, reject) => {
+      this.promiseResolver = resolve;
+      this.promiseRejector = reject;
+    });
+    return true;
+    // return this.loadingPromise;
+  }
+}

6e5c0a7b3fb564f01d86fcde4940ca7dc9bfe618
bigopon <bigopon.777@gmail.com>
2025-03-26 23:00:15 +1100

    fix: ref binding after master sync

--- packages/runtime-html/src/binding/ref-binding.ts
+++ packages/runtime-html/src/binding/ref-binding.ts
@@ -4,6 +4,10 @@ import {
   type IObserverLocatorBasedConnectable,
   type ISubscriber,
   type Scope,
+  astAssign,
+  astBind,
+  astEvaluate,
+  astUnbind,
   type IAstEvaluator,
   connectable,
   IObserverLocator,
@@ -11,7 +15,6 @@ import {
 import { createPrototypeMixer, mixinAstEvaluator, mixingBindingLimited, mixinUseScope } from './binding-utils';
 import { type IsBindingBehavior } from '@aurelia/expression-parser';
 import { IBinding } from './interfaces-bindings';
-import { bindingHandleChange, bindingHandleCollectionChange } from './_lifecycle';
 
 export interface RefBinding extends IAstEvaluator, IObserverLocatorBasedConnectable, IServiceLocator { }
 export class RefBinding implements IBinding, ISubscriber, ICollectionSubscriber {
@@ -42,12 +45,59 @@ export class RefBinding implements IBinding, ISubscriber, ICollectionSubscriber 
     this.l = locator;
   }
 
+  public updateSource() {
+    if (this.isBound) {
+      this.obs.version++;
+      astAssign(this.ast, this._scope!, this, this, this.target);
+      this.obs.clear();
+    } else {
+      astAssign(this.ast, this._scope!, this, null, null);
+    }
+  }
+
   public handleChange(): void {
-    // TODO: see if we can get rid of this by integrating this call in connectable
-    bindingHandleChange(this);
+    if (this.isBound) {
+      this.updateSource();
+    }
   }
 
   public handleCollectionChange(): void {
-    bindingHandleCollectionChange(this);
+    if (this.isBound) {
+      this.updateSource();
+    }
+  }
+
+  public bind(_scope: Scope): void {
+    if (this.isBound) {
+      if (this._scope === _scope) {
+      /* istanbul-ignore-next */
+        return;
+      }
+
+      this.unbind();
+    }
+    this._scope = _scope;
+
+    astBind(this.ast, _scope, this);
+    this.isBound = true;
+
+    this.updateSource();
+  }
+
+  public unbind(): void {
+    if (!this.isBound) {
+      /* istanbul-ignore-next */
+      return;
+    }
+    this.isBound = false;
+    this.obs.clearAll();
+
+    if (astEvaluate(this.ast, this._scope!, this, null) === this.target) {
+      this.updateSource();
+    }
+
+    astUnbind(this.ast, this._scope!, this);
+
+    this._scope = void 0;
   }
 }

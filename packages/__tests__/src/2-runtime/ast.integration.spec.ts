27a2340e84253bab4ad0217333dcef63751f63de
Fred Kleuver <fred@avurad.nl>
2025-04-17 10:36:26 +0200

    chore: fix ast.integration.spec

--- packages/__tests__/src/2-runtime/ast.integration.spec.ts
+++ packages/__tests__/src/2-runtime/ast.integration.spec.ts
@@ -47,6 +47,7 @@ describe('2-runtime/ast.integration.spec.ts', function () {
 
         Array.from({ length: 5 }).forEach((_, idx) => {
           source.name = `${idx}`;
+          flush();
           assert.strictEqual(target.name, `${idx}`);
         });
       });
@@ -88,29 +89,36 @@ describe('2-runtime/ast.integration.spec.ts', function () {
         Array.from({ length: 5 }).forEach((_, idx) => {
           const $count = handleChangeCallCount;
           source.checked = !source.checked;
+          flush();
           assert.strictEqual(target.value, source.checked ? 'yes' : 'no');
           assert.strictEqual(handleChangeCallCount, $count + 1);
           if (source.checked) {
             source.yesMessage = `yes ${idx}`;
+            flush();
             assert.strictEqual(target.value, `yes ${idx}`);
             assert.strictEqual(handleChangeCallCount, $count + 2);
             // assert the binding has dropped the old observers of the inactive branch in conditional
             source.noMessage = `no ${idx}`;
+            flush();
             assert.strictEqual(target.value, `yes ${idx}`);
             assert.strictEqual(handleChangeCallCount, $count + 2);
             // revert it back for next assertion
             source.noMessage = 'no';
+            flush();
             assert.strictEqual(handleChangeCallCount, $count + 2);
           } else {
             source.noMessage = `no ${idx}`;
+            flush();
             assert.strictEqual(target.value, `no ${idx}`);
             assert.strictEqual(handleChangeCallCount, $count + 2);
             // assert the binding has dropped the old observers of the inactive branch in conditional
             source.yesMessage = `yes ${idx}`;
+            flush();
             assert.strictEqual(target.value, `no ${idx}`);
             assert.strictEqual(handleChangeCallCount, $count + 2);
             // revert it back to normal for next assertion
             source.yesMessage = 'yes';
+            flush();
             assert.strictEqual(handleChangeCallCount, $count + 2);
           }
         });

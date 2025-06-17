5cd4ee404d04b6d916d92437ac3c87ed87808226
Fred Kleuver <fred@avurad.nl>
2025-06-17 14:00:50 +0200

    chore: fix array-length-observer.spec

--- packages/__tests__/src/3-runtime-html/array-length-observer.spec.ts
+++ packages/__tests__/src/3-runtime-html/array-length-observer.spec.ts
@@ -66,12 +66,15 @@ describe('3-runtime-html/array-length-observer.spec.ts', function () {
     assertText('123');
 
     type('input', '2');
+    runTasks();
     assertText('12');
 
     type('input', '3');
+    runTasks();
     assertText('12');
 
     type('input', '1');
+    runTasks();
     assertText('1');
   });
 });

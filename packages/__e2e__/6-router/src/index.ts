75aaae0e69f2179da23e6e39e2555b1269c24cb0
Sayan751 <sayan751@gmail.com>
2025-09-08 21:39:24 +0200

    test(router): animation with or without fragment hash configuration

--- packages/__e2e__/6-router/src/index.ts
+++ packages/__e2e__/6-router/src/index.ts
@@ -3,9 +3,13 @@ import { RouterConfiguration } from '@aurelia/router';
 import { App } from './app';
 import { IRouterEventLoggerService } from './router-event-logger-service';
 
+// set the useUrlFragmentHash based on the query string of the same parameter
+const params = new URLSearchParams(window.location.search);
+const useUrlFragmentHash = params.get('useUrlFragmentHash') === 'true';
+
 Aurelia
   .register(
-    RouterConfiguration,
+    RouterConfiguration.customize({ useUrlFragmentHash }),
     IRouterEventLoggerService,
   )
   .app({
